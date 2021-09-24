e6e8217fc27769be836a50c246025352682d4dc8
Szymon Szlachtowicz <38212223+Szymx95@users.noreply.github.com>
2021-09-24 13:36:26 +0200

    Add react chat packages (#11)

---
+++ packages/react-chat-example/src/index.tsx
@@ -0,0 +1,10 @@
+import { ReactChat } from "@dappconnect/react-chat";
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div style={{ height: "100%" }}>
+    <ReactChat />
+  </div>,
+  document.getElementById("root")
+);
