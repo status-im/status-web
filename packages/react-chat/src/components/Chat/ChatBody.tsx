e3f2cec181471ec9112e47c7c3392c846f0591a0
Maria Rushkova <maria@ethworks.io>
2021-09-28 12:23:06 +0200

    Add chat body components

--- packages/react-chat/src/components/Chat/ChatBody.tsx
+++ packages/react-chat/src/components/Chat/ChatBody.tsx
@@ -1,33 +1,42 @@
-import React, { useCallback, useState } from "react";
-import styled from "styled-components";
+import React, { useCallback, useState } from 'react';
+import styled from 'styled-components';
+import { ChannelData } from '../../helpers/channelsMock';
+import { Theme } from '../../styles/themes';
+import { Channel } from '../Channels';
+import { ChatMessage } from '../models/ChatMessage';
+import { ChatInput } from './ChatInput';
+import { ChatMessages } from './ChatMessages';
 
-import { ChatMessage } from "../models/ChatMessage";
+interface ChatBodyProps {
+  theme: Theme;
+  channel: ChannelData;
+}
 
-import { ChatInput } from "./ChatInput";
-import { ChatMessages } from "./ChatMessages";
-
-export function ChatBody() {
+export function ChatBody({ theme, channel }: ChatBodyProps) {
   const [messages, setMessages] = useState<ChatMessage[]>([]);
 
   const addMessage = useCallback(
     (message: string) => {
-      setMessages((prevMessages) => [
-        ...prevMessages,
-        { sender: "User1", date: new Date(), content: message },
-      ]);
+      setMessages(prevMessages => [...prevMessages, { sender: 'User1', date: new Date(), content: message }]);
     },
     [messages]
   );
 
   return (
-    <ChatBodyWrapper>
+    <ChatBodyWrapper theme={theme}>
+      <Channel channel={channel} theme={theme} isActive={true} activeView={true} />
       <ChatMessages messages={messages} />
       <ChatInput addMessage={addMessage} />
     </ChatBodyWrapper>
   );
 }
+interface ThemeProps {
+  theme: Theme;
+}
 
-const ChatBodyWrapper = styled.div`
+const ChatBodyWrapper = styled.div<ThemeProps>`
+  display: flex;
+  flex-direction: column;
   flex: 1;
   height: 100%;
   max-width: 50%;
