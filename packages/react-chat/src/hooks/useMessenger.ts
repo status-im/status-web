6938816fa6c8354523ddd88913608b33acb8e431
Maria Rushkova <maria@ethworks.io>
2021-09-30 11:30:01 +0200

    Change network

--- packages/react-chat/src/hooks/useMessenger.ts
+++ packages/react-chat/src/hooks/useMessenger.ts
@@ -1,26 +1,24 @@
 // import { StoreCodec } from "js-waku";
-import { getBootstrapNodes, StoreCodec } from "js-waku";
-import { useCallback, useEffect, useState } from "react";
+import { getBootstrapNodes, StoreCodec } from 'js-waku';
+import { useCallback, useEffect, useState } from 'react';
 import {
   ChatMessage as ApiChatMessage,
   ApplicationMetadataMessage,
   Identity,
   Messenger,
-} from "status-communities/dist/cjs";
+} from 'status-communities/dist/cjs';
 
-import { ChatMessage } from "../models/ChatMessage";
+import { ChatMessage } from '../models/ChatMessage';
 
 export function useMessenger(chatId: string, chatIdList: string[]) {
   const [messenger, setMessenger] = useState<Messenger | undefined>(undefined);
-  const [messages, setMessages] = useState<{ [chatId: string]: ChatMessage[] }>(
-    {}
-  );
+  const [messages, setMessages] = useState<{ [chatId: string]: ChatMessage[] }>({});
   const [notifications, setNotifications] = useState<{
     [chatId: string]: number;
   }>({});
 
   const clearNotifications = useCallback((id: string) => {
-    setNotifications((prevNotifications) => {
+    setNotifications(prevNotifications => {
       return {
         ...prevNotifications,
         [id]: 0,
@@ -28,94 +26,68 @@ export function useMessenger(chatId: string, chatIdList: string[]) {
     });
   }, []);
 
-  const addNewMessage = useCallback(
-    (sender: Uint8Array, content: string, date: Date, id: string) => {
-      let signer = "0x";
-      sender.forEach((e) => {
-        signer = signer + e.toString(16);
-      });
-      setMessages((prevMessages) => {
-        const newMessage = {
-          sender: signer,
-          content: content,
-          date: date,
-        };
-        if (prevMessages?.[id]) {
-          return {
-            ...prevMessages,
-            [id]: [...prevMessages[id], newMessage],
-          };
-        } else {
-          return {
-            ...prevMessages,
-            [id]: [newMessage],
-          };
-        }
-      });
-      setNotifications((prevNotifications) => {
+  const addNewMessage = useCallback((sender: Uint8Array, content: string, date: Date, id: string) => {
+    let signer = '0x';
+    sender.forEach(e => {
+      signer = signer + e.toString(16);
+    });
+    setMessages(prevMessages => {
+      const newMessage = {
+        sender: signer,
+        content: content,
+        date: date,
+      };
+      if (prevMessages?.[id]) {
         return {
-          ...prevNotifications,
-          [id]: prevNotifications[id] + 1,
+          ...prevMessages,
+          [id]: [...prevMessages[id], newMessage],
         };
-      });
-    },
-    []
-  );
+      } else {
+        return {
+          ...prevMessages,
+          [id]: [newMessage],
+        };
+      }
+    });
+    setNotifications(prevNotifications => {
+      return {
+        ...prevNotifications,
+        [id]: prevNotifications[id] + 1,
+      };
+    });
+  }, []);
 
   useEffect(() => {
     const createMessenger = async () => {
       const identity = Identity.generate();
 
       const messenger = await Messenger.create(identity, {
-        bootstrap: getBootstrapNodes.bind({}, [
-          "fleets",
-          "wakuv2.test",
-          "waku-websocket",
-        ]),
+        bootstrap: getBootstrapNodes.bind({}, ['fleets', 'wakuv2.test', 'waku-websocket']),
       });
-      await new Promise((resolve) => {
-        messenger.waku.libp2p.peerStore.on(
-          "change:protocols",
-          ({ protocols }) => {
-            if (protocols.includes(StoreCodec)) {
-              resolve("");
-            }
+      await new Promise(resolve => {
+        messenger.waku.libp2p.peerStore.on('change:protocols', ({ protocols }) => {
+          if (protocols.includes(StoreCodec)) {
+            resolve('');
           }
-        );
+        });
       });
 
       await Promise.all(
-        chatIdList.map(async (id) => {
+        chatIdList.map(async id => {
           await messenger.joinChat(id);
           const chat = messenger.chatsById.get(id);
           if (chat) {
-            const messages = await messenger.waku.store.queryHistory(
-              [chat.contentTopic],
-              { decryptionKeys: [chat.symKey] }
-            );
-            messages.sort((a, b) =>
-              (a?.timestamp?.getTime() ?? 0) > (b?.timestamp?.getTime() ?? 0)
-                ? 1
-                : -1
-            );
-            messages.forEach((message) => {
+            const messages = await messenger.waku.store.queryHistory([chat.contentTopic], {
+              decryptionKeys: [chat.symKey],
+            });
+            messages.sort((a, b) => ((a?.timestamp?.getTime() ?? 0) > (b?.timestamp?.getTime() ?? 0) ? 1 : -1));
+            messages.forEach(message => {
               if (message.payload) {
-                const metadata = ApplicationMetadataMessage.decode(
-                  message.payload
-                );
+                const metadata = ApplicationMetadataMessage.decode(message.payload);
                 if (metadata.payload) {
                   const chatMessage = ApiChatMessage.decode(metadata.payload);
-                  if (
-                    metadata.signer &&
-                    chatMessage.text &&
-                    chatMessage.proto.timestamp
-                  ) {
-                    addNewMessage(
-                      metadata.signer,
-                      chatMessage.text,
-                      new Date(chatMessage.proto.timestamp ?? 0),
-                      id
-                    );
+                  if (metadata.signer && chatMessage.text && chatMessage.proto.timestamp) {
+                    addNewMessage(metadata.signer, chatMessage.text, new Date(chatMessage.proto.timestamp ?? 0), id);
                   }
                 }
               }
@@ -123,10 +95,10 @@ export function useMessenger(chatId: string, chatIdList: string[]) {
             });
           }
           clearNotifications(id);
-          messenger.addObserver((message) => {
+          messenger.addObserver(message => {
             addNewMessage(
               message.signer ?? new Uint8Array(),
-              message.chatMessage?.text ?? "",
+              message.chatMessage?.text ?? '',
               new Date(message.chatMessage?.proto.timestamp ?? 0),
               id
             );
@@ -141,12 +113,7 @@ export function useMessenger(chatId: string, chatIdList: string[]) {
   const sendMessage = useCallback(
     async (messageText: string) => {
       await messenger?.sendMessage(messageText, chatId);
-      addNewMessage(
-        messenger?.identity.publicKey ?? new Uint8Array(),
-        messageText,
-        new Date(),
-        chatId
-      );
+      addNewMessage(messenger?.identity.publicKey ?? new Uint8Array(), messageText, new Date(), chatId);
     },
     [chatId, messenger]
   );
