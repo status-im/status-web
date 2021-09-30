740331fa0df034555522c4518306148462840fb8
Maria Rushkova <maria@ethworks.io>
2021-09-30 11:35:03 +0200

    Add mock members data

--- packages/react-chat/src/helpers/channelsMock.ts
+++ packages/react-chat/src/helpers/channelsMock.ts
@@ -5,29 +5,49 @@ export type ChannelData = {
   members: number;
   notifications?: number;
   isMuted?: boolean;
+  membersList?: any[];
 };
 
 export const channels = [
   {
     id: 1,
-    name: "welcome",
-    icon: "https://www.cryptokitties.co/icons/logo.svg",
+    name: 'welcome',
+    icon: 'https://www.cryptokitties.co/icons/logo.svg',
     members: 7,
+    membersList: [
+      {
+        id: 1,
+        name: 'jason.eth',
+        avatar: 'https://unsplash.com/photos/kOuoJJt5YRU',
+        isOnline: true,
+      },
+      {
+        id: 2,
+        name: 'viola.stateofus.eth',
+        avatar: 'https://unsplash.com/photos/1KXHKaaJylM',
+        isOnline: false,
+      },
+      {
+        id: 3,
+        name: 'Guest',
+        isOnline: true,
+      },
+    ],
   },
   {
     id: 2,
-    name: "general",
+    name: 'general',
     members: 15,
   },
   {
     id: 3,
-    name: "beginners",
+    name: 'beginners',
     members: 3,
     notifications: 1,
   },
   {
     id: 4,
-    name: "random",
+    name: 'random',
     members: 6,
     isMuted: true,
   },
