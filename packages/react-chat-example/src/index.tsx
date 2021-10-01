eaa848929b93c8bc6c411e675ca60c5c50d102cb
Maria Rushkova <maria@ethworks.io>
2021-10-01 17:48:22 +0200

    Yarn fix

--- packages/react-chat-example/src/index.tsx
+++ packages/react-chat-example/src/index.tsx
@@ -1,10 +1,10 @@
-import { ReactChat, lightTheme, community } from '@dappconnect/react-chat';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { community, lightTheme, ReactChat } from "@dappconnect/react-chat";
+import React from "react";
+import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  <div style={{ height: '100%' }}>
+  <div style={{ height: "100%" }}>
     <ReactChat theme={lightTheme} community={community} />
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
