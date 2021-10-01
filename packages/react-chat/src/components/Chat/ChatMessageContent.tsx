5dc5bd9e6786e7e6d5031cd2bbb1f9b22e5c3893
Szymon <szszymon95@gmail.com>
2021-10-01 16:47:27 +0200

    Add preview proxy and div

--- packages/react-chat/src/components/Chat/ChatMessageContent.tsx
+++ packages/react-chat/src/components/Chat/ChatMessageContent.tsx
@@ -1,27 +1,31 @@
+import { decode } from "html-entities";
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
+import { Metadata } from "../../models/Metadata";
 import { Theme } from "../../styles/themes";
 
 /* eslint-disable no-useless-escape */
 const regEx =
-    /(?:(?:http|https):\/\/)?(?:[-a-z0-9]+\.)+[a-z]+(?::\d+)?(?:(?:\/[-\+~%/\.\w]+)?\/?(?:[&?][-\+=&;%@\.\w]+)?(?:#[\w]+)?)?/gi;
+  /(?:(?:http|https):\/\/)?(?:[-a-z0-9]+\.)+[a-z]+(?::\d+)?(?:(?:\/[-\+~%/\.\w]+)?\/?(?:[&?][-\+=&;%@\.\w]+)?(?:#[\w]+)?)?/gi;
 /* eslint-enable no-useless-escape */
 
 type ChatMessageContentProps = {
   content: string;
   theme: Theme;
+  fetchMetadata?: (url: string) => Promise<Metadata | undefined>;
 };
 
 export function ChatMessageContent({
   content,
   theme,
+  fetchMetadata,
 }: ChatMessageContentProps) {
   const [elements, setElements] = useState<(string | React.ReactElement)[]>([
     content,
   ]);
-  const [link, setLink] = useState<string | undefined>(undefined)
-  const [ openGraph , setOpenGraph ] = useState<{'og:site_name':string, 'og:title':string, 'og:image':string} | undefined>(undefined)
+  const [link, setLink] = useState<string | undefined>(undefined);
+  const [openGraph, setOpenGraph] = useState<Metadata | undefined>(undefined);
 
   useEffect(() => {
     const split = content.split(regEx);
@@ -46,59 +50,100 @@ export function ChatMessageContent({
           split[idx + 1]
         );
       });
-      const match = matches[0]
+      const match = matches[0];
       const link =
-          match.startsWith("http://") || match.startsWith("https://")
-              ? match
-              : "https://" + match;
-      setLink(link)
+        match.startsWith("http://") || match.startsWith("https://")
+          ? match
+          : "https://" + match;
+      setLink(link);
       setElements(newSplit);
     }
   }, [content]);
 
-    useEffect(() => {
-        const updatePreview = async () => {
-            if (link) {
-                const response = await fetch('https://localhost:3000',
-                    {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify({ 'site': link })
-                    })
-                const body = await response.text()
-                console.log(body)
-                try{
-                    const parsedBody = JSON.parse(body)
-                    if('og:image' in parsedBody && 'og:site_name' in parsedBody && 'og:title' in parsedBody){
-                        setOpenGraph(JSON.parse(body))
-                    }
-                } catch{
-
-                }
-            }
+  useEffect(() => {
+    const updatePreview = async () => {
+      if (link && fetchMetadata) {
+        try {
+          const metadata = await fetchMetadata(link);
+          if (metadata) {
+            setOpenGraph(metadata);
+          }
+        } catch {
+          return;
         }
-        updatePreview()
-    }, [link])
-    if(openGraph){
-        return (
-            <ContentWrapper>
-                <div>{elements.map((el) => el)}</div>
-                <img src={openGraph["og:image"]}/>
-            </ContentWrapper>
-        )
-    } else {
-        return <>{elements.map((el) => el)}</>;
-    }
+      }
+    };
+    updatePreview();
+  }, [link]);
+  if (openGraph) {
+    return (
+      <ContentWrapper>
+        <div>{elements.map((el) => el)}</div>
+        <PreviewWrapper
+          onClick={() => window?.open(link, "_blank", "noopener")?.focus()}
+        >
+          <PreviewImage src={decodeURI(decode(openGraph["og:image"]))} />
+          <PreviewTitleWrapper>{openGraph["og:title"]}</PreviewTitleWrapper>
+          <PreviewSiteNameWrapper>
+            {openGraph["og:site_name"]}
+          </PreviewSiteNameWrapper>
+        </PreviewWrapper>
+      </ContentWrapper>
+    );
+  } else {
+    return <>{elements.map((el) => el)}</>;
+  }
 }
 
+const PreviewSiteNameWrapper = styled.div`
+  font-family: Inter;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: 0.1px;
+  margin-top: 2px;
+  color: #939ba1;
+  margin-left: 12px;
+`;
 
+const PreviewTitleWrapper = styled.div`
+  margin-top: 7px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 13px;
+  width: 290px;
+  line-height: 18px;
+  margin-left: 12px;
+`;
+
+const PreviewImage = styled.img`
+  border-radius: 15px 15px 15px 4px;
+  width: 305px;
+  height: 170px;
+`;
+
+const PreviewWrapper = styled.div`
+  margin-top: 9px;
+  background: #ffffff;
+  width: 305px;
+  height: 224px;
+  border: 1px solid #eef2f5;
+  box-sizing: border-box;
+  border-radius: 16px 16px 16px 4px;
+  display: flex;
+  flex-direction: column;
+  padding: 0px;
+`;
 
 const ContentWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+`;
 
 const Link = styled.a`
   text-decoration: underline;
--- packages/react-chat/src/components/ReactChat.tsx
+++ packages/react-chat/src/components/ReactChat.tsx
@@ -2,6 +2,7 @@ import React, { useRef } from "react";
 
 import { NarrowProvider } from "../contexts/narrowProvider";
 import { CommunityData } from "../helpers/communityMock";
+import { Metadata } from "../models/Metadata";
 import { GlobalStyle } from "../styles/GlobalStyle";
 import { Theme } from "../styles/themes";
 
@@ -10,15 +11,20 @@ import { Chat } from "./Chat";
 interface ReactChatProps {
   theme: Theme;
   community: CommunityData;
+  fetchMetadata?: (url: string) => Promise<Metadata | undefined>;
 }
 
-export function ReactChat({ theme, community }: ReactChatProps) {
+export function ReactChat({ theme, community, fetchMetadata }: ReactChatProps) {
   const ref = useRef<HTMLHeadingElement>(null);
   return (
     <NarrowProvider myRef={ref}>
       <div ref={ref}>
         <GlobalStyle />
-        <Chat theme={theme} community={community} />
+        <Chat
+          theme={theme}
+          community={community}
+          fetchMetadata={fetchMetadata}
+        />
       </div>
     </NarrowProvider>
   );
