8a6be653dfdfde4e233e6118f604de80a4d30b31
Maria Rushkova <maria@ethworks.io>
2021-10-07 09:44:04 +0200

    Delete additional skeleton blocks

--- packages/react-chat/src/components/Skeleton/LoadingSkeleton.tsx
+++ packages/react-chat/src/components/Skeleton/LoadingSkeleton.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 
-import useWindowDimensions from "../../hooks/useWindowDimensions";
 import { Theme } from "../../styles/themes";
 
 import { MessageSkeleton } from "./MessageSkeleton";
@@ -12,55 +11,47 @@ interface LoadingSkeletonProps {
 }
 
 export const LoadingSkeleton = ({ theme }: LoadingSkeletonProps) => {
-  const { height } = useWindowDimensions();
-  const skeletonHeight = (height - 125) / 70;
-  const skeletonBlocksAmount = Math.floor(skeletonHeight);
-
   return (
     <Loading>
-      {height > 950 && (
-        <AdditionalSkeletons>
-          {[...Array(skeletonBlocksAmount)].map((_, index) => (
-            <MessageSkeleton theme={theme} key={index}>
-              <Skeleton theme={theme} />
-            </MessageSkeleton>
-          ))}
-        </AdditionalSkeletons>
-      )}
-
-      <InitialSleletons>
-        {" "}
-        <MessageSkeleton theme={theme}>
-          <Skeleton theme={theme} />
-          <Skeleton theme={theme} width="30%" />
-        </MessageSkeleton>
-        <MessageSkeleton theme={theme}>
-          <Skeleton theme={theme} width="70%" />
-        </MessageSkeleton>
-        <MessageSkeleton theme={theme}>
-          <Skeleton theme={theme} width="40%" />
-          <Skeleton theme={theme} width="25%" />
-          <Skeleton theme={theme} width="30%" />
-        </MessageSkeleton>
-        <MessageSkeleton theme={theme}>
-          <Skeleton theme={theme} width="50%" />
-          <Skeleton
-            theme={theme}
-            width="147px"
-            height="196px"
-            borderRadius="16px"
-          />
-        </MessageSkeleton>
-        <MessageSkeleton theme={theme}>
-          <Skeleton theme={theme} width="50%" />
-        </MessageSkeleton>
-        <MessageSkeleton theme={theme}>
-          <Skeleton theme={theme} width="70%" />
-        </MessageSkeleton>
-        <MessageSkeleton theme={theme}>
-          <Skeleton theme={theme} />
-        </MessageSkeleton>
-      </InitialSleletons>
+      <MessageSkeleton theme={theme}>
+        <Skeleton theme={theme} />
+      </MessageSkeleton>
+      <MessageSkeleton theme={theme}>
+        <Skeleton theme={theme} />
+      </MessageSkeleton>
+      <MessageSkeleton theme={theme}>
+        <Skeleton theme={theme} />
+      </MessageSkeleton>
+      <MessageSkeleton theme={theme}>
+        <Skeleton theme={theme} />
+      </MessageSkeleton>
+      <MessageSkeleton theme={theme}>
+        <Skeleton theme={theme} />
+        <Skeleton theme={theme} width="30%" />
+      </MessageSkeleton>
+      <MessageSkeleton theme={theme}>
+        <Skeleton theme={theme} width="70%" />
+      </MessageSkeleton>
+      <MessageSkeleton theme={theme}>
+        <Skeleton theme={theme} width="40%" />
+        <Skeleton theme={theme} width="25%" />
+        <Skeleton theme={theme} width="30%" />
+      </MessageSkeleton>
+      <MessageSkeleton theme={theme}>
+        <Skeleton theme={theme} width="50%" />
+        <Skeleton
+          theme={theme}
+          width="147px"
+          height="196px"
+          borderRadius="16px"
+        />
+      </MessageSkeleton>
+      <MessageSkeleton theme={theme}>
+        <Skeleton theme={theme} width="50%" />
+      </MessageSkeleton>
+      <MessageSkeleton theme={theme}>
+        <Skeleton theme={theme} width="70%" />
+      </MessageSkeleton>
     </Loading>
   );
 };
@@ -72,14 +63,4 @@ const Loading = styled.div`
   height: calc(100% - 44px);
   padding: 8px 16px 0;
   overflow: auto;
-`;
-
-const InitialSleletons = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const AdditionalSkeletons = styled.div`
-  display: flex;
-  flex-direction: column;
 `;
