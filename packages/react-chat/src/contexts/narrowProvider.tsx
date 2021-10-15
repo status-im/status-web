5dc0bd335b5222768f83049673dc8cf8807c9a11
Maria Rushkova <66270386+mrushkova@users.noreply.github.com>
2021-10-15 16:30:41 +0200

    Add height provider (#75)

--- packages/react-chat/src/contexts/narrowProvider.tsx
+++ packages/react-chat/src/contexts/narrowProvider.tsx
@@ -2,10 +2,19 @@ import React, { createContext, useContext } from "react";
 
 import { useRefBreak } from "../hooks/useRefBreak";
 
-const NarrowContext = createContext<boolean>(false);
+const NarrowContext = createContext<{ narrow: boolean; low: boolean }>({
+  narrow: false,
+  low: false,
+});
 
 export function useNarrow() {
-  return useContext(NarrowContext);
+  const { narrow } = useContext(NarrowContext);
+  return narrow;
+}
+
+export function useLow() {
+  const { low } = useContext(NarrowContext);
+  return low;
 }
 
 interface NarrowProviderProps {
@@ -15,5 +24,6 @@ interface NarrowProviderProps {
 
 export function NarrowProvider({ children, myRef }: NarrowProviderProps) {
   const narrow = useRefBreak(myRef?.current?.offsetWidth ?? 0, 736);
-  return <NarrowContext.Provider value={narrow} children={children} />;
+  const low = useRefBreak(myRef?.current?.offsetHeight ?? 0, 465);
+  return <NarrowContext.Provider value={{ narrow, low }} children={children} />;
 }
