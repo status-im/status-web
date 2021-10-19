5cbc4b90fbd527ba9d060dcdd303e033f5686830
Franck Royer <franck@royer.one>
2021-10-19 11:26:09 +1100

    Fix KDF algorithm

--- packages/status-communities/src/encryption.ts
+++ packages/status-communities/src/encryption.ts
@@ -1,9 +1,15 @@
-import { kdf } from "ecies-geth";
+import pbkdf2 from "pbkdf2";
 
 const AESKeyLength = 32; // bytes
 
 export async function createSymKeyFromPassword(
   password: string
 ): Promise<Uint8Array> {
-  return kdf(Buffer.from(password, "utf-8"), AESKeyLength);
+  return pbkdf2.pbkdf2Sync(
+    Buffer.from(password, "utf-8"),
+    "",
+    65356,
+    AESKeyLength,
+    "sha256"
+  );
 }
