f81f44ab4734a8913694248bc49106529dda9507
Szymon <szszymon95@gmail.com>
2021-11-03 00:38:38 +0100

    Refactor dropdowns

--- packages/react-chat/src/components/Form/ImageMenu.tsx
+++ packages/react-chat/src/components/Form/ImageMenu.tsx
@@ -4,9 +4,10 @@ import styled from "styled-components";
 import { useContextMenu } from "../../hooks/useContextMenu";
 import { copyImg } from "../../utils/copyImg";
 import { downloadImg } from "../../utils/downloadImg";
-import { CopyIcon } from "../Icons/CopyIcon";
-import { DownloadIcon } from "../Icons/DownloadIcon";
-import { textSmallStyles } from "../Text";
+import { CopySvg } from "../Icons/CopyIcon";
+import { DownloadSvg } from "../Icons/DownloadIcon";
+
+import { DropdownMenu, MenuItem, MenuText } from "./DropdownMenu";
 
 interface ImageMenuProps {
   imageId: string;
@@ -16,51 +17,22 @@ export const ImageMenu = ({ imageId }: ImageMenuProps) => {
   const { showMenu } = useContextMenu(imageId);
 
   return showMenu ? (
-    <MenuBlock>
-      <MenuList>
-        <MenuItem onClick={() => copyImg(imageId)}>
-          <CopyIcon /> <MenuText>Copy image</MenuText>
-        </MenuItem>
-        <MenuItem onClick={() => downloadImg(imageId)}>
-          <DownloadIcon />
-          <MenuText> Download image</MenuText>
-        </MenuItem>
-      </MenuList>
-    </MenuBlock>
+    <ImageDropdown>
+      <MenuItem onClick={() => copyImg(imageId)}>
+        <CopySvg height={16} width={16} /> <MenuText>Copy image</MenuText>
+      </MenuItem>
+      <MenuItem onClick={() => downloadImg(imageId)}>
+        <DownloadSvg height={16} width={16} />
+        <MenuText> Download image</MenuText>
+      </MenuItem>
+    </ImageDropdown>
   ) : (
     <></>
   );
 };
 
-const MenuBlock = styled.div`
+const ImageDropdown = styled(DropdownMenu)`
   width: 176px;
-  height: 84px;
-  background: ${({ theme }) => theme.bodyBackgroundColor};
-  box-shadow: 0px 2px 4px rgba(0, 34, 51, 0.16),
-    0px 4px 12px rgba(0, 34, 51, 0.08);
-  border-radius: 8px;
-  padding: 8px 0;
-  position: absolute;
   left: 120px;
   top: 46px;
-  z-index: 2;
-`;
-
-const MenuList = styled.ul`
-  list-style: none;
-`;
-
-const MenuItem = styled.li`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  padding: 8px 14px;
-  cursor: pointer;
-`;
-
-const MenuText = styled.span`
-  margin-left: 6px;
-  color: ${({ theme }) => theme.primary};
-
-  ${textSmallStyles}
 `;
