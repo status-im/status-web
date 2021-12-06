896b487ef56f78265d666b44ed097f8c255b3f72
Maria Rushkova <maria@ethworks.io>
2021-12-06 18:42:58 +0100

    Add Activity provider

--- packages/react-chat/src/models/Activity.ts
+++ packages/react-chat/src/models/Activity.ts
@@ -1,7 +1,16 @@
+import { ChannelData } from "./ChannelData";
+import { ChatMessage } from "./ChatMessage";
+
 export type Activity = {
   id: string;
   type: "mention" | "request" | "reply";
   isRead?: boolean;
+  date: Date;
+  user: string;
+  message?: ChatMessage;
+  channel?: ChannelData;
+  request?: string;
+  status?: "sent" | "accepted" | "declined" | "blocked";
 };
 
 export type Activities = {
