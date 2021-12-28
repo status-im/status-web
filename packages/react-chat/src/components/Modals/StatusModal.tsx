1f58cd796fa31dd57cc177eb33ff53031b9457c0
Maria Rushkova <maria@ethworks.io>
2021-12-28 11:23:15 +0100

    Extract login instructions

--- packages/react-chat/src/components/Modals/StatusModal.tsx
+++ packages/react-chat/src/components/Modals/StatusModal.tsx
@@ -3,11 +3,8 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 import { buttonStyles } from "../Buttons/buttonStyle";
+import { LoginInstructions } from "../Form/LoginInstructions";
 import { PasteInput } from "../Form/PasteInput";
-import { MobileIcon } from "../Icons/MobileIcon";
-import { ProfileSvg } from "../Icons/ProfileIcon";
-import { ScanIcon } from "../Icons/ScanIcon";
-import { textMediumStyles } from "../Text";
 
 import { Modal } from "./Modal";
 import { Heading, Section } from "./ModalStyle";
@@ -55,40 +52,7 @@ export function StatusModal() {
 
         {desktopFlow && <PasteInput label="Paste sync code" />}
 
-        <Instructions>
-          <InstructionStep>
-            Open Status App on your {mobileFlow ? "mobile" : "desktop"}
-          </InstructionStep>
-          <InstructionStep>
-            Navigate yourself to{" "}
-            <InstructionIcon>
-              {" "}
-              <ProfileSvg width={13} height={13} /> <span>Profile</span>
-            </InstructionIcon>{" "}
-            tab
-          </InstructionStep>
-          <InstructionStep>
-            Select{" "}
-            <InstructionIcon>
-              <MobileIcon />
-            </InstructionIcon>{" "}
-            <span>Sync Settings</span>
-          </InstructionStep>
-          <InstructionStep>
-            Tap{" "}
-            <InstructionIcon>
-              {" "}
-              <ScanIcon />{" "}
-            </InstructionIcon>{" "}
-            <span>{mobileFlow ? "Scan" : "Display"} sync code</span>
-          </InstructionStep>
-          <InstructionStep>
-            {mobileFlow
-              ? "Scan the sync code from this screen"
-              : "Paste the sync code above"}{" "}
-            ↑
-          </InstructionStep>
-        </Instructions>
+        <LoginInstructions mobileFlow={mobileFlow} />
       </MiddleSection>
     </Modal>
   );
@@ -125,47 +89,4 @@ const SwitchBtn = styled.button`
 
 const SwitchBtnMobile = styled(SwitchBtn)`
   margin-left: -8px;
-`;
-
-const Instructions = styled.ol`
-  color: ${({ theme }) => theme.secondary};
-  margin: auto 0;
-  list-style-type: decimal;
-  counter-reset: ollist;
-
-  ${textMediumStyles}
-`;
-
-const InstructionStep = styled.li`
-  display: flex;
-  align-items: center;
-
-  & + & {
-    margin-top: 10px;
-  }
-
-  & > span {
-    color: ${({ theme }) => theme.tertiary};
-  }
-
-  &::before {
-    counter-increment: ollist;
-    content: counter(ollist) ".";
-    margin-right: 4px;
-  }
-`;
-
-const InstructionIcon = styled.div`
-  width: 40px;
-  height: 40px;
-  display: inline-flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  border-radius: 50%;
-  background: ${({ theme }) => theme.buttonBg};
-  color: ${({ theme }) => theme.tertiary};
-  font-size: 8px;
-  line-height: 10px;
-  margin: 0 6px;
 `;
