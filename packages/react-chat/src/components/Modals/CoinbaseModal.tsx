9d74510e7aa23267ea518f7e93e052bedd98799b
Maria Rushkova <maria@ethworks.io>
2021-12-29 22:19:17 +0100

    Extract ConnectModal

--- packages/react-chat/src/components/Modals/CoinbaseModal.tsx
+++ packages/react-chat/src/components/Modals/CoinbaseModal.tsx
@@ -1,27 +1,18 @@
-import QRCode from "qrcode.react";
 import React from "react";
 
-import { CopyInput } from "../Form/CopyInput";
-
+import { ConnectModal } from "./ConnectModal";
 import { Modal } from "./Modal";
-import { Heading, MiddleSection, QRWrapper, Section, Text } from "./ModalStyle";
 
 export const CoinbaseModalName = "CoinbaseModal";
 
 export function CoinbaseModal() {
   return (
     <Modal name={CoinbaseModalName}>
-      <Section>
-        <Heading>Connect with Coinbase Wallet</Heading>
-      </Section>
-      <MiddleSection>
-        <Text>Scan QR code or copy and pase it in your Coinbase Wallet.</Text>
-        <QRWrapper>
-          {" "}
-          <QRCode value="https://www.coinbase.com/wallet" size={224} />
-        </QRWrapper>
-        <CopyInput value="2Ef1907d50926...6dt4cEbd975aC5E0Ba" />
-      </MiddleSection>
+      <ConnectModal
+        name="Coinbase Wallet"
+        text="Scan QR code or copy and pase it in your Coinbase Wallet."
+        address="https://www.coinbase.com/wallet"
+      />
     </Modal>
   );
 }
--- packages/react-chat/src/components/Modals/WalletConnectModal.tsx
+++ packages/react-chat/src/components/Modals/WalletConnectModal.tsx
@@ -1,30 +1,19 @@
-import QRCode from "qrcode.react";
 import React from "react";
 
-import { CopyInput } from "../Form/CopyInput";
-
+import { ConnectModal } from "./ConnectModal";
 import { Modal } from "./Modal";
-import { Heading, MiddleSection, QRWrapper, Section, Text } from "./ModalStyle";
 
 export const WalleConnectModalName = "WalleConnectModal";
 
 export function WalleConnectModal() {
   return (
     <Modal name={WalleConnectModalName}>
-      <Section>
-        <Heading>Connect with WalletConnect</Heading>
-      </Section>
-      <MiddleSection>
-        <Text>
-          Scan QR code with a WallectConnect-compatible wallet or copy code and
-          paste it in your hardware wallet.
-        </Text>
-        <QRWrapper>
-          {" "}
-          <QRCode value="https://walletconnect.com/" size={224} />
-        </QRWrapper>
-        <CopyInput value="2Ef1907d50926...6dt4cEbd975aC5E0Ba" />
-      </MiddleSection>
+      <ConnectModal
+        name="WalletConnect"
+        text="Scan QR code with a WallectConnect-compatible wallet or copy code and
+          paste it in your hardware wallet."
+        address="https://walletconnect.com/"
+      />
     </Modal>
   );
 }
