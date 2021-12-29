8e4d404bda4dc68e9dc6fbaa19e53bb5261aeeb6
Maria Rushkova <maria@ethworks.io>
2021-12-29 16:28:00 +0100

    Fix coinbase logo

--- packages/react-chat/src/components/Icons/CoinbaseLogo.tsx
+++ packages/react-chat/src/components/Icons/CoinbaseLogo.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export const CoinbaseLogo = () => {
   return (
     <svg
-      width="32"
-      height="32"
+      width="40"
+      height="40"
       viewBox="0 0 32 32"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
