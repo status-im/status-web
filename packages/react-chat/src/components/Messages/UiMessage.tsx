b7f1a793854891066fcd2a4e9127da280f286828
Maria Rushkova <maria@ethworks.io>
2022-01-03 18:05:09 +0100

    Extract Reactions

--- packages/react-chat/src/components/Messages/UiMessage.tsx
+++ packages/react-chat/src/components/Messages/UiMessage.tsx
@@ -12,10 +12,8 @@ import { equalDate } from "../../utils";
 import { ChatMessageContent } from "../Chat/ChatMessageContent";
 import { ContactMenu } from "../Form/ContactMenu";
 import { ReactionPicker } from "../Form/ReactionPicker";
-import { Tooltip } from "../Form/Tooltip";
+import { Reactions } from "../Form/Reactions";
 import { Icon } from "../Icons/Icon";
-import { ReactionSvg } from "../Icons/ReactionIcon";
-import { ReplySvg } from "../Icons/ReplyIcon";
 import { UntrustworthIcon } from "../Icons/UntrustworthIcon";
 import { UserLogo } from "../Members/UserLogo";
 
@@ -151,25 +149,12 @@ export function UiMessage({
           </ContentWrapper>
         </UserMessageWrapper>
         {showReactions && <ReactionPicker />}
-        <Reactions>
-          <ReactionBtn onClick={() => setShowReactions(!showReactions)}>
-            <ReactionSvg width={22} height={22} />
-            <Tooltip tip="Add reaction" />
-          </ReactionBtn>
-          <ReactionBtn
-            onClick={() =>
-              setReply({
-                sender: message.sender,
-                content: message.content,
-                image: message.image,
-                id: message.id,
-              })
-            }
-          >
-            <ReplySvg width={22} height={22} />
-            <Tooltip tip="Reply" />
-          </ReactionBtn>
-        </Reactions>
+        <Reactions
+          message={message}
+          setReply={setReply}
+          showReactions={showReactions}
+          setShowReactions={setShowReactions}
+        />
       </MessageWrapper>
     </MessageOuterWrapper>
   );
@@ -178,39 +163,4 @@ export function UiMessage({
 const UserMessageWrapper = styled.div`
   width: 100%;
   display: flex;
-`;
-
-const Reactions = styled.div`
-  display: flex;
-  position: absolute;
-  right: 20px;
-  top: -18px;
-  box-shadow: 0px 4px 12px rgba(0, 34, 51, 0.08);
-  border-radius: 8px;
-  background: ${({ theme }) => theme.bodyBackgroundColor};
-  padding: 2px;
-  visibility: hidden;
-`;
-
-const ReactionBtn = styled.button`
-  width: 32px;
-  height: 32px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 8px;
-  align-self: center;
-  position: relative;
-
-  &:hover {
-    background: ${({ theme }) => theme.buttonBgHover};
-  }
-
-  &:hover > svg {
-    fill: ${({ theme }) => theme.tertiary};
-  }
-
-  &:hover > div {
-    visibility: visible;
-  }
 `;
