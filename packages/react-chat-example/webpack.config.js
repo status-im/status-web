964d468821c2b8207c54adc9b75bd569c38cbc83
Szymon Szlachtowicz <38212223+Szymx95@users.noreply.github.com>
2022-01-11 09:27:32 +0100

    Fix webpack build warning (#181)

--- packages/react-chat-example/webpack.config.js
+++ packages/react-chat-example/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = env => {
         buffer: require.resolve('buffer/'),
         crypto: require.resolve('crypto-browserify'),
         stream: require.resolve('stream-browserify'),
-        assert: require.resolve('assert'),
+        assert: require.resolve('assert/'),
         http: require.resolve('stream-http'),
         https: require.resolve('https-browserify'),
         zlib: require.resolve('browserify-zlib')
