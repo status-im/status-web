7ca3a2e89df55019fd022ecfc4d20b093bdcf75c
Maria Rushkova <maria@ethworks.io>
2022-01-13 16:42:57 +0100

    Add CreationStartModal

--- packages/react-chat/src/components/UserCreation/UserCreationButtons.tsx
+++ packages/react-chat/src/components/UserCreation/UserCreationButtons.tsx
@@ -2,11 +2,13 @@ import React, { useMemo } from "react";
 import styled from "styled-components";
 
 import { useModal } from "../../contexts/modalProvider";
+import { useNarrow } from "../../contexts/narrowProvider";
 import { loadEncryptedIdentity } from "../../utils";
 import { buttonStyles, buttonTransparentStyles } from "../Buttons/buttonStyle";
 import { ProfileFoundModalName } from "../Modals/ProfileFoundModal";
 import { StatusModalName } from "../Modals/StatusModal";
 import { UserCreationModalName } from "../Modals/UserCreationModal";
+import { UserCreationStartModalName } from "../Modals/UserCreationStartModal";
 import { WalletModalName } from "../Modals/WalletModal";
 import { textSmallStyles } from "../Text";
 
@@ -15,26 +17,43 @@ interface UserCreationProps {
 }
 
 export function UserCreationButtons({ permission }: UserCreationProps) {
+  const narrow = useNarrow();
   const { setModal } = useModal(UserCreationModalName);
   const { setModal: setStatusModal } = useModal(StatusModalName);
   const { setModal: setWalletModal } = useModal(WalletModalName);
   const { setModal: setProfileFoundModal } = useModal(ProfileFoundModalName);
+  const { setModal: setCreationStartModal } = useModal(
+    UserCreationStartModalName
+  );
 
   const encryptedIdentity = useMemo(() => loadEncryptedIdentity(), []);
 
   return (
     <Wrapper>
-      <LoginBtn onClick={() => setStatusModal(true)}>
+      <LoginBtn
+        onClick={() => {
+          setStatusModal(true);
+          setCreationStartModal(false);
+        }}
+        className={`${narrow && "narrow"}`}
+      >
         Sync with Status profile
       </LoginBtn>
-      <LoginBtn onClick={() => setWalletModal(true)}>
+      <LoginBtn
+        onClick={() => {
+          setWalletModal(true);
+          setCreationStartModal(false);
+        }}
+        className={`${narrow && "narrow"}`}
+      >
         Connect Ethereum Wallet
       </LoginBtn>
       {permission && (
         <ThrowAwayButton
-          onClick={() =>
-            encryptedIdentity ? setProfileFoundModal(true) : setModal(true)
-          }
+          onClick={() => {
+            encryptedIdentity ? setProfileFoundModal(true) : setModal(true);
+            setCreationStartModal(false);
+          }}
         >
           Use a throwaway account
         </ThrowAwayButton>
@@ -55,6 +74,10 @@ const LoginBtn = styled.button`
   ${textSmallStyles}
   padding: 10px 12px;
   margin-bottom: 16px;
+
+  &.narrow {
+    margin-bottom: 32px;
+  }
 `;
 
 const ThrowAwayButton = styled.button`
