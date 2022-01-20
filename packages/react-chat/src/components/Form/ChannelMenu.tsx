bdd27d3850ccd8e4cfefa7787b34e558440f0ee4
Maria Rushkova <maria@ethworks.io>
2022-01-20 17:10:37 +0100

    Add menu items to channel menu

--- packages/react-chat/src/components/Form/ChannelMenu.tsx
+++ packages/react-chat/src/components/Form/ChannelMenu.tsx
@@ -5,15 +5,18 @@ import { useMessengerContext } from "../../contexts/messengerProvider";
 import { useModal } from "../../contexts/modalProvider";
 import { useNarrow } from "../../contexts/narrowProvider";
 import { ChannelData } from "../../models/ChannelData";
-import { AddMemberIconSvg } from "../Icons/AddMemberIcon";
-import { CheckSvg } from "../Icons/CheckIcon";
+import { AddMemberIcon } from "../Icons/AddMemberIcon";
+import { CheckIcon } from "../Icons/CheckIcon";
 import { DeleteIcon } from "../Icons/DeleteIcon";
-import { EditSvg } from "../Icons/EditIcon";
-import { LeftIconSvg } from "../Icons/LeftIcon";
-import { MembersSmallSvg } from "../Icons/MembersSmallIcon";
-import { MuteSvg } from "../Icons/MuteIcon";
+import { DownloadIcon } from "../Icons/DownloadIcon";
+import { EditIcon } from "../Icons/EditIcon";
+import { LeftIcon } from "../Icons/LeftIcon";
+import { MembersSmallIcon } from "../Icons/MembersSmallIcon";
+import { MuteIcon } from "../Icons/MuteIcon";
+import { ProfileIcon } from "../Icons/ProfileIcon";
 import { EditModalName } from "../Modals/EditModal";
 import { LeavingModalName } from "../Modals/LeavingModal";
+import { ProfileModalName } from "../Modals/ProfileModal";
 
 import { DropdownMenu, MenuItem, MenuText } from "./DropdownMenu";
 
@@ -34,6 +37,7 @@ export const ChannelMenu = ({
   const { clearNotifications } = useMessengerContext();
   const { setModal } = useModal(EditModalName);
   const { setModal: setLeavingModal } = useModal(LeavingModalName);
+  const { setModal: setProfileModal } = useModal(ProfileModalName);
 
   return (
     <DropdownMenu closeMenu={setShowChannelMenu}>
@@ -44,7 +48,7 @@ export const ChannelMenu = ({
             setShowChannelMenu(false);
           }}
         >
-          <MembersSmallSvg width={16} height={16} />
+          <MembersSmallIcon width={16} height={16} />
           <MenuText>View Members</MenuText>
         </MenuItem>
       )}
@@ -56,58 +60,83 @@ export const ChannelMenu = ({
               setShowChannelMenu(false);
             }}
           >
-            <AddMemberIconSvg width={16} height={16} />
+            <AddMemberIcon width={16} height={16} />
             <MenuText>Add / remove from group</MenuText>
           </MenuItem>
           <MenuItem onClick={() => setModal(true)}>
-            <EditSvg width={16} height={16} />
+            <EditIcon width={16} height={16} />
             <MenuText>Edit name and image</MenuText>
           </MenuItem>
         </>
       )}
-      <MenuItem
-        onClick={() => {
-          channel.isMuted = !channel.isMuted;
-          setShowChannelMenu(false);
-        }}
-      >
-        <MuteSvg width={16} height={16} />
-        <MenuText>
-          {(channel.isMuted ? "Unmute" : "Mute") +
-            (channel.type === "group" ? " Group" : " Chat")}
-        </MenuText>
-      </MenuItem>
-      <MenuItem onClick={() => clearNotifications(channel.id)}>
-        <CheckSvg width={16} height={16} />
-        <MenuText>Mark as Read</MenuText>
-      </MenuItem>
+      {channel.type === "dm" && (
+        <MenuItem
+          onClick={() => {
+            setProfileModal({
+              id: channel.name,
+              renamingState: false,
+              requestState: false,
+            });
+            setShowChannelMenu(false);
+          }}
+        >
+          <ProfileIcon width={16} height={16} />
+          <MenuText>View Profile</MenuText>
+        </MenuItem>
+      )}
+      <MenuSection className={`${channel.type === "channel" && "channel"}`}>
+        <MenuItem
+          onClick={() => {
+            channel.isMuted = !channel.isMuted;
+            setShowChannelMenu(false);
+          }}
+        >
+          <MuteIcon width={16} height={16} />
+          <MenuText>
+            {(channel.isMuted ? "Unmute" : "Mute") +
+              (channel.type === "group" ? " Group" : " Chat")}
+          </MenuText>
+        </MenuItem>
+        <MenuItem onClick={() => clearNotifications(channel.id)}>
+          <CheckIcon width={16} height={16} />
+          <MenuText>Mark as Read</MenuText>
+        </MenuItem>
+        <MenuItem>
+          <DownloadIcon width={16} height={16} />
+          <MenuText>Fetch Messages</MenuText>
+        </MenuItem>
+      </MenuSection>
       {(channel.type === "group" || channel.type === "dm") && (
-        <MenuSection>
-          {" "}
-          <MenuItem
-            onClick={() => {
-              setLeavingModal(true);
-              setShowChannelMenu(false);
-            }}
-          >
-            {channel.type === "group" && (
-              <LeftIconSvg width={16} height={16} className="red" />
-            )}
-            {channel.type === "dm" && (
-              <DeleteIcon width={16} height={16} className="red" />
-            )}
-            <MenuText className="red">
-              {channel.type === "group" ? "Leave Group" : "Delete Chat"}
-            </MenuText>
-          </MenuItem>
-        </MenuSection>
+        <MenuItem
+          onClick={() => {
+            setLeavingModal(true);
+            setShowChannelMenu(false);
+          }}
+        >
+          {channel.type === "group" && (
+            <LeftIcon width={16} height={16} className="red" />
+          )}
+          {channel.type === "dm" && (
+            <DeleteIcon width={16} height={16} className="red" />
+          )}
+          <MenuText className="red">
+            {channel.type === "group" ? "Leave Group" : "Delete Chat"}
+          </MenuText>
+        </MenuItem>
       )}
     </DropdownMenu>
   );
 };
 
 const MenuSection = styled.div`
-  margin-top: 5px;
-  padding-top: 5px;
+  padding: 4px 0;
+  margin: 4px 0;
   border-top: 1px solid ${({ theme }) => theme.inputColor};
+  border-bottom: 1px solid ${({ theme }) => theme.inputColor};
+
+  &.channel {
+    padding: 0;
+    margin: 0;
+    border: none;
+  }
 `;
