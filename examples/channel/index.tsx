3334108bddc723c81e52ffbc7fd81048bec3971b
Pavel <14926950+prichodko@users.noreply.github.com>
2022-02-23 15:32:08 +0100

    Move examples and migrate to Parcel (#224)
    * Remove old examples
    
    * Add Community and Channel examples

---
+++ examples/channel/index.tsx
@@ -0,0 +1,25 @@
+import React, { render } from 'react-dom';
+import { GroupChat, lightTheme } from '@status-im/react';
+import { StrictMode } from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <GroupChat
+        theme={lightTheme}
+        communityKey="<YOUR_COMMUNITY_KEY>"
+        config={{
+          environment: '<ENVIRONMENT>',
+          dappUrl: '<URL>',
+        }}
+      />
+    </div>
+  );
+};
+
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root')
+);
