9b76ef6db29e9e1618293e0b218402b90515f2bf
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-03-16 12:42:30 +0100

    feat(react): add MemberSidebar component

---
+++ packages/status-react/src/components/member-sidebar/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+import { styled } from '~/src/styles/config'
+import { Grid } from '~/src/system'
+import { Heading } from '~/src/system/heading'
+
+import { MemberGroup } from './member-group'
+import { MemberItem } from './member-item'
+import { UserItem } from './user-item'
+
+export function MemberSidebar() {
+  return (
+    <Wrapper>
+      <Heading size="15" css={{ marginBottom: '$3' }}>
+        Members
+      </Heading>
+      <Grid gap="2">
+        <MemberGroup label="You">
+          <UserItem />
+        </MemberGroup>
+        <MemberGroup label="Online">
+          <MemberItem verified={true} untrustworthy={false} indicator="online">
+            pvl.eth
+          </MemberItem>
+          <MemberItem verified={false} untrustworthy={false} indicator="online">
+            carmen
+          </MemberItem>
+          <MemberItem verified={false} untrustworthy={false} indicator="online">
+            carmen
+          </MemberItem>
+        </MemberGroup>
+        <MemberGroup label="Offline">
+          <MemberItem verified={false} untrustworthy indicator="offline">
+            mark
+          </MemberItem>
+          <MemberItem
+            verified={false}
+            untrustworthy={false}
+            indicator="offline"
+          >
+            mark
+          </MemberItem>
+        </MemberGroup>
+      </Grid>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled('div', {
+  width: 256,
+  flexShrink: 0,
+  padding: '18px 16px',
+  display: 'none',
+  backgroundColor: '#F6F8FA',
+  overflowY: 'scroll',
+
+  '@medium': {
+    display: 'block',
+  },
+})
