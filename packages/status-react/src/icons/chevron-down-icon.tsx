d910d44defdf8fca4ab213a7b0c515220522bedf
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-03-18 19:12:50 +0100

    refactor(react): improve icons

---
+++ packages/status-react/src/icons/chevron-down-icon.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const ChevronDownIcon = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      width="10"
+      height="7"
+      viewBox="0 0 10 7"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M0.587087 1.08709C0.953204 0.720971 1.5468 0.720971 1.91291 1.08709L4.64645 3.82062C4.84171 4.01588 5.15829 4.01588 5.35355 3.82062L8.08709 1.08709C8.4532 0.720971 9.0468 0.720971 9.41291 1.08709C9.77903 1.4532 9.77903 2.0468 9.41291 2.41291L5.66291 6.16291C5.2968 6.52903 4.7032 6.52903 4.33709 6.16291L0.587087 2.41291C0.220971 2.0468 0.220971 1.4532 0.587087 1.08709Z"
+        fill="currentColor"
+      />
+    </svg>
+  )
+}
