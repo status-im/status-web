b771115011302078b2e3cd475d6421c40aaff0e6
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-04-11 21:00:10 +0200

    feat(react): unify chat menu

--- packages/status-react/src/components/main-sidebar/components/messages/chat-item.tsx
+++ packages/status-react/src/components/main-sidebar/components/messages/chat-item.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
-import { BellIcon } from '~/src/icons/bell-icon'
-import { ContextMenu, ContextMenuTrigger } from '~/src/system'
+import { ChatMenu } from '~/src/components/chat-menu'
+import { useChat } from '~/src/protocol/use-chat'
+import { ContextMenuTrigger } from '~/src/system'
 
 import { SidebarItem } from '../sidebar-item'
 
@@ -14,24 +15,12 @@ interface Props extends SidebarItemProps {
 export const ChatItem = (props: Props) => {
   const { children, ...sidebarItemProps } = props
 
+  const chat = useChat(children)
+
   return (
     <ContextMenuTrigger>
       <SidebarItem {...sidebarItemProps}>{children}</SidebarItem>
-      <ContextMenu>
-        <ContextMenu.Item icon={<BellIcon />}>View Profile</ContextMenu.Item>
-        <ContextMenu.Separator />
-        <ContextMenu.TriggerItem label="Mute Chat" icon={<BellIcon />}>
-          <ContextMenu.Item>For 15 min</ContextMenu.Item>
-          <ContextMenu.Item>For 1 hour</ContextMenu.Item>
-          <ContextMenu.Item>For 8 hours</ContextMenu.Item>
-          <ContextMenu.Item>For 24 hours</ContextMenu.Item>
-          <ContextMenu.Item>Until I turn it back on</ContextMenu.Item>
-        </ContextMenu.TriggerItem>
-        <ContextMenu.Separator />
-        <ContextMenu.Item icon={<BellIcon />} danger>
-          Delete
-        </ContextMenu.Item>
-      </ContextMenu>
+      <ChatMenu type="context" chatType={chat.type} />
     </ContextMenuTrigger>
   )
 }
