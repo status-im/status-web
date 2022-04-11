03c4b961769393bf29f6701761c1284fccd0bf01
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-04-11 21:01:29 +0200

    feat(system): add more control to Popover

--- packages/status-react/src/system/popover/styles.tsx
+++ packages/status-react/src/system/popover/styles.tsx
@@ -6,7 +6,6 @@ export type Variants = VariantProps<typeof Content>
 
 export const Content = styled('div', {
   backgroundColor: 'white',
-  padding: '$4',
   borderRadius: 8,
   boxShadow:
     'hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px',
