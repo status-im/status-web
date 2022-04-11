bbd2f19b89e5dbc58d56c931e3c10311ad5a2127
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-04-11 21:05:08 +0200

    feat(react): add static protocol layer base

---
+++ packages/status-react/src/protocol/provider.tsx
@@ -0,0 +1,37 @@
+import React, { createContext, useContext, useMemo } from 'react'
+
+import type { Config } from '~/src/types/config'
+// import { createClient } from '@status-im/core'
+// import type { Client } from '@status-im/core'
+
+interface ClientContext {
+  client: Config
+}
+
+const Context = createContext<ClientContext | undefined>(undefined)
+
+export function useClient() {
+  const context = useContext(Context)
+
+  if (!context) {
+    throw new Error(`useClient must be used within a ClientProvider`)
+  }
+
+  return context
+}
+
+interface ClientProviderProps {
+  config: Config
+  children: React.ReactNode
+}
+
+export const ClientProvider = (props: ClientProviderProps) => {
+  const { config, children } = props
+
+  const client = useMemo(() => {
+    // return createClient({ ...config })
+    return { client: config }
+  }, [config])
+
+  return <Context.Provider value={client}>{children}</Context.Provider>
+}
---
+++ packages/status-react/src/protocol/use-community.tsx
@@ -0,0 +1,11 @@
+export const useCommunity = () => {
+  return {
+    name: 'CryptoKitties',
+    description: 'A community of cat lovers, meow!',
+    publicKey: '0x2Ef1907d50926...6cEbd975aC5E0Ba',
+    imageUrl:
+      'https://images.unsplash.com/photo-1592194996308-7b43878e84a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+    membersCount: 182,
+    requestNeeded: true,
+  }
+}
