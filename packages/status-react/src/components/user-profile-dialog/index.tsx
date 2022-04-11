cd21b93fc28f2fa89b7290cee9a7319f14aafb5e
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-04-11 20:41:18 +0200

    feat(react): add user profile dialog

---
+++ packages/status-react/src/components/user-profile-dialog/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { Avatar, Dialog, EmojiHash, Heading, Text } from '~/src/system'
+
+interface Props {
+  contact: string
+}
+
+// TODO: Add all states of contact, wait for desktop release
+export const UserProfileDialog = (props: Props) => {
+  const { contact } = props
+
+  return (
+    <Dialog title={`${contact}'s Profile`} size="640">
+      <Dialog.Body align="center">
+        <Avatar size="80" />
+        <Heading size="22">{contact}</Heading>
+        <Text>Chatkey:0x63FaC9201494f0bd17B9892B9fae4d52fe3BD377</Text>
+        <EmojiHash />
+      </Dialog.Body>
+      <Dialog.Actions>
+        <Dialog.Action variant="danger">Remove Contact</Dialog.Action>
+        <Dialog.Action variant="danger">Mark Untrustworthy</Dialog.Action>
+        <Dialog.Action>Send Contact Request</Dialog.Action>
+      </Dialog.Actions>
+    </Dialog>
+  )
+}
