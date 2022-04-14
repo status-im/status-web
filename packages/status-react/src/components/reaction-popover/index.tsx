688fd70d81bc5780785e98318cf282d271e57bf5
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-04-14 12:37:59 +0200

    feat(react): implement message reactions

--- packages/status-react/src/components/reaction-popover/index.tsx
+++ packages/status-react/src/components/reaction-popover/index.tsx
@@ -3,63 +3,64 @@ import React from 'react'
 import { styled } from '~/src/styles/config'
 import { Flex, Image, Popover, PopoverTrigger } from '~/src/system'
 
+import type { Reaction, Reactions } from '~/src/protocol/use-messages'
+
 interface Props {
   children: React.ReactElement
-  onClick: (emoji: string) => void
+  reactions: Reactions
+  onClick: (reaction: Reaction) => void
   open?: boolean
   onOpenChange?: (open: boolean) => void
 }
 
+export const emojis: Record<Reaction, { url: string; symbol: string }> = {
+  heart: {
+    symbol: '❤️',
+    url: 'https://twemoji.maxcdn.com/v/latest/svg/2764.svg',
+  },
+  'thumbs-up': {
+    symbol: '👍️',
+    url: 'https://twemoji.maxcdn.com/v/latest/svg/1f44d.svg',
+  },
+  'thumbs-down': {
+    symbol: '👎️',
+    url: 'https://twemoji.maxcdn.com/v/latest/svg/1f44e.svg',
+  },
+  smile: {
+    symbol: '😆',
+    url: 'https://twemoji.maxcdn.com/v/latest/svg/1f606.svg',
+  },
+  sad: {
+    symbol: '😭',
+    url: 'https://twemoji.maxcdn.com/v/latest/svg/1f62d.svg',
+  },
+  angry: {
+    symbol: '😡',
+    url: 'https://twemoji.maxcdn.com/v/latest/svg/1f621.svg',
+  },
+}
+
 export const ReactionPopover = (props: Props) => {
-  const { children, onClick, ...popoverProps } = props
+  const { reactions, children, onClick, ...popoverProps } = props
 
   return (
     <PopoverTrigger {...popoverProps}>
       {children}
       <Popover side="top" align="center" sideOffset={6}>
         <Flex gap={1} css={{ padding: 8 }}>
-          <Button onClick={() => onClick('')} active={false}>
-            <Image
-              width={30}
-              src="https://twemoji.maxcdn.com/v/latest/svg/2764.svg"
-              alt="React with ❤️"
-            />
-          </Button>
-          <Button onClick={() => onClick('')} active>
-            <Image
-              width={30}
-              src="https://twemoji.maxcdn.com/v/latest/svg/1f44d.svg"
-              alt="React with 👍️"
-            />
-          </Button>
-          <Button onClick={() => onClick('')} active>
-            <Image
-              width={30}
-              src="https://twemoji.maxcdn.com/v/latest/svg/1f44e.svg"
-              alt="React with 👎️"
-            />
-          </Button>
-          <Button onClick={() => onClick('')} active={false}>
-            <Image
-              width={30}
-              src="https://twemoji.maxcdn.com/v/latest/svg/1f606.svg"
-              alt="React with 😆"
-            />
-          </Button>
-          <Button onClick={() => onClick('')} active={false}>
-            <Image
-              width={30}
-              src="https://twemoji.maxcdn.com/v/latest/svg/1f62d.svg"
-              alt="React with 😭"
-            />
-          </Button>
-          <Button onClick={() => onClick('')} active={false}>
-            <Image
-              width={30}
-              src="https://twemoji.maxcdn.com/v/latest/svg/1f621.svg"
-              alt="React with 😡"
-            />
-          </Button>
+          {Object.entries(reactions).map(([reaction, value]) => {
+            const emoji = emojis[reaction as Reaction]
+            return (
+              <Button
+                key={reaction}
+                onClick={() => onClick(reaction as Reaction)}
+                active={value.me}
+                aria-label={`React with ${emoji.symbol}`}
+              >
+                <Image width={30} src={emoji.url} alt={emoji.symbol} />
+              </Button>
+            )
+          })}
         </Flex>
       </Popover>
     </PopoverTrigger>
