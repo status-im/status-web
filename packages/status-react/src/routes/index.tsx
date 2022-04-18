2794358e34cc06118035785a3c66411a8f816103
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-04-18 16:18:16 +0200

    feat(react): simplify Community component structure

--- packages/status-react/src/routes/index.tsx
+++ packages/status-react/src/routes/index.tsx
@@ -2,43 +2,37 @@ import React from 'react'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+import { MainSidebar } from '~/src/components/main-sidebar'
 import { AppProvider } from '~/src/contexts/app-context'
 import { DialogProvider } from '~/src/contexts/dialog-context'
+import { ThemeProvider } from '~/src/contexts/theme-context'
+import { Chat } from '~/src/routes/chat'
+import { NewChat } from '~/src/routes/new-chat'
 import { styled } from '~/src/styles/config'
-
-import { MainSidebar } from '../components/main-sidebar'
-import { Box } from '../system'
-import { Chat } from './chat'
-import { NewChat } from './new-chat'
+import { GlobalStyle } from '~/src/styles/GlobalStyle'
 
 import type { Config } from '~/src/types/config'
 
 type Props = Config
 
 export const Community = (props: Props) => {
-  const {
-    // theme,
-    // environment,
-    // publicKey,
-    router: Router = BrowserRouter,
-  } = props
-
-  const { options } = props
+  const { theme, router: Router = BrowserRouter } = props
 
   return (
     <Router>
       <AppProvider config={props}>
-        <DialogProvider>
-          <Box css={{ flex: '1 0 100%' }}>
+        <ThemeProvider theme={theme}>
+          <DialogProvider>
+            <GlobalStyle />
             <Wrapper>
-              {options.enableMembers && <MainSidebar />}
+              <MainSidebar />
               <Routes>
                 <Route path="/:id" element={<Chat />} />
                 <Route path="/new" element={<NewChat />} />
               </Routes>
             </Wrapper>
-          </Box>
-        </DialogProvider>
+          </DialogProvider>
+        </ThemeProvider>
       </AppProvider>
     </Router>
   )
@@ -47,6 +41,7 @@ export const Community = (props: Props) => {
 export type { Props as CommunityProps }
 
 const Wrapper = styled('div', {
+  overflow: 'hidden',
   position: 'relative',
   width: '100%',
   height: '100%',
