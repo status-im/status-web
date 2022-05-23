01c861565b2ed27e0a5a20ab51117bd056e360a7
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-05-23 14:41:33 +0200

    fix(react): use color token in popver

--- packages/status-react/src/system/popover/styles.tsx
+++ packages/status-react/src/system/popover/styles.tsx
@@ -5,7 +5,7 @@ import type { VariantProps } from '~/src/styles/config'
 export type Variants = VariantProps<typeof Content>
 
 export const Content = styled('div', {
-  backgroundColor: 'white',
+  backgroundColor: '$accent-11',
   borderRadius: 8,
   boxShadow:
     'hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px',
