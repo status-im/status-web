7265ec260c28a6beffcd8bb0c3a3cf58be236328
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-05-23 14:42:28 +0200

    feat(react): add user profile dialog to chat message

--- packages/status-react/src/components/user-profile-dialog/index.tsx
+++ packages/status-react/src/components/user-profile-dialog/index.tsx
@@ -3,18 +3,18 @@ import React from 'react'
 import { Avatar, Dialog, EmojiHash, Heading, Text } from '~/src/system'
 
 interface Props {
-  contact: string
+  name: string
 }
 
 // TODO: Add all states of contact, wait for desktop release
 export const UserProfileDialog = (props: Props) => {
-  const { contact, ...dialogProps } = props
+  const { name, ...dialogProps } = props
 
   return (
-    <Dialog title={`${contact}'s Profile`} size="640" {...dialogProps}>
+    <Dialog title={`${name}'s Profile`} size="640" {...dialogProps}>
       <Dialog.Body align="center">
         <Avatar size="80" />
-        <Heading size="22">{contact}</Heading>
+        <Heading size="22">{name}</Heading>
         <Text>Chatkey:0x63FaC9201494f0bd17B9892B9fae4d52fe3BD377</Text>
         <EmojiHash />
       </Dialog.Body>
