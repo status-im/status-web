8bbe72d6994075a465dc3e192f80c73764af0464
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-05-23 14:40:03 +0200

    style(react): update static data

--- packages/status-react/src/protocol/use-chat.tsx
+++ packages/status-react/src/protocol/use-chat.tsx
@@ -15,6 +15,16 @@ const chats: Record<string, Chat> = {
     imageUrl:
       'https://images.unsplash.com/photo-1599420186946-7b6fb4e297f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80',
   },
+  pm: {
+    type: 'channel',
+    imageUrl:
+      'https://images.unsplash.com/photo-1599420186946-7b6fb4e297f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80',
+  },
+  watercooler: {
+    type: 'channel',
+    imageUrl:
+      'https://images.unsplash.com/photo-1599420186946-7b6fb4e297f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80',
+  },
   'vitalik.eth': { type: 'chat', imageUrl: '' },
   'pvl.eth': { type: 'chat', imageUrl: '' },
   'Climate Change': { type: 'group-chat', imageUrl: '' },
