b2a01da88730fadfcb121ed1995f8d4ce933a555
Felicio Mununga <felicio@users.noreply.github.com>
2022-05-30 16:05:22 +0200

    convert test files

--- packages/status-js/src/community.spec.ts
+++ packages/status-js/src/community.spec.ts
@@ -1,4 +1,3 @@
-import { expect } from 'chai'
 import { Waku } from 'js-waku'
 
 import { Community } from './community'
@@ -6,16 +5,7 @@ import { Community } from './community'
 import type { CommunityDescription } from './wire/community_description'
 
 describe('Community [live data]', () => {
-  before(function () {
-    if (process.env.CI) {
-      // Skip live data test in CI
-      this.skip()
-    }
-  })
-
-  it('Retrieves community description For DappConnect Test from Waku prod fleet', async function () {
-    this.timeout(20000)
-    // FIXME?: mock
+  test('Retrieves community description For DappConnect Test from Waku prod fleet', async () => {
     const waku = await Waku.create({ bootstrap: { default: true } })
 
     await waku.waitForRemotePeer()
@@ -25,20 +15,20 @@ describe('Community [live data]', () => {
       waku
     )
     const desc = community.description as CommunityDescription
-    expect(desc).to.not.be.undefined
+    expect(desc).toBeDefined()
 
-    expect(desc.identity?.displayName).to.eq('Test Community')
+    expect(desc.identity?.displayName).toEqual('Test Community')
 
     const descChats = Array.from(desc.chats.values()).map(
       chat => chat?.identity?.displayName
     )
-    expect(descChats).to.include('Test Chat')
-    expect(descChats).to.include('Additional Chat')
+    expect(descChats).toEqual(expect.arrayContaining(['Test Chat']))
+    expect(descChats).toEqual(expect.arrayContaining(['Additional Chat']))
 
     const chats = Array.from(community.chats.values()).map(
       chat => chat?.communityChat?.identity?.displayName
     )
-    expect(chats).to.include('Test Chat')
-    expect(chats).to.include('Additional Chat')
+    expect(chats).toEqual(expect.arrayContaining(['Test Chat']))
+    expect(chats).toEqual(expect.arrayContaining(['Additional Chat']))
   })
 })
