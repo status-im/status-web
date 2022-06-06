643f7faa92457c2a9e13afd5f7d012c0b759aadb
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-06-06 11:05:44 +0200

    make pbkdf2 async

--- packages/status-js/src/encryption.ts
+++ packages/status-js/src/encryption.ts
@@ -1,13 +1,25 @@
-import pbkdf2 from 'pbkdf2'
+import { utf8ToBytes } from 'ethereum-cryptography/utils'
+import { pbkdf2 } from 'pbkdf2'
 
 const AESKeyLength = 32 // bytes
 
-export function createSymKeyFromPassword(password: string): Uint8Array {
-  return pbkdf2.pbkdf2Sync(
-    Buffer.from(password, 'utf-8'),
-    '',
-    65356,
-    AESKeyLength,
-    'sha256'
-  )
+export async function createSymKeyFromPassword(
+  password: string
+): Promise<Uint8Array> {
+  return new Promise((resolve, reject) => {
+    pbkdf2(
+      utf8ToBytes(password),
+      '',
+      65356,
+      AESKeyLength,
+      'sha256',
+      (err, buf) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(buf)
+        }
+      }
+    )
+  })
 }
