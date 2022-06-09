23871e432a89a693873c8090f1a83669748a1eee
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-06-09 15:56:19 +0200

    unify reaction type

--- packages/status-js/src/client/community/get-reactions.ts
+++ packages/status-js/src/client/community/get-reactions.ts
@@ -1,12 +1,6 @@
-import { EmojiReaction } from '../../../protos/emoji-reaction'
+import type { EmojiReaction } from '../../../protos/emoji-reaction'
 
-type Reaction =
-  | 'heart'
-  | 'thumbs-up'
-  | 'thumbs-down'
-  | 'smile'
-  | 'sad'
-  | 'angry'
+type Reaction = Exclude<`${EmojiReaction.Type}`, 'UNKNOWN_EMOJI_REACTION_TYPE'>
 
 export type Reactions = {
   [key in Reaction]: {
@@ -15,38 +9,30 @@ export type Reactions = {
   }
 }
 
-const REACTION_MAP: Record<EmojiReaction.Type, string> = {
-  [EmojiReaction.Type.LOVE]: 'heart',
-  [EmojiReaction.Type.THUMBS_UP]: 'thumbs-up',
-  [EmojiReaction.Type.THUMBS_DOWN]: 'thumbs-down',
-  [EmojiReaction.Type.LAUGH]: 'smile',
-  [EmojiReaction.Type.SAD]: 'sad',
-  [EmojiReaction.Type.ANGRY]: 'angry',
-  [EmojiReaction.Type.UNKNOWN_EMOJI_REACTION_TYPE]: 'unknown',
-}
-
 export function getReactions(
   reaction: EmojiReaction,
   reactions: Reactions,
   isMe: boolean
 ) {
-  // fixme!: type
-  const type = REACTION_MAP[reaction.type] as Reaction
-  const isRetracted = reaction.retracted
+  const { type, retracted } = reaction
+
+  if (type === 'UNKNOWN_EMOJI_REACTION_TYPE') {
+    return reactions
+  }
 
   const _reaction = {
     count: reactions[type].count,
     me: reactions[type].me,
   }
 
-  if (isRetracted && _reaction.count !== 0) {
+  if (retracted && _reaction.count !== 0) {
     _reaction.count -= 1
   } else {
     _reaction.count += 1
   }
 
   if (isMe) {
-    _reaction.me = isRetracted ? false : true
+    _reaction.me = retracted ? false : true
   }
 
   return {
