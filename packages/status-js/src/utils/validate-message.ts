13b9b7939a7567b3a3469857e011f1ed07a6cae4
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-06-10 13:43:48 +0200

    add basic validateMessage function

---
+++ packages/status-js/src/utils/validate-message.ts
@@ -0,0 +1,29 @@
+import type { ChatMessage } from '~/protos/chat-message'
+
+// TODO?: maybe this should normalize the message?
+export function validateMessage(message: ChatMessage): boolean {
+  if (message.messageType !== 'COMMUNITY_CHAT') {
+    return false
+  }
+
+  switch (message.contentType) {
+    case 'TEXT_PLAIN': {
+      return message.text !== ''
+    }
+    case 'IMAGE': {
+      if (message.image.type === 'UNKNOWN_IMAGE_TYPE') {
+        return false
+      }
+
+      return message.image.payload.length !== 0
+    }
+    case 'AUDIO': {
+      if (message.audio.type === 'UNKNOWN_AUDIO_TYPE') {
+        return false
+      }
+      return message.audio.payload.length !== 0
+    }
+  }
+
+  return false
+}
