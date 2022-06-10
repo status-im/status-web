020f20e801c9872060ccda611271058b084ed76c
Felicio Mununga <felicio@users.noreply.github.com>
2022-06-10 17:46:38 +0200

    remove debug

--- packages/status-js/src/account.ts
+++ packages/status-js/src/account.ts
@@ -1,10 +1,6 @@
 import { keccak256 } from 'ethereum-cryptography/keccak'
 import { getPublicKey, sign, utils } from 'ethereum-cryptography/secp256k1'
-import {
-  bytesToHex,
-  concatBytes,
-  hexToBytes,
-} from 'ethereum-cryptography/utils'
+import { bytesToHex, concatBytes } from 'ethereum-cryptography/utils'
 
 export class Account {
   public privateKey: string
@@ -17,9 +13,7 @@ export class Account {
     const chatKey = getPublicKey(privateKey, true)
 
     this.privateKey = bytesToHex(privateKey)
-    // this.publicKey = bytesToHex(publicKey)
-    this.publicKey =
-      '0x04ac419dac9a8bbb58825a3cde60eef0ee71b8cf6c63df611eeefc8e7aac7c79b55954b679d24cf5ec82da7ed921caf240628a9bfb3450c5111a9cffe54e631811'
+    this.publicKey = bytesToHex(publicKey)
 
     // TODO?: add 0x prefix to public key
     this.chatKey = bytesToHex(chatKey)
