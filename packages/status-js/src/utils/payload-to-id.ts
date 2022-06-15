ef34cb517420b1558b2c01b057950b870f7ea0d8
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-06-15 13:38:44 +0200

    use concatBytes in payloadToId
    https://jsben.ch/jze3P

--- packages/status-js/src/utils/payload-to-id.ts
+++ packages/status-js/src/utils/payload-to-id.ts
@@ -1,11 +1,11 @@
 import { keccak256 } from 'ethereum-cryptography/keccak'
-import { bytesToHex } from 'ethereum-cryptography/utils'
+import { bytesToHex, concatBytes } from 'ethereum-cryptography/utils'
 
 export function payloadToId(
   payload: Uint8Array,
   publicKey: Uint8Array
 ): string {
-  const hash = keccak256(new Uint8Array([...publicKey, ...payload]))
+  const hash = keccak256(concatBytes(payload, publicKey))
   const hex = bytesToHex(hash)
 
   return `0x${hex}`
