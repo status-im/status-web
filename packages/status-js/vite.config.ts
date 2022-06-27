649f174e570f1199e1d263c9eaeac7c90051f507
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-06-27 22:37:28 +0200

    migrate to vite

---
+++ packages/status-js/vite.config.ts
@@ -0,0 +1,31 @@
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+
+import { dependencies } from './package.json'
+
+const external = [
+  ...Object.keys(dependencies || {}),
+  // ...Object.keys(peerDependencies || {}),
+].map(name => new RegExp(`^${name}(/.*)?`))
+
+export default defineConfig(({ command }) => {
+  return {
+    resolve: {
+      alias: {
+        '~': resolve('.'),
+      },
+    },
+    build: {
+      lib: {
+        entry: './src/index.ts',
+        fileName: 'index',
+        formats: ['es'],
+      },
+      target: 'es2020',
+      emptyOutDir: command === 'build',
+      rollupOptions: {
+        external,
+      },
+    },
+  }
+})
---
+++ packages/status-react/vite.config.ts
@@ -0,0 +1,40 @@
+import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+
+import { dependencies, peerDependencies } from './package.json'
+
+const external = [
+  ...Object.keys(dependencies || {}),
+  ...Object.keys(peerDependencies || {}),
+].map(name => new RegExp(`^${name}(/.*)?`))
+
+export default defineConfig(({ command }) => {
+  return {
+    resolve: {
+      alias: {
+        '~': resolve('.'),
+      },
+    },
+    build: {
+      lib: {
+        entry: './src/index.tsx',
+        fileName: 'index',
+        formats: ['es'],
+      },
+      emptyOutDir: command === 'build',
+      // sourcemap: true,
+      target: 'es2020',
+
+      rollupOptions: {
+        external,
+      },
+    },
+
+    plugins: [
+      react({
+        // jsxRuntime: 'classic',
+      }),
+    ],
+  }
+})
