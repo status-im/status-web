914bf7be759edc3ce1f92f98cf6356d1f4b26986
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-06-27 23:05:23 +0200

    remove unused files

--- packages/status-react/src/utils/equalDate.ts
+++
@@ -1,7 +0,0 @@
-export function equalDate(a: Date, b: Date) {
-  return (
-    a.getDate() === b.getDate() &&
-    a.getMonth() === b.getMonth() &&
-    a.getFullYear() === b.getFullYear()
-  )
-}
--- packages/status-react/src/utils/reduceString.ts
+++
@@ -1,9 +0,0 @@
-export const reduceString = (
-  string: string,
-  limitBefore: number,
-  limitAfter: number
-) => {
-  return `${string.substring(0, limitBefore)}...${string.substring(
-    string.length - limitAfter
-  )}`
-}
