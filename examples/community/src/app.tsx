f521f30488ffd71e4611f62c9e4acad179afb3dc
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-07-01 17:14:34 +0200

    use env variables in example

--- examples/community/src/app.tsx
+++ examples/community/src/app.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 import { Community } from '@status-im/react'
 
+const publicKey = process.env.PUBLIC_KEY
+
+if (!publicKey) {
+  throw new Error(
+    'Add PUBLIC_KEY to your environment variables (see .env.example)'
+  )
+}
+
 export const App = () => {
-  return <Community publicKey="<YOUR_COMMUNITY_KEY>" theme="light" />
+  return <Community publicKey={publicKey} theme="light" />
 }
