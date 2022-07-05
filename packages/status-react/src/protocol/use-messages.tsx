02880ab63e2fc8f1268143e32adef7e85426a0d6
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-07-05 16:37:48 +0200

    add basic fetching more messages

--- packages/status-react/src/protocol/use-messages.tsx
+++ packages/status-react/src/protocol/use-messages.tsx
@@ -10,26 +10,53 @@ interface Result {
   data: Message[]
   loading: boolean
   // error?: Error
-  // fetchMore: () => void
+  fetchMore: () => void
 }
 
-export const useMessages = (channelId: string): Result => {
+// type State = {}
+
+// type Action = {}
+
+// const reducer = (state: State, _action: Action): State => {
+//   return state
+// }
+
+export const useMessages = (chatId: string): Result => {
   const { client } = useProtocol()
 
-  const chat = client.community.chats.get(channelId)!
+  const chat = client.community.chats.get(chatId)!
   // const [state, dispatch] = useReducer<Result>((state,action) => {}, {})
 
   const [data, setData] = useState<Message[]>(() => chat.getMessages())
+  const [date, setDate] = useState<Date>(() => {
+    const date = new Date()
+    date.setDate(date.getDate() - 1)
+    return date
+  })
+
   const [loading, setLoading] = useState(true)
   // const [error, setError] = useState<Error>()
 
+  const fetchMore = async () => {
+    const newDate = new Date()
+    newDate.setDate(date.getDate() - 1)
+    setLoading(true)
+    await chat.fetchMessages({ start: newDate })
+    setLoading(false)
+    setDate(newDate)
+  }
+
   useEffect(() => {
-    setData(chat.getMessages())
+    const messages = chat.getMessages()
 
     const handleUpdate = (messages: Message[]) => {
       setLoading(false)
       setData(messages)
     }
+
+    handleUpdate(messages)
+
+    chat.fetchMessages({ start: date })
 
     return chat.onMessage(handleUpdate)
   }, [chat])
@@ -39,7 +66,7 @@ export const useMessages = (channelId: string): Result => {
     loading,
     // error,
     // hasMore
-    // fetchMore,
+    fetchMore,
     // refetch
   }
 }
