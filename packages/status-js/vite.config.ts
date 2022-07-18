50d934f78614348f0703ddb93b2426f2b2665fe8
Felicio Mununga <felicio@users.noreply.github.com>
2022-07-18 16:32:09 +0200

    use `resolve.alias` for pbkdf2 if test

--- packages/status-js/vite.config.ts
+++ packages/status-js/vite.config.ts
@@ -4,12 +4,28 @@ import { defineConfig } from 'vite'
 
 import { dependencies } from './package.json'
 
+import type { Alias } from 'vite'
+
+const isTest = process.env.NODE_ENV === 'test'
+
 const external = [
   ...Object.keys(dependencies || {}),
   // ...Object.keys(peerDependencies || {}),
 ].map(name => new RegExp(`^${name}(/.*)?`))
 
 export default defineConfig(({ mode }) => {
+  const alias: Alias[] = []
+
+  if (isTest) {
+    alias.push({
+      /**
+       * Note: `happy-dom` nor `jsdom` have Crypto implemented (@see https://github.com/jsdom/jsdom/issues/1612)
+       */
+      find: /^.*\/crypto\/pbkdf2.browser$/,
+      replacement: 'ethereum-cryptography/pbkdf2',
+    })
+  }
+
   return {
     build: {
       target: 'es2020',
@@ -24,9 +40,11 @@ export default defineConfig(({ mode }) => {
         external,
       },
     },
-
+    resolve: {
+      alias,
+    },
     test: {
-      // environment: 'happy-dom',
+      environment: 'happy-dom',
     },
   }
 })
