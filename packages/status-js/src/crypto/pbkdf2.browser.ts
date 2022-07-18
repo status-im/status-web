e70ad9b4847bcc8201521b49c3e2e42651bd8d70
Felicio Mununga <felicio@users.noreply.github.com>
2022-07-18 11:52:45 +0200

    use webcrypto in pbkdf2

--- packages/status-js/src/crypto/pbkdf2.browser.ts
+++ packages/status-js/src/crypto/pbkdf2.browser.ts
@@ -1,6 +1,23 @@
+import _crypto from 'crypto'
+
 import type { pbkdf2 as pbkdf2Type } from 'ethereum-cryptography/pbkdf2'
 
 type PBKDF2 = typeof pbkdf2Type
+
+let crypto: Crypto
+
+if (globalThis.crypto) {
+  crypto = globalThis.crypto
+} else if (_crypto.webcrypto) {
+  /**
+   * Note: allows debugging and testing with `vite-node` and `vitest`
+   *
+   * Note: `webcrypto` is experimental (@see https://nodejs.org/dist/latest-v16.x/docs/api/webcrypto.html#web-crypto-api)
+   */
+  crypto = _crypto.webcrypto as unknown as Crypto
+} else {
+  throw new Error('Crypto is not supported in this environment')
+}
 
 export const pbkdf2: PBKDF2 = async (
   password: Uint8Array,
@@ -8,7 +25,7 @@ export const pbkdf2: PBKDF2 = async (
   iterations: number,
   keylen: number
 ): Promise<Uint8Array> => {
-  const cryptoKey = await window.crypto.subtle.importKey(
+  const cryptoKey = await crypto.subtle.importKey(
     'raw',
     password,
     { name: 'PBKDF2' },
@@ -16,7 +33,7 @@ export const pbkdf2: PBKDF2 = async (
     ['deriveBits']
   )
 
-  const derivedKey = await window.crypto.subtle.deriveBits(
+  const derivedKey = await crypto.subtle.deriveBits(
     {
       name: 'PBKDF2',
       salt,
