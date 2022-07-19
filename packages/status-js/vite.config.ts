1f567e189a8aec7d95dd9184abdf7a96d7e5ea36
Pavel <14926950+prichodko@users.noreply.github.com>
2022-07-19 17:45:29 +0200

    Use built-in PBKDF2 implementation for browsers (#295)
    * add pbkdf2 browser implementation
    
    * use webcrypto in pbkdf2
    
    * rename pbkdf2 file
    
    * use pbkdf2
    
    * add changeset
    
    * revert rename
    
    * remove browser field from package.json
    
    * use `resolve.alias` for pbkdf2 if test
    
    * use `mode` in vite.config.ts
    
    Co-authored-by: Felicio Mununga <felicio@users.noreply.github.com>

--- packages/status-js/vite.config.ts
+++ packages/status-js/vite.config.ts
@@ -4,12 +4,26 @@ import { defineConfig } from 'vite'
 
 import { dependencies } from './package.json'
 
+import type { Alias } from 'vite'
+
 const external = [
   ...Object.keys(dependencies || {}),
   // ...Object.keys(peerDependencies || {}),
 ].map(name => new RegExp(`^${name}(/.*)?`))
 
 export default defineConfig(({ mode }) => {
+  const alias: Alias[] = []
+
+  if (mode === 'test') {
+    alias.push({
+      /**
+       * Note: `happy-dom` nor `jsdom` have Crypto implemented (@see https://github.com/jsdom/jsdom/issues/1612)
+       */
+      find: /^.*\/crypto\/pbkdf2.browser$/,
+      replacement: 'ethereum-cryptography/pbkdf2',
+    })
+  }
+
   return {
     build: {
       target: 'es2020',
@@ -24,9 +38,11 @@ export default defineConfig(({ mode }) => {
         external,
       },
     },
-
+    resolve: {
+      alias,
+    },
     test: {
-      // environment: 'happy-dom',
+      environment: 'happy-dom',
     },
   }
 })
