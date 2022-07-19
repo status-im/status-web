0a8a5316dabd29655df68ad6af6df0c0142032c7
Felicio Mununga <felicio@users.noreply.github.com>
2022-07-19 09:59:53 +0200

    update vite.config.ts

--- examples/community/vite.config.ts
+++ examples/community/vite.config.ts
@@ -1,18 +1,32 @@
-// todo: see if even necessary?
-
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
+/**
+ * @see https://vitejs.dev/config/ for Vite configuration
+ * @see https://vitest.dev/config/ for Vitest configuration
+ */
 export default defineConfig({
   build: {
     target: 'esnext',
   },
-  plugins: [react()],
-  test: {
-    deps: {
-      // inline: ['@status-im/react', '@status-im/js', 'js-waku'],
-      // inline: true,
-    },
+  resolve: {
+    alias: [
+      {
+        find: '@status-im/js',
+        replacement: '../../packages/status-js/src/index.ts',
+      },
+      { find: 'js-waku', replacement: './js-waku-mock.ts' },
+      { find: /\.\/client\/client/, replacement: './client-mock.ts' },
+    ],
   },
+  // todo?: remove
+  plugins: [react()],
+  // todo: remove
+  // test: {
+  //   deps: {
+  //     // inline: ['js-waku'],
+  //     // inline: ['@status-im/react', '@status-im/js', 'js-waku'],
+  //     inline: true,
+  //   },
+  // },
 })
