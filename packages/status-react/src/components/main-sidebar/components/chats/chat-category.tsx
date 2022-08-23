fcaa75fae6f864eece79557ab6f8808e0d4f0381
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-08-23 15:36:16 +0200

    show active chat even when category closed

--- packages/status-react/src/components/main-sidebar/components/chats/chat-category.tsx
+++ packages/status-react/src/components/main-sidebar/components/chats/chat-category.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import * as Collapsible from '@radix-ui/react-collapsible'
 
-// import { BellIcon } from '../../../../icons/bell-icon'
 import { ChevronDownIcon } from '../../../../icons/chevron-down-icon'
+import { useActiveChat } from '../../../../protocol/use-active-chat'
 import { styled } from '../../../../styles/config'
+
+import type { ChatItem } from './chat-item'
 
 interface Props {
   name: string
@@ -13,33 +15,43 @@ interface Props {
 
 export const ChatCategory = (props: Props) => {
   const { name, children } = props
+  const chat = useActiveChat()
+
+  const [open, setOpen] = useState(true)
+
+  // show active chat even though the category is closed
+  const activeChild = React.Children.toArray(children).find(child => {
+    if (React.isValidElement<React.ComponentProps<typeof ChatItem>>(child)) {
+      return child.props.chat.id === chat?.uuid
+    }
+  })
 
   return (
-    <Collapsible.Root defaultOpen>
-      <CollapsibleTrigger>
-        {name}
-        <ChevronDownIcon />
-      </CollapsibleTrigger>
-      <CollapsibleContent>{children}</CollapsibleContent>
-    </Collapsible.Root>
+    <>
+      <Collapsible.Root open={open} onOpenChange={setOpen}>
+        <CollapsibleTrigger>
+          {name}
+          <ChevronDownIcon />
+        </CollapsibleTrigger>
+        <CollapsibleContent>{children}</CollapsibleContent>
+      </Collapsible.Root>
+      {open === false && activeChild}
+    </>
   )
-
-  {
-    /*
-<ContextMenuTrigger>
-  <ContextMenu>
-    <ContextMenu.TriggerItem label="Mute Category" icon={<BellIcon />}>
-      <ContextMenu.Item>For 15 min</ContextMenu.Item>
-      <ContextMenu.Item>For 1 hour</ContextMenu.Item>
-      <ContextMenu.Item>For 8 hours</ContextMenu.Item>
-      <ContextMenu.Item>For 24 hours</ContextMenu.Item>
-      <ContextMenu.Item>Until I turn it back on</ContextMenu.Item>
-    </ContextMenu.TriggerItem>
-    <ContextMenu.Item icon={<BellIcon />}>Mark as Read</ContextMenu.Item>
-  </ContextMenu>
-</ContextMenuTrigger> */
-  }
 }
+
+/* <ContextMenuTrigger>
+<ContextMenu>
+  <ContextMenu.TriggerItem label="Mute Category" icon={<BellIcon />}>
+    <ContextMenu.Item>For 15 min</ContextMenu.Item>
+    <ContextMenu.Item>For 1 hour</ContextMenu.Item>
+    <ContextMenu.Item>For 8 hours</ContextMenu.Item>
+    <ContextMenu.Item>For 24 hours</ContextMenu.Item>
+    <ContextMenu.Item>Until I turn it back on</ContextMenu.Item>
+  </ContextMenu.TriggerItem>
+  <ContextMenu.Item icon={<BellIcon />}>Mark as Read</ContextMenu.Item>
+</ContextMenu>
+</ContextMenuTrigger> */
 
 const CollapsibleTrigger = styled(Collapsible.Trigger, {
   width: '100%',
