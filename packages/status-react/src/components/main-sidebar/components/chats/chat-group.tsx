28aee2519f817900ef2a8eb2f4ad2e0ec88e9237
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-08-23 15:09:44 +0200

    disable context menu for chat group

--- packages/status-react/src/components/main-sidebar/components/chats/chat-group.tsx
+++ packages/status-react/src/components/main-sidebar/components/chats/chat-group.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 import * as Collapsible from '@radix-ui/react-collapsible'
 
-import { BellIcon } from '../../../../icons/bell-icon'
+// import { BellIcon } from '../../../../icons/bell-icon'
 import { ChevronDownIcon } from '../../../../icons/chevron-down-icon'
 import { styled } from '../../../../styles/config'
-import { ContextMenu, ContextMenuTrigger, Text } from '../../../../system'
+import { Text } from '../../../../system'
 
 interface Props {
   name: string
@@ -16,28 +16,32 @@ export const ChatGroup = (props: Props) => {
   const { name, children } = props
 
   return (
-    <ContextMenuTrigger>
-      <Collapsible.Root defaultOpen>
-        <CollapsibleTrigger>
-          <Text size="15" weight="500" color="accent">
-            {name}
-          </Text>
-          <ChevronDownIcon />
-        </CollapsibleTrigger>
-        <CollapsibleContent>{children}</CollapsibleContent>
-      </Collapsible.Root>
-      <ContextMenu>
-        <ContextMenu.TriggerItem label="Mute Category" icon={<BellIcon />}>
-          <ContextMenu.Item>For 15 min</ContextMenu.Item>
-          <ContextMenu.Item>For 1 hour</ContextMenu.Item>
-          <ContextMenu.Item>For 8 hours</ContextMenu.Item>
-          <ContextMenu.Item>For 24 hours</ContextMenu.Item>
-          <ContextMenu.Item>Until I turn it back on</ContextMenu.Item>
-        </ContextMenu.TriggerItem>
-        <ContextMenu.Item icon={<BellIcon />}>Mark as Read</ContextMenu.Item>
-      </ContextMenu>
-    </ContextMenuTrigger>
+    <Collapsible.Root defaultOpen>
+      <CollapsibleTrigger>
+        <Text size="15" weight="500" color="gray">
+          {name}
+        </Text>
+        <ChevronDownIcon />
+      </CollapsibleTrigger>
+      <CollapsibleContent>{children}</CollapsibleContent>
+    </Collapsible.Root>
   )
+
+  {
+    /*
+<ContextMenuTrigger>
+  <ContextMenu>
+    <ContextMenu.TriggerItem label="Mute Category" icon={<BellIcon />}>
+      <ContextMenu.Item>For 15 min</ContextMenu.Item>
+      <ContextMenu.Item>For 1 hour</ContextMenu.Item>
+      <ContextMenu.Item>For 8 hours</ContextMenu.Item>
+      <ContextMenu.Item>For 24 hours</ContextMenu.Item>
+      <ContextMenu.Item>Until I turn it back on</ContextMenu.Item>
+    </ContextMenu.TriggerItem>
+    <ContextMenu.Item icon={<BellIcon />}>Mark as Read</ContextMenu.Item>
+  </ContextMenu>
+</ContextMenuTrigger> */
+  }
 }
 
 const CollapsibleTrigger = styled(Collapsible.Trigger, {
