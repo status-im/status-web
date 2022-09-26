49de0bb6882d521898fed207a11944d45dc88c52
Felicio Mununga <felicio@users.noreply.github.com>
2022-09-26 22:14:00 +0200

    add comment

--- packages/status-js/src/client/chat.ts
+++ packages/status-js/src/client/chat.ts
@@ -314,6 +314,7 @@ export class Chat {
     }
 
     newMessage.member = this.client.community.getMember(newMessage.signer)!
+    // todo?: asign as .community, .community.description, .communityDescription, or .community.displayName
     newMessage.communityDisplayName =
       this.client.community.description.identity?.displayName
     newMessage.chatDisplayName = this.description.identity?.displayName
