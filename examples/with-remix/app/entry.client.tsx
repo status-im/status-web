d4f449ebf0e71f07136e3ae54c96d5265a1387cd
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-10-10 15:13:28 -0500

    remove with-remix example

--- examples/with-remix/app/entry.client.tsx
+++
@@ -1,5 +0,0 @@
-import { hydrate } from 'react-dom'
-
-import { RemixBrowser } from '@remix-run/react'
-
-hydrate(<RemixBrowser />, document)
--- examples/with-remix/app/routes/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Community } from '@status-im/react'
-
-export default function Index() {
-  return <Community publicKey="<YOUR_COMMUNITY_KEY>" theme="light" />
-}
