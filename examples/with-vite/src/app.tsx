a0f995a342fb4407fb8ee13af00df349638bb7d5
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2022-10-10 14:41:04 -0500

    update vite example

--- examples/with-vite/src/app.tsx
+++ examples/with-vite/src/app.tsx
@@ -1,5 +1,17 @@
 import { Community } from '@status-im/react'
 
+const publicKey = process.env.PUBLIC_KEY
+
+if (!publicKey) {
+  throw new Error(
+    'Add PUBLIC_KEY to your environment variables (see .env.example)'
+  )
+}
+
+const environment = process.env.ENVIRONMENT as 'production' | 'test'
+
 export const App = () => {
-  return <Community publicKey="<YOUR_COMMUNITY_KEY>" theme="light" />
+  return (
+    <Community publicKey={publicKey} environment={environment} theme="light" />
+  )
 }
