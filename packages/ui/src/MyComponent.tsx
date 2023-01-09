1164ab2dba672deacbbca897d8a03be3ae044ef2
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2023-01-09 22:14:58 +0100

    add ui package

---
+++ packages/ui/src/MyComponent.tsx
@@ -0,0 +1,7 @@
+import { styled, YStack } from 'tamagui'
+
+export const MyComponent = styled(YStack, {
+  backgroundColor: 'pink',
+  width: 100,
+  height: 100
+})
---
+++ packages/ui/src/index.tsx
@@ -0,0 +1,3 @@
+export * from './MyComponent'
+export { config } from './tamagui.config'
+export * from 'tamagui'
