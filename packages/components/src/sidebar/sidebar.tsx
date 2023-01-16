a31a87e0c9983c16918114205d4c653f5f2598ce
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2023-01-16 15:08:39 +0100

    update sidebar

--- packages/components/src/sidebar/sidebar.tsx
+++ packages/components/src/sidebar/sidebar.tsx
@@ -1,77 +1,12 @@
-import { setupReactNative, Stack, styled, Text } from '@tamagui/core'
-// import { focusableInputHOC } from '@tamagui/focusable'
-import { TextInput } from 'react-native'
+import { Stack, styled, Text } from '@tamagui/core'
 
 import { Avatar } from '../avatar'
 import { Button } from '../button'
 import { Image } from '../image'
 
-import type { GetProps } from '@tamagui/core'
-
-// import { inputSizeVariant } from '../helpers/inputHelpers'
-
-setupReactNative({
-  TextInput,
-})
-
-export const InputFrame = styled(
-  TextInput,
-  {
-    tag: 'input',
-    name: 'Input',
-    // fontFamily: '$body',
-    borderWidth: 1,
-    outlineWidth: 0,
-    borderColor: 'rgba(0, 200, 0, 1)',
-
-    paddingHorizontal: 30,
-    color: 'hsla(218, 51%, 7%, 1)',
-    placeholderTextColor: 'hsla(219, 17%, 69%, 1)',
-    // color: 'red',
-    // color: '$color',
-    // focusable: true,
-    // borderColor: '$borderColor',
-    // backgroundColor: '$background',
-    // placeholderTextColor: '$placeholderColor',
-
-    backgroundColor: 'rgb(255, 255, 255)',
-
-    height: 40,
-    borderRadius: 12,
-
-    // this fixes a flex bug where it overflows container
-    minWidth: 0,
-
-    // hoverStyle: {
-    // borderColor: '$borderColorHover',
-    // },/
-
-    // focusStyle: {
-    //   // borderColor: '$borderColorFocus',
-    //   borderWidth: 2,
-    //   marginHorizontal: -1,
-    // },
-
-    // variants: {
-    //   size: {
-    //     // '...size': inputSizeVariant,
-    //   },
-    // } as const,
-
-    // defaultVariants: {
-    //   size: '$true',
-    // },
-  }
-  // {
-  //   isInput: true,
-  // }
-)
-
 const Name = styled(Text, {})
 
 const Desc = styled(Text, {})
-
-export type InputProps = GetProps<typeof InputFrame>
 
 interface Props {
   name: string
@@ -113,5 +48,4 @@ const _Sidebar = (props: Props) => {
   )
 }
 
-// export const Input = focusableInputHOC(InputFrame)
 export const Sidebar = _Sidebar
