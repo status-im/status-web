c39f7f36c875f8afda015ce4159d3b9649e65165
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2023-01-17 16:01:33 +0100

    add icon and accept only size

--- packages/icons/src/types.ts
+++ packages/icons/src/types.ts
@@ -2,5 +2,8 @@ import type { SVGAttributes } from 'react'
 
 export interface IconProps extends SVGAttributes<SVGElement> {
   children?: never
+  width?: never
+  height?: never
   color?: string
+  size?: number
 }
