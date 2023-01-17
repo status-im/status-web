87957c1611003b2f3567a3f4991b8652edeab954
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2023-01-17 16:00:33 +0100

    add IconButton component

---
+++ packages/components/src/icon-button/icon-button.stories.tsx
@@ -0,0 +1,22 @@
+import { OptionsIcon } from '@status-im/icons'
+
+import { IconButton } from './icon-button'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+// More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
+const meta: Meta<typeof IconButton> = {
+  component: IconButton,
+  argTypes: {},
+}
+
+type Story = StoryObj<typeof IconButton>
+
+// More on writing stories with args: https://storybook.js.org/docs/7.0/react/writing-stories/args
+export const Default: Story = {
+  args: {
+    icon: <OptionsIcon />,
+  },
+}
+
+export default meta
