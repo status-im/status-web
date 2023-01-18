e513572f6084370c11f8c98a47175a78a60f7906
marcelines <marcio@significa.co>
2023-01-18 12:37:07 +0000

    fix: id from channel

--- packages/components/src/sidebar/sidebar.tsx
+++ packages/components/src/sidebar/sidebar.tsx
@@ -44,7 +44,7 @@ const COMMUNITIES: CommunitiesProps[] = [
         icon: <Collaboration hasBackground />,
       },
       {
-        id: 'general',
+        id: 'general-welcome',
         title: '# general',
         icon: <Play hasBackground />,
       },
@@ -218,7 +218,7 @@ const _Sidebar = (props: Props) => {
     <Stack backgroundColor="$background">
       <Image
         src="https://images.unsplash.com/photo-1584475784921-d9dbfd9d17ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-        width={350}
+        width={352}
         height={136}
       />
       <Stack
