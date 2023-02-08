8fc523ce3703cb2c036942c832b4a269d2231a56
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2023-02-08 12:31:08 +0100

    update chat message actions

--- packages/components/src/messages/components/actions.tsx
+++ packages/components/src/messages/components/actions.tsx
@@ -1,18 +1,43 @@
-import { AddReactionIcon, OptionsIcon, ReplyIcon } from '@status-im/icons/20'
+import { useEffect } from 'react'
+
+import {
+  AddReactionIcon,
+  CopyIcon,
+  DeleteIcon,
+  EditIcon,
+  ForwardIcon,
+  LinkIcon,
+  OptionsIcon,
+  PinIcon,
+  ReplyIcon,
+} from '@status-im/icons/20'
 import { Stack } from 'tamagui'
 
 import { Button } from '../../button'
+import { DropdownMenu } from '../../dropdown-menu'
+import { ReactionPopover } from './reaction-popover'
+
+import type { ReactionsType } from '../types'
 
 interface Props {
-  onClick: VoidFunction
+  reactions: ReactionsType
+  onOpenChange: (open: boolean) => void
+  onReplyPress: VoidFunction
 }
-export const Actions = (_props: Props) => {
+
+export const Actions = (props: Props) => {
+  const { reactions, onOpenChange, onReplyPress } = props
+
+  useEffect(() => {
+    return () => onOpenChange(false)
+  }, [onOpenChange])
+
   return (
     <Stack
       backgroundColor="$white-100"
       borderWidth={1}
       borderColor="$neutral-10"
-      borderRadius={10}
+      borderRadius={12}
       overflow="hidden"
       position="absolute"
       top={-16}
@@ -23,9 +48,64 @@ export const Actions = (_props: Props) => {
       shadowColor="rgba(9, 16, 28, 0.08)"
       zIndex={10}
     >
-      <Button type="ghost" icon={<AddReactionIcon />} borderRadius={0} />
-      <Button type="ghost" icon={<ReplyIcon />} borderRadius={0} />
-      <Button type="ghost" icon={<OptionsIcon />} borderRadius={0} />
+      {/* REACTION */}
+      <ReactionPopover
+        reactions={reactions}
+        side="left"
+        sideOffset={6}
+        onOpenChange={onOpenChange}
+      >
+        <Button type="ghost" icon={<AddReactionIcon />} />
+      </ReactionPopover>
+
+      {/* REPLY */}
+      <Button type="ghost" icon={<ReplyIcon />} onPress={onReplyPress} />
+
+      {/* OPTIONS MENU */}
+      <DropdownMenu modal={false} onOpenChange={onOpenChange}>
+        <Button type="ghost" icon={<OptionsIcon />} />
+        <DropdownMenu.Content align="end" sideOffset={10}>
+          <DropdownMenu.Item
+            icon={<EditIcon />}
+            label="Edit message"
+            onSelect={() => console.log('edit')}
+          />
+          <DropdownMenu.Item
+            icon={<ReplyIcon />}
+            label="Reply"
+            onSelect={() => console.log('reply')}
+          />
+          <DropdownMenu.Item
+            icon={<CopyIcon />}
+            label="Copy text"
+            onSelect={() => console.log('copy')}
+          />
+          <DropdownMenu.Item
+            icon={<PinIcon />}
+            label="Pin to the channel"
+            onSelect={() => console.log('pin')}
+          />
+          <DropdownMenu.Item
+            icon={<ForwardIcon />}
+            label="Forward"
+            onSelect={() => console.log('forward')}
+          />
+          <DropdownMenu.Item
+            icon={<LinkIcon />}
+            label="Share link to message"
+            onSelect={() => console.log('share')}
+          />
+
+          <DropdownMenu.Separator />
+
+          <DropdownMenu.Item
+            icon={<DeleteIcon />}
+            label="Delete message"
+            danger
+            onSelect={() => console.log('delete')}
+          />
+        </DropdownMenu.Content>
+      </DropdownMenu>
     </Stack>
   )
 }
