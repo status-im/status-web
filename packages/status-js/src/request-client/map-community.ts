fd372f9445970e8bec32dc5f0ab5b34d2d6bf351
Felicio Mununga <felicio@users.noreply.github.com>
2023-03-15 14:14:26 +0100

    Add request client (#342)
    * add `preview-client.ts`
    
    * add exports
    
    * fix imports
    
    * set emoji hash
    
    * use `message.timestamp`
    
    * rename `RequestClient`
    
    * drop preview keyword
    
    * rm `appUrl`
    
    * rm todo?:
    
    * remove social urls
    
    * remove if clause

---
+++ packages/status-js/src/request-client/map-community.ts
@@ -0,0 +1,51 @@
+import { tags as tagsMap } from './tags'
+
+import type { CommunityDescription } from '../protos/communities_pb'
+
+export type CommunityInfo = {
+  banner?: Uint8Array
+  photo?: Uint8Array
+  displayName: string
+  description: string
+  membersCount: number
+  tags: Array<{
+    emoji: string
+    text: string
+  }>
+  color: string
+}
+
+export function mapCommunity(
+  communityDescription: CommunityDescription
+): CommunityInfo | undefined {
+  const { identity, tags, members } = communityDescription
+
+  if (!identity) {
+    return
+  }
+
+  const communityInfo: CommunityInfo = {
+    banner: identity.images.banner?.payload,
+    photo: identity.images.thumbnail?.payload,
+    displayName: identity.displayName,
+    description: identity.description,
+    membersCount: Object.keys(members).length,
+    tags: tags.reduce<CommunityInfo['tags']>((tags, nextTag) => {
+      const emoji = tagsMap[nextTag as keyof typeof tagsMap]
+
+      if (!emoji) {
+        return tags
+      }
+
+      tags.push({
+        text: nextTag,
+        emoji,
+      })
+
+      return tags
+    }, []),
+    color: identity.color,
+  }
+
+  return communityInfo
+}
