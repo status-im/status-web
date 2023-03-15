d832a0c283b2b3211a24b8c38d704bdb17466b93
Felicio Mununga <felicio@users.noreply.github.com>
2023-03-15 14:14:26 +0100

    Add request client (#342)
    * add `preview-client.ts`
    
    * add exports
    
    * fix imports
    
    * set emoji hash
    
    * use `message.timestamp`
    
    * rename `RequestClient`
    
    * drop preview keyword
    
    * rm `appUrl`
    
    * rm todo?:
    
    * remove social urls
    
    * remove if clause

---
+++ packages/status-js/src/request-client/map-channel.ts
@@ -0,0 +1,42 @@
+import { mapCommunity } from './map-community'
+
+import type {
+  CommunityChat,
+  CommunityDescription,
+} from '../protos/communities_pb'
+import type { CommunityInfo } from './map-community'
+
+export type ChannelInfo = {
+  emoji?: string
+  displayName: string
+  description: string
+  color: string
+  community: CommunityInfo
+}
+
+export function mapChannel(
+  communityChat: CommunityChat,
+  communityDescription: CommunityDescription
+): ChannelInfo | undefined {
+  const community = mapCommunity(communityDescription)
+
+  if (!community) {
+    return
+  }
+
+  const { identity } = communityChat
+
+  if (!identity) {
+    return
+  }
+
+  const channelInfo: ChannelInfo = {
+    emoji: identity.emoji,
+    displayName: identity.displayName,
+    description: identity.description,
+    color: identity.color,
+    community,
+  }
+
+  return channelInfo
+}
---
+++ packages/status-js/src/request-client/map-user.ts
@@ -0,0 +1,32 @@
+import { publicKeyToEmojiHash } from '../utils/public-key-to-emoji-hash'
+
+import type { ContactCodeAdvertisement } from '../protos/push-notifications_pb'
+
+export type UserInfo = {
+  photo?: Uint8Array
+  displayName: string
+  description?: string
+  emojiHash: string
+  // todo: currently not in protobuf nor in product
+  // color: string
+}
+
+export function mapUser(
+  contactCodeAdvertisement: ContactCodeAdvertisement,
+  userPublicKey: string
+): UserInfo | undefined {
+  const { chatIdentity: identity } = contactCodeAdvertisement
+
+  if (!identity) {
+    return
+  }
+
+  const userInfo: UserInfo = {
+    photo: identity.images.thumbnail?.payload,
+    displayName: identity.displayName,
+    description: identity.description,
+    emojiHash: publicKeyToEmojiHash(userPublicKey),
+  }
+
+  return userInfo
+}
