b606d92ce4148cc692806731933c49f11f1fa5f1
Jakub Kotula <520927+jkbktl@users.noreply.github.com>
2023-03-21 15:31:47 +0100

    context tag

--- packages/components/src/context-tag/context-tag.stories.tsx
+++ packages/components/src/context-tag/context-tag.stories.tsx
@@ -1,6 +1,3 @@
-import { PinIcon } from '@status-im/icons/20'
-
-import { List } from '../utils'
 import { ContextTag } from './context-tag'
 
 import type { Meta, StoryObj } from '@storybook/react'
@@ -16,39 +13,9 @@ const meta: Meta<typeof ContextTag> = {
 
 type Story = StoryObj<typeof ContextTag>
 
-export const Full: Story = {
-  args: {
-    icon: <PinIcon />,
-    children: 'ContextTag message',
-    count: 5,
-  },
-}
-
-export const NoIcon: Story = {
-  args: {
-    children: 'ContextTag message',
-    count: 5,
-  },
-}
-
-export const NoCount: Story = {
-  args: {
-    icon: <PinIcon />,
-    children: 'ContextTag message',
-  },
-}
-
 export const AllVariants: Story = {
-  args: {},
-  render: () => (
-    <List>
-      <ContextTag icon={<PinIcon />} count={5}>
-        ContextTag message
-      </ContextTag>
-      <ContextTag count={5}>ContextTag message</ContextTag>
-      <ContextTag icon={<PinIcon />}>ContextTag message</ContextTag>
-    </List>
-  ),
+  args: { children: 'ContextTag' },
+  render: ({ children }) => <ContextTag>{children}</ContextTag>,
 }
 
 export default meta
--- packages/components/src/context-tag/context-tag.tsx
+++ packages/components/src/context-tag/context-tag.tsx
@@ -1,44 +1,26 @@
 import { styled } from '@tamagui/core'
 import { View } from 'react-native'
 
-import { Badge } from '../badge'
 import { Paragraph } from '../typography'
 
 type ContextTagProps = {
   children: React.ReactNode
-  icon?: React.ReactNode
-  count?: number
 }
 
 const StyledView = styled(View, {
-  backgroundColor: '$primary-50-opa-20',
-  padding: 12,
-  flexDirection: 'row',
-  alignItems: 'center',
-  justifyContent: 'space-between',
+  backgroundColor: '$neutral-10',
+  paddingHorizontal: 8,
+  paddingVertical: 3,
+  borderRadius: 10,
+  display: 'inline',
 })
 
-const InsideView = styled(View, {
-  flexDirection: 'row',
-  gap: 10,
-})
-
-const Truncate = styled(Paragraph, {
-  display: 'inline-flex',
-  overflow: 'hidden',
-  textOverflow: 'ellipsis',
-  whiteSpace: 'nowrap',
-  maxWidth: '200px',
-})
-
-export const ContextTag = ({ icon, children, count }: ContextTagProps) => {
+export const ContextTag = ({ children }: ContextTagProps) => {
   return (
     <StyledView>
-      <InsideView>
-        {icon}
-        <Truncate color="$textPrimary">{children}</Truncate>
-      </InsideView>
-      {count ? <Badge value={count} /> : null}
+      <Paragraph size={12} weight="medium" color="$neutral-100">
+        {children}
+      </Paragraph>
     </StyledView>
   )
 }
