4cc7d829ba2d4830e3fd0a6e6e5e1b5abfe0d276
Felicio Mununga <felicio@users.noreply.github.com>
2023-03-24 04:56:35 +0100

    add `resolveChatKey`

---
+++ packages/status-js/src/ethereum-client/ethereum-client.ts
@@ -0,0 +1,42 @@
+import { Point } from 'ethereum-cryptography/secp256k1'
+import { ethers } from 'ethers'
+
+export class EthereumClient {
+  private provider: ethers.JsonRpcApiProvider
+
+  constructor(url: string) {
+    this.provider = new ethers.JsonRpcProvider(url)
+  }
+
+  async resolveChatKey(ensName: string): Promise<string | undefined> {
+    try {
+      const resolver = await this.provider.getResolver(ensName)
+
+      if (!resolver) {
+        return
+      }
+
+      const address = resolver.address
+      const abi = [
+        'function pubkey(bytes32 node) view returns (bytes32 x, bytes32 y)',
+      ]
+
+      const resolverContract = new ethers.Contract(address, abi, this.provider)
+
+      const node = ethers.namehash(ensName)
+      const [x, y] = await resolverContract.pubkey(node)
+
+      const px = BigInt(x)
+      const py = BigInt(y)
+
+      const point = new Point(px, py)
+      point.assertValidity()
+
+      const hex = point.toHex(true)
+
+      return `0x${hex}`
+    } catch {
+      return
+    }
+  }
+}
