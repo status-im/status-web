0fbee8fbdde2e9a7c2469921945613037adea4a5
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2023-03-31 14:46:28 +0200

    setup provider and font

--- apps/website/pages/_app.tsx
+++ apps/website/pages/_app.tsx
@@ -1,17 +1,22 @@
 import '@/styles/globals.css'
 
-import { TamaguiProvider } from '@tamagui/core'
-
-import tamaguiConfig from '../tamagui.config'
+import { Provider } from '@status-im/components'
+import { Inter } from 'next/font/google'
 
 import type { AppProps } from 'next/app'
 
+const inter = Inter({
+  variable: '--font-sans',
+  weight: ['400', '500', '600'],
+  subsets: ['latin'],
+})
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <TamaguiProvider config={tamaguiConfig}>
+    <div className={inter.variable}>
+      <Provider>
         <Component {...pageProps} />
-      </TamaguiProvider>
-    </>
+      </Provider>
+    </div>
   )
 }
