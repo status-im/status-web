11301c91f1223c3d69b322b2c69d044b9c3fbfb1
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2023-03-31 14:46:49 +0200

    add example page

---
+++ apps/website/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@status-im/components'
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="primary">Hello from @status-im/components</Button>
+    </div>
+  )
+}
