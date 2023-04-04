6e17805290d9cee604db87ff9fa75f0ca889a1ef
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2023-04-04 15:38:00 +0200

    finalize SidebarMembers component

--- packages/components/src/community/sidebar-members/sidebar-member.stories.tsx
+++ packages/components/src/community/sidebar-members/sidebar-member.stories.tsx
@@ -4,9 +4,15 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 // More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
 const meta: Meta<typeof SidebarMembers> = {
-  title: 'Sidebar/Members',
+  title: 'Community/Members Sidebar',
   component: SidebarMembers,
+  args: {},
   argTypes: {},
+  render: () => (
+    <div style={{ maxWidth: 360 }}>
+      <SidebarMembers />
+    </div>
+  ),
 }
 
 type Story = StoryObj<typeof SidebarMembers>
