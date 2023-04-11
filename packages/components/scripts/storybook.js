cf8d302ca64eb9862388946d7fb4bdb85e0f48f6
Felicio Mununga <felicio@users.noreply.github.com>
2023-04-11 20:53:24 +0200

    terminate `storybook` on `SIGINT` (#375)

---
+++ packages/components/scripts/storybook.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// todo: remove after adding `"type": "module",` to `package.json`
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const child_process = require('node:child_process')
+const process = require('node:process')
+
+const subprocess = child_process.spawn('yarn', ['storybook:dev'], {
+  detached: true,
+  stdio: 'inherit',
+})
+
+process.once('SIGINT', () => {
+  process.kill(-subprocess.pid, 'SIGKILL')
+})
