c027ed2045b426cf6b42f980c2ee2c7004895a2b
Felicio Mununga <felicio@users.noreply.github.com>
2023-04-11 14:03:55 +0200

    remove index

--- apps/website/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from '@status-im/components'
-
-export default function Home() {
-  return (
-    <div>
-      <Button variant="primary">Hello from @status-im/components</Button>
-    </div>
-  )
-}
