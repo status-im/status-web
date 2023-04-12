dade1b06781a152bf799de865f95163e2a7faf2b
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2023-04-12 20:03:20 +0200

    generate colors

---
+++ packages/colors/src/networks.ts
@@ -0,0 +1,10 @@
+export const networks = {
+  arbitrum: 'rgba(107 213 240 / 100%)',
+  ethereum: 'rgba(117 142 235 / 100%)',
+  hermez: 'rgba(235 132 98 / 100%)',
+  optimism: 'rgba(231 110 110 / 100%)',
+  polygon: 'rgba(173 113 243 / 100%)',
+  unknow: 'rgba(238 242 245 / 100%)',
+  'x-dai': 'rgba(63 192 189 / 100%)',
+  'zk-sync': 'rgba(159 160 254 / 100%)',
+}
