eee39ca52e7b56c522e68303ed57a5b557689a9c
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2023-04-29 19:12:14 +0200

    add colors to tailwind config

--- apps/website/tailwind.config.js
+++ apps/website/tailwind.config.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require('@status-im/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -10,7 +13,21 @@ module.exports = {
     // './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      sans: ['var(--font-inter)'],
+    },
+    colors,
+
+    // use <Text /> from @status-im/components
+    fontSize: {},
+    fontWeight: {
+      //   regular: '400',
+      //   medium: '500',
+      //   semibold: '600',
+    },
+
     extend: {},
   },
+
   plugins: [],
 }
