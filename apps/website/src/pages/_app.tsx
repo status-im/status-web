93fece86f925c5dd3f145d972aa7b0dc70506d66
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2023-05-04 15:15:16 +0200

    fix fonts

--- apps/website/src/pages/_app.tsx
+++ apps/website/src/pages/_app.tsx
@@ -1,5 +1,4 @@
-import '@/styles/app.css'
-import '@/styles/reset.css'
+import '@/styles/global.css'
 
 import { Provider } from '@status-im/components'
 import { Inter } from 'next/font/google'
@@ -8,8 +7,8 @@ import type { Page, PageLayout } from 'next'
 import type { AppProps } from 'next/app'
 
 const inter = Inter({
-  variable: '--font-sans',
-  weight: ['400', '500', '600'],
+  variable: '--font-inter',
+  weight: ['400', '500', '600', '700'],
   subsets: ['latin'],
 })
 
@@ -21,7 +20,7 @@ export default function App({ Component, pageProps }: Props) {
   const getLayout: PageLayout = Component.getLayout || (page => page)
 
   return (
-    <div id="app" className={inter.variable}>
+    <div id="app" className={inter.variable + ' font-sans'}>
       <Provider>{getLayout(<Component {...pageProps} />)}</Provider>
     </div>
   )
--- apps/website/tailwind.config.js
+++ apps/website/tailwind.config.js
@@ -1,4 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { fontFamily } = require('tailwindcss/defaultTheme')
 const colors = require('@status-im/colors')
 
 /** @type {import('tailwindcss').Config} */
@@ -9,16 +11,17 @@ module.exports = {
   ],
   theme: {
     fontFamily: {
-      sans: ['var(--font-inter)'],
+      sans: ['var(--font-inter)', ...fontFamily.sans],
     },
     colors,
 
-    // use <Text /> from @status-im/components
-    fontSize: {},
+    // use <Text /> from @status-im/components or arbitrary values
+    // fontSize: {},
     fontWeight: {
-      //   regular: '400',
-      //   medium: '500',
-      //   semibold: '600',
+      regular: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
     },
 
     extend: {},
