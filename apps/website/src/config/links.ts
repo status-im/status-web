4aa1aea0ac2abc1609701f456dfef5a18ec4e7f8
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2023-05-10 15:42:33 +0200

    update links

--- apps/website/src/config/links.ts
+++ apps/website/src/config/links.ts
@@ -5,30 +5,63 @@ export const LINKS = {
     { name: 'Wallet', href: '/' },
     { name: 'Browser', href: '/' },
   ],
+  Platforms: [
+    { name: 'Mobile', href: '/' },
+    { name: 'Desktop', href: '/' },
+    { name: 'Web', href: '/' },
+  ],
   About: [
-    { name: 'Our mission', href: '/' },
+    { name: 'Mission', href: '/' },
     { name: 'Principles', href: '/' },
     { name: 'Team', href: '/' },
     { name: 'Partners', href: '/' },
     { name: 'Press Kit', href: '/' },
   ],
-  Help: [
-    { name: 'Wallet', href: '/' },
-    { name: 'Wallet', href: '/' },
+  Learn: [
+    { name: 'Getting Started', href: '/learn/' },
+    { name: 'Using Status', href: '/learn/' },
+    { name: 'Communities', href: '/learn/' },
+    { name: 'Wallet', href: '/learn/' },
+    { name: 'Profile', href: '/learn/' },
   ],
   Collaborate: [
-    { name: 'Wallet', href: '/' },
-    { name: 'Wallet', href: '/' },
+    { name: 'Discuss', href: '/' },
+    { name: 'Feature upvote', href: '/' },
+    { name: 'Blog', href: '/' },
+    { name: 'Translations', href: '/' },
+    { name: 'Community groups', href: '/' },
+    { name: 'Jobs', href: '/' },
   ],
   Developers: [
-    { name: 'Repos', href: '/' },
+    { name: 'Repos', href: 'https://github.com/status-im' },
     { name: 'Insights', href: '/insights' },
     { name: 'Integrations', href: '/' },
   ],
   SNT: [
-    { name: 'Wallet', href: '/' },
-    { name: 'Wallet', href: '/' },
+    { name: 'Token', href: '/' },
+    { name: 'Governance', href: '/' },
+    { name: 'Curate', href: '/' },
+    { name: 'Exchanges', href: '/' },
   ],
 } as const
+
+export const SOCIALS = {
+  status: {
+    name: 'Status',
+    href: '<TODO>',
+  },
+  twitter: {
+    name: 'Twitter',
+    href: 'https://twitter.com/ethstatus',
+  },
+  github: {
+    name: 'GitHub',
+    href: 'https://github.com/status-im',
+  },
+  youtube: {
+    name: 'YouTube',
+    href: 'https://youtube.com/<TODO>',
+  },
+}
 
 export type Links = (typeof LINKS)[keyof typeof LINKS]
