180c81e70c9de3a5cbc7189ff222644399e3b371
Felicio Mununga <felicio@users.noreply.github.com>
2023-05-12 21:52:05 +0200

    env

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,12 +1,9 @@
 import { tamaguiPlugin } from '@tamagui/vite-plugin'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 import type { PluginOption } from 'vite'
-
-process.env.TAMAGUI_TARGET = 'web'
-process.env.TAMAGUI_DISABLE_WARN_DYNAMIC_LOAD = '1'
 
 const tamaguiConfig = {
   components: ['@status-im/components'],
@@ -15,22 +12,29 @@ const tamaguiConfig = {
 }
 
 // @see: https://vitejs.dev/config
-export default defineConfig({
-  resolve: {
-    // mainFields: ['module', 'jsnext:main', 'jsnext'],
-    alias: {
-      '@status-im/components/hooks': path.resolve(
-        '../../packages/components/hooks'
-      ),
-      '@status-im/components': path.resolve('../../packages/components/src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    resolve: {
+      // mainFields: ['module', 'jsnext:main', 'jsnext'],
+      alias: {
+        '@status-im/components/hooks': path.resolve(
+          '../../packages/components/hooks'
+        ),
+        '@status-im/components': path.resolve('../../packages/components/src'),
+      },
     },
-  },
-  define: {
-    TAMAGUI_TARGET: JSON.stringify('web'),
-  },
-  plugins: [
-    react(),
-    tamaguiPlugin(tamaguiConfig) as PluginOption,
-    // tamaguiExtractPlugin(tamaguiConfig)
-  ],
+    define: {
+      'process.env.TAMAGUI_TARGET': JSON.stringify(env.TAMAGUI_TARGET),
+      'process.env.TAMAGUI_DISABLE_WARN_DYNAMIC_LOAD': JSON.stringify(
+        env.TAMAGUI_DISABLE_WARN_DYNAMIC_LOAD
+      ),
+    },
+    plugins: [
+      react(),
+      tamaguiPlugin(tamaguiConfig) as PluginOption,
+      // tamaguiExtractPlugin(tamaguiConfig)
+    ],
+  }
 })
