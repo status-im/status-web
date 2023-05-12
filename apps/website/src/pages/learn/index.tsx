347bde1d55f36e84b8f39d114af0ad046b31fe3d
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2023-05-12 20:48:40 +0200

    setup mdx provider

--- apps/website/src/pages/learn/index.tsx
+++ apps/website/src/pages/learn/index.tsx
@@ -1,11 +1,64 @@
-import { AppLayout } from '@/layouts/app-layout'
+import { MDXProvider } from '@mdx-js/react'
+import { Text } from '@status-im/components'
+// import Image from 'next/image'
+import Link from 'next/link'
+
+import { AppLayout, PageBody } from '@/layouts/app-layout'
+
+import Test from './test.md'
 
 import type { Page } from 'next'
+// import type { ImageProps } from 'next/image'
+import type { ComponentProps } from 'react'
+
+const components = {
+  h1: (props: ComponentProps<'h1'>) => (
+    <h1 className="text-[40px] font-bold" {...props}>
+      {props.children}
+    </h1>
+  ),
+  h2: (props: ComponentProps<'h2'>) => (
+    <h2 className="mb-4 mt-8 text-[27px] font-semibold" {...props}>
+      {props.children}
+    </h2>
+  ),
+  h3: (props: ComponentProps<'h3'>) => (
+    <h3 className="mb-2 text-[19px] font-semibold" {...props}>
+      {props.children}
+    </h3>
+  ),
+  a: (props: ComponentProps<'a'>) => (
+    // @ts-expect-error something with ref
+    <Link href={props.href!} {...props}>
+      <Text size={15} color="$primary-50" weight="semibold">
+        {props.children}
+      </Text>
+    </Link>
+  ),
+  p: (props: ComponentProps<'p'>) => <Text size={15}>{props.children}</Text>,
+  // img: (props: ImageProps) => (
+  //   <Image
+  //     alt={props.alt}
+  //     sizes="100vw"
+  //     style={{ width: '100%', height: 'auto' }}
+  //     {...props}
+  //   />
+  // ),
+  // pre: Pre,
+  // code: InlineCode,
+}
 
 const LearnPage: Page = () => {
   return (
     <div>
-      <h1>Learn</h1>
+      <PageBody>
+        <h1>Learn</h1>
+        <div className="mx-auto max-w-[542px]">
+          <MDXProvider components={components}>
+            <Test />
+          </MDXProvider>
+        </div>
+      </PageBody>
     </div>
   )
 }
