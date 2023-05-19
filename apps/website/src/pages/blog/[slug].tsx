8e6ac7f0b1aa0a4023a9a8114112ed75d97e0bbe
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2023-05-19 15:18:15 +0200

    add ssg to blog too

--- apps/website/src/pages/blog/[slug].tsx
+++ apps/website/src/pages/blog/[slug].tsx
@@ -1,13 +1,37 @@
-import { AppLayout } from '@/layouts/app-layout'
+import { AppLayout, PageBody } from '@/layouts/app-layout'
 
-import type { Page } from 'next'
+import type {
+  GetStaticPaths,
+  GetStaticProps,
+  InferGetStaticPropsType,
+  Page,
+} from 'next'
 
-const BlogDetailPage: Page = () => {
-  return (
-    <div>
-      <h1>Blog</h1>
-    </div>
-  )
+type Params = { slug: string[] }
+
+export const getStaticPaths: GetStaticPaths<Params> = async () => {
+  // TODO: fetch from Ghost
+  return { paths: [], fallback: false }
+}
+
+type Post = {
+  id: string
+}
+
+export const getStaticProps: GetStaticProps<
+  { post: Post },
+  Params
+> = async () => {
+  return { notFound: true }
+  // return { props: {  } }
+}
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>
+
+const BlogDetailPage: Page<Props> = (props: Props) => {
+  const { post } = props
+
+  return <PageBody>{post.id}</PageBody>
 }
 
 BlogDetailPage.getLayout = AppLayout
