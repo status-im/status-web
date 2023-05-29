d71a07ca3ac67a83aad52b425f2371a3a095020d
marcelines <marcio@status.im>
2023-05-29 11:33:04 +0100

    feat: add datepicker component to use in insights

---
+++ apps/website/src/components/datepicker/datepicker.tsx
@@ -0,0 +1,40 @@
+import { Calendar } from '@status-im/components/src/calendar/calendar'
+import { Popover } from '@status-im/components/src/popover'
+import { EditIcon } from '@status-im/icons'
+
+import { formatDate } from '../chart/utils/format-time'
+
+import type { DateRange } from '@status-im/components/src/calendar/calendar'
+
+type Props = {
+  selected?: DateRange
+  onSelect: (selected?: DateRange) => void
+}
+
+const DatePicker = (props: Props) => {
+  const { selected, onSelect } = props
+
+  return (
+    <div className="sticky bottom-5 flex justify-center">
+      <Popover>
+        <button className="border-neutral-80/5 bg-blur-neutral-5/70 active inline-flex min-h-[30px] cursor-pointer items-center justify-center gap-2 rounded-xl border-solid pl-3 pr-2 uppercase text-neutral-100 backdrop-blur-sm">
+          <span className="text-blur-neutral-80/80 text-[13px] font-medium">
+            Filter between
+          </span>
+          <span className="text-[13px] font-medium text-neutral-100">
+            {`${selected?.from ? formatDate(selected.from) : 'Start Date'} → ${
+              selected?.to ? formatDate(selected.to) : 'End Date'
+            }`}
+          </span>
+          <div className="bg-neutral-80/5 h-full w-[1px]" />
+          <EditIcon size={20} color="$neutral-80-opa-40" />
+        </button>
+        <Popover.Content>
+          <Calendar mode="range" selected={selected} onSelect={onSelect} />
+        </Popover.Content>
+      </Popover>
+    </div>
+  )
+}
+
+export { DatePicker }
