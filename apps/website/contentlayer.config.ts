26360201f8db7c02ff1e94698d2981c091f3d757
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2023-06-12 17:57:41 +0200

    fix types

--- apps/website/contentlayer.config.ts
+++ apps/website/contentlayer.config.ts
@@ -183,18 +183,18 @@ export default makeSource({
           // keepBackground: true,
           // Callback hooks to add custom logic to nodes when visiting
           // them.
-          onVisitLine(node) {
+          onVisitLine(node: any) {
             // Prevent lines from collapsing in `display: grid` mode, and
             // allow empty lines to be copy/pasted
             if (node.children.length === 0) {
               node.children = [{ type: 'text', value: ' ' }]
             }
           },
-          onVisitHighlightedLine(node) {
+          onVisitHighlightedLine(node: any) {
             // Each line node by default has `class="line"`.
             node.properties.className.push('highlighted')
           },
-          onVisitHighlightedWord(node) {
+          onVisitHighlightedWord(node: any) {
             // Each word node has no className by default.
             node.properties.className = ['word']
           },
