f0ae41f890578d78de5590b74b0d917724a6f9c6
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2023-06-27 13:57:36 +0100

    add size to stickers

--- apps/website/src/config/stickers.ts
+++ apps/website/src/config/stickers.ts
@@ -3,6 +3,7 @@ import grumpyCat from '@images/stickers/grumpy-cat.png'
 import pizza from '@images/stickers/pizza.png'
 import punk from '@images/stickers/punk.png'
 import smudgeCat from '@images/stickers/smudge-cat.png'
+import { StaticImageData } from 'next/image'
 
 export type Sticker = keyof typeof stickers
 
@@ -10,21 +11,31 @@ export const stickers = {
   doge: {
     src: doge,
     alt: 'doge',
+    width: 114,
+    height: 114,
   },
   grumpyCat: {
     src: grumpyCat,
     alt: 'grumpyCat',
+    width: 114,
+    height: 114,
   },
   pizza: {
     src: pizza,
     alt: 'pizza',
+    width: 114,
+    height: 114,
   },
   punk: {
     src: punk,
     alt: 'punk',
+    width: 114,
+    height: 114,
   },
   smudgeCat: {
     src: smudgeCat,
     alt: 'smudgeCat',
+    width: 114,
+    height: 114,
   },
 } as const
