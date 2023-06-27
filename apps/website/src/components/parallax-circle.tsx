9069b9cce2822b204942753097cb99f954f91a4c
Pavel Prichodko <14926950+prichodko@users.noreply.github.com>
2023-06-27 17:42:20 +0100

    update parallax circle

--- apps/website/src/components/parallax-circle.tsx
+++ apps/website/src/components/parallax-circle.tsx
@@ -1,6 +1,4 @@
-// import { customisation } from '@status-im/colors'
-
-import { useParalax } from '@/hooks/use-parallax'
+import { Parallax } from 'react-scroll-parallax'
 
 type ParalaxProps = {
   left?: number
@@ -12,19 +10,19 @@ type ParalaxProps = {
 
 const ParalaxCircle = (props: ParalaxProps) => {
   const { color, ...position } = props
-  const parallax = useParalax(props)
 
   return (
-    <div
+    <Parallax
+      speed={-10}
       style={{
         // '--feature-color': customisation['blue-50'],
         // Fixes the performance issue on safari with filter: blur() to force the browser use GPU acceleration for that particular element instead of the CPU.
         transform: 'translate3d(0, 0, 0)',
         ...position,
-        ...parallax,
+        // ...parallax,
       }}
       className={
-        'pointer-events-none absolute z-[0] h-[676px] w-[676px] rounded-full bg-[var(--feature-color)] opacity-10 blur-[250px] ' +
+        'pointer-events-none absolute z-[0] h-[676px] w-[676px] rounded-full opacity-10 blur-[250px] ' +
         color
       }
     />
