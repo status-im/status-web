8a3cf6ce4814608f0bac2998c9a06c69e1412175
Felicio Mununga <felicio@users.noreply.github.com>
2023-06-28 17:41:35 +0100

    [website] use demo ghost for blog in dev (#428)
    * use demo ghost in dev
    
    * union
    
    * u ci.yml

--- apps/website/src/config/env.client.mjs
+++ apps/website/src/config/env.client.mjs
@@ -1,9 +1,19 @@
 import { z } from 'zod'
 
 export const envSchema = z.object({
+  NEXT_PUBLIC_VERCEL_ENV: z
+    .union([
+      z.literal('production'),
+      z.literal('preview'),
+      z.literal('development'),
+    ])
+    .optional(),
+  NEXT_PUBLIC_GHOST_API_URL: z.string(),
   NEXT_PUBLIC_GHOST_API_KEY: z.string(),
 })
 
 export const clientEnv = envSchema.parse({
+  NEXT_PUBLIC_VERCEL_ENV: process.env.NEXT_PUBLIC_VERCEL_ENV,
+  NEXT_PUBLIC_GHOST_API_URL: process.env.NEXT_PUBLIC_GHOST_API_URL,
   NEXT_PUBLIC_GHOST_API_KEY: process.env.NEXT_PUBLIC_GHOST_API_KEY,
 })
