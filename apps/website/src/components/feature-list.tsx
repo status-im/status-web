e98aec1eab1532ff17464ec2a99e62c78c5bdf7d
Jakub Kotula <520927+jkbktl@users.noreply.github.com>
2023-06-29 10:50:52 +0100

    feature list

--- apps/website/src/components/feature-list.tsx
+++ apps/website/src/components/feature-list.tsx
@@ -1,4 +1,5 @@
 import { Text } from '@status-im/components'
+import { cx } from 'class-variance-authority'
 import Image from 'next/image'
 
 import type { illustrations } from '@/config/illustrations'
@@ -11,25 +12,32 @@ type Item = {
 
 type Props = {
   list: Item[]
+  dark?: boolean
 }
 
 export const FeatureList = (props: Props) => {
-  const { list } = props
+  const { list, dark = false } = props
   return (
-    <div className="container grid grid-cols-1 gap-12 pb-24 pt-12 sm:grid-cols-2 lg:grid-cols-3 lg:gap-20">
-      {list.map(feature => (
-        <div key={feature.title} className="flex flex-col">
-          <Image
-            {...feature.image}
-            alt={feature.image.alt}
-            className="mb-4 ml-0"
-          />
-          <Text size={27} weight="semibold">
-            {feature.title}
-          </Text>
-          <Text size={19}>{feature.description}</Text>
-        </div>
-      ))}
+    <div className="container pb-24 pt-12 lg:m-auto lg:gap-10">
+      <div className="grid grid-cols-1 gap-12 sm:grid-cols-2 lg:-ml-10 lg:grid-cols-3">
+        {list.map(({ title, image, description }, i) => (
+          <div key={title} className="flex flex-col">
+            <Image {...image} alt={image.alt} className="mb-4 ml-10" />
+            <div
+              className={cx(
+                'flex flex-col border-dashed border-l-neutral-30 lg:border-l-[1px] lg:pl-10',
+                (i === 0 || i === 3) && 'lg:border-l-0',
+                dark && 'border-l-neutral-70'
+              )}
+            >
+              <Text size={27} weight="semibold">
+                {title}
+              </Text>
+              <Text size={19}>{description}</Text>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
