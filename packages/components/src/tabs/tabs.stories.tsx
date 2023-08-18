ed7183bef3f48106d2524a4388bc788529d8bc21
Felicio Mununga <felicio@users.noreply.github.com>
2023-08-18 17:15:54 +0200

    u stories

--- packages/components/src/tabs/tabs.stories.tsx
+++ packages/components/src/tabs/tabs.stories.tsx
@@ -3,8 +3,8 @@ import { PlaceholderIcon } from '@status-im/icons'
 import { Text } from '../text'
 import { Tabs } from './tabs'
 
+import type { TabsProps } from './tabs'
 import type { Meta, StoryObj } from '@storybook/react'
-import type { ComponentProps } from 'react'
 
 const meta: Meta<typeof Tabs> = {
   component: Tabs,
@@ -18,40 +18,132 @@ const meta: Meta<typeof Tabs> = {
   },
 }
 
-type Story = StoryObj<{ size: 24 | 32; icon: boolean; count: boolean }>
-
-export const Default: Story = {
-  name: 'Default',
+export const Default: StoryObj<TabsProps> = {
   args: {
-    size: 24,
-    icon: false,
-  },
-  argTypes: {
-    size: {
-      control: 'select',
-      options: [24, 32] satisfies ComponentProps<typeof Tabs.List>['size'][],
-    },
-    icon: {
-      control: 'boolean',
-    },
-    count: {
-      control: 'boolean',
-    },
+    defaultValue: '1',
   },
   render(args) {
-    const icon = args.icon ? <PlaceholderIcon size={20} /> : undefined
-    const count = args.count ? 8 : undefined
+    // const icon = args.icon ? <PlaceholderIcon size={20} /> : undefined
+    // const count = args.count ? 8 : undefined
 
     return (
-      <Tabs defaultValue="1">
-        <Tabs.List size={args.size}>
-          <Tabs.Trigger value="1" icon={icon} count={count}>
+      <Tabs {...args}>
+        <Tabs.List size={32}>
+          <Tabs.Trigger type="default" value="1">
             Tab 1
           </Tabs.Trigger>
-          <Tabs.Trigger value="2" icon={icon}>
+          <Tabs.Trigger type="default" value="2">
             Tab 2
           </Tabs.Trigger>
-          <Tabs.Trigger value="3" icon={icon}>
+          <Tabs.Trigger type="default" value="3">
+            Tab 3
+          </Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="1">
+          <Text size={15}>Content 1</Text>
+        </Tabs.Content>
+        <Tabs.Content value="2">
+          <Text size={15}>Content 2</Text>
+        </Tabs.Content>
+        <Tabs.Content value="3">
+          <Text size={15}>Content 3</Text>
+        </Tabs.Content>
+      </Tabs>
+    )
+  },
+}
+
+export const Icon: StoryObj<TabsProps> = {
+  args: {
+    defaultValue: '1',
+  },
+  render(args) {
+    return (
+      <Tabs {...args}>
+        <Tabs.List size={32}>
+          <Tabs.Trigger
+            type="icon"
+            value="1"
+            icon={<PlaceholderIcon size={16} />}
+          >
+            Tab 1
+          </Tabs.Trigger>
+          <Tabs.Trigger
+            type="icon"
+            value="2"
+            icon={<PlaceholderIcon size={16} />}
+          >
+            Tab 2
+          </Tabs.Trigger>
+          <Tabs.Trigger
+            type="icon"
+            value="3"
+            icon={<PlaceholderIcon size={16} />}
+          >
+            Tab 3
+          </Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="1">
+          <Text size={15}>Content 1</Text>
+        </Tabs.Content>
+        <Tabs.Content value="2">
+          <Text size={15}>Content 2</Text>
+        </Tabs.Content>
+        <Tabs.Content value="3">
+          <Text size={15}>Content 3</Text>
+        </Tabs.Content>
+      </Tabs>
+    )
+  },
+}
+
+export const Counter: StoryObj<TabsProps> = {
+  args: {
+    defaultValue: '1',
+  },
+  render(args) {
+    return (
+      <Tabs {...args}>
+        <Tabs.List size={32}>
+          <Tabs.Trigger type="counter" value="1" count={5}>
+            Tab 1
+          </Tabs.Trigger>
+          <Tabs.Trigger type="counter" value="2" count={10}>
+            Tab 2
+          </Tabs.Trigger>
+          <Tabs.Trigger type="counter" value="3" count={100}>
+            Tab 3
+          </Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="1">
+          <Text size={15}>Content 1</Text>
+        </Tabs.Content>
+        <Tabs.Content value="2">
+          <Text size={15}>Content 2</Text>
+        </Tabs.Content>
+        <Tabs.Content value="3">
+          <Text size={15}>Content 3</Text>
+        </Tabs.Content>
+      </Tabs>
+    )
+  },
+}
+
+export const Step: StoryObj<TabsProps> = {
+  args: {
+    defaultValue: '1',
+  },
+  render(args) {
+    return (
+      <Tabs {...args}>
+        <Tabs.List size={32}>
+          <Tabs.Trigger type="step" value="1" step={1}>
+            Tab 1
+          </Tabs.Trigger>
+          <Tabs.Trigger type="step" value="2" step={2}>
+            Tab 2
+          </Tabs.Trigger>
+          <Tabs.Trigger type="step" value="3" step={3}>
             Tab 3
           </Tabs.Trigger>
         </Tabs.List>
