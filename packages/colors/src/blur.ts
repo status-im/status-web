ed918fc4f9f67863f0aaa2f325539039f44d10af
marcelines <marcio@status.im>
2023-09-28 14:34:11 +0100

    sync colors with figma (#487)
    * fix: sync colors with figma
    
    * feat: add changeset

--- packages/colors/src/blur.ts
+++ packages/colors/src/blur.ts
@@ -1,6 +1,7 @@
 export const blur = {
   'neutral-100/70': 'rgba(9 16 28 / 70%)',
   'neutral-5/70': 'rgba(245 246 248 / 70%)',
+  'neutral-80/1': 'rgba(25 36 56 / 1%)',
   'neutral-80/80': 'rgba(25 36 56 / 80%)',
   'neutral-90/70': 'rgba(19 29 47 / 70%)',
   'neutral-95/70': 'rgba(13 22 37 / 70%)',
