a2956fb69d7375affbff652bf459dc8b980aa88b
Felicio Mununga <felicio@users.noreply.github.com>
2024-01-10 18:18:28 +0100

    u

--- packages/status-js/src/client/client.ts
+++ packages/status-js/src/client/client.ts
@@ -28,7 +28,7 @@ export interface ClientOptions {
    * Public key of a community to join.
    */
   publicKey: string
-  environment?: 'production' | 'test'
+  environment?: 'test' // 'production' | 'test'
   /**
    * Custom storage for data persistance
    * @default window.localStorage
@@ -132,7 +132,7 @@ class Client {
   }
 
   static async start(options: ClientOptions): Promise<Client> {
-    const { environment = 'production' } = options
+    const { environment = 'test' } = options
 
     let waku: LightNode | undefined
     let client: Client | undefined
--- packages/status-js/src/request-client/request-client.ts
+++ packages/status-js/src/request-client/request-client.ts
@@ -29,7 +29,7 @@ import type { LightNode } from '@waku/interfaces'
 import type { DecodedMessage } from '@waku/message-encryption/symmetric'
 
 export interface RequestClientOptions {
-  environment?: 'production' | 'test'
+  environment?: 'test' // 'production' | 'test'
 }
 
 class RequestClient {
@@ -46,7 +46,7 @@ class RequestClient {
   }
 
   static async start(options: RequestClientOptions): Promise<RequestClient> {
-    const { environment = 'production' } = options
+    const { environment = 'test' } = options
 
     let waku: LightNode | undefined
     let client: RequestClient | undefined
@@ -58,8 +58,8 @@ class RequestClient {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         emitSelf: true,
-        pingKeepAlive: 0,
-        relayKeepAlive: 0,
+        // pingKeepAlive: 0,
+        // relayKeepAlive: 0,
         libp2p: {
           peerDiscovery: [
             bootstrap({
@@ -70,6 +70,7 @@ class RequestClient {
             }),
           ],
         },
+        pubsubTopics: ['/waku/2/rs/16/32', '/waku/2/default-waku/proto'],
       })
       await waku.start()
       await waitForRemotePeer(waku, [Protocols.Store], 10 * 1000)
@@ -150,7 +151,7 @@ class RequestClient {
 
     let communityDescription: CommunityDescription | undefined = undefined
     await this.waku.store.queryWithOrderedCallback(
-      [createDecoder(contentTopic, symmetricKey)],
+      [createDecoder(contentTopic, symmetricKey, '/waku/2/rs/16/32')],
       wakuMessage => {
         // handle
         const message = this.handleWakuMessage(wakuMessage)
@@ -207,7 +208,7 @@ class RequestClient {
     let contactCodeAdvertisement: ContactCodeAdvertisement | undefined =
       undefined
     await this.waku.store.queryWithOrderedCallback(
-      [createDecoder(contentTopic, symmetricKey)],
+      [createDecoder(contentTopic, symmetricKey, '/waku/2/rs/16/32')],
       wakuMessage => {
         // handle
         const message = this.handleWakuMessage(wakuMessage)
