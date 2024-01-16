7a45631aae8a5739666bddcda101bda6f06b4752
Pavel <14926950+prichodko@users.noreply.github.com>
2024-01-16 18:19:54 +0100

    add typography tokens

---
+++ packages/components/src/_tokens/typography.ts
@@ -0,0 +1,42 @@
+export const typography = {
+  88: {
+    fontSize: '5.5rem',
+    lineHeight: '5.25rem',
+    letterSpacing: ' -0.1155rem',
+  },
+  64: {
+    fontSize: '4rem',
+    lineHeight: '4.25rem',
+    letterSpacing: '-0.08rem',
+  },
+  40: {
+    fontSize: '2.5rem',
+    lineHeight: '2.75rem',
+    letterSpacing: '-0.05rem',
+  },
+  27: {
+    fontSize: '1.6875rem',
+    lineHeight: '2rem',
+    letterSpacing: '-0.035rem',
+  },
+  19: {
+    fontSize: '1.1875rem',
+    lineHeight: '1.75rem',
+    letterSpacing: '-0.019rem',
+  },
+  15: {
+    fontSize: '0.9375rem',
+    lineHeight: '1.359375rem',
+    // letterSpacing: '-0.0084375rem',
+  },
+  13: {
+    fontSize: '0.8125rem',
+    lineHeight: '1.1375rem',
+    // letterSpacing: '-0.0024375rem',
+  },
+  11: {
+    fontSize: '0.6875rem',
+    lineHeight: '0.97625rem',
+    // letterSpacing: '-0.0034375rem',
+  },
+} as const
