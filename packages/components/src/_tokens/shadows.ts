286a886df19638a535e1cb2445c00dbb83df002c
Pavel <14926950+prichodko@users.noreply.github.com>
2024-01-16 18:20:03 +0100

    add shadows tokens

---
+++ packages/components/src/_tokens/shadows.ts
@@ -0,0 +1,14 @@
+export const shadows = {
+  light: {
+    1: '0px 2px 20px 0px rgba(9, 16, 28, 0.04)',
+    2: '0px 4px 20px 0px rgba(9, 16, 28, 0.08)',
+    3: '0px 8px 30px 0px rgba(9, 16, 28, 0.12)',
+    4: '0px 12px 56px 0px rgba(9, 16, 28, 0.16)',
+  },
+  dark: {
+    1: '0px 4px 40px 0px rgba(9, 16, 28, 0.50)',
+    2: '0px 8px 40px 0px rgba(9, 16, 28, 0.64)',
+    3: '0px 12px 50px 0px rgba(9, 16, 28, 0.64)',
+    4: '0px 16px 64px 0px rgba(9, 16, 28, 0.72)',
+  },
+}
