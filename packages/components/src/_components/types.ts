9d3b4b0580113556102ae0a798592cd6e938c6f1
Pavel <14926950+prichodko@users.noreply.github.com>
2024-01-31 15:16:24 +0100

    f storybook types

--- packages/components/src/_components/types.ts
+++ packages/components/src/_components/types.ts
@@ -1,1 +1,11 @@
+import {} from '@storybook/react'
+
+declare module '@storybook/react' {
+  export interface Parameters {
+    backgrounds?: {
+      default: 'light' | 'dark'
+    }
+  }
+}
+
 export type * as Aria from 'react-aria-components'
