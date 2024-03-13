70302983b198cd4ffead086e59be20b893db5011
Felicio Mununga <felicio@users.noreply.github.com>
2024-03-13 17:51:49 +0900

    t

--- packages/status-js/.scripts/debug/index.ts
+++ packages/status-js/.scripts/debug/index.ts
@@ -18,8 +18,9 @@ process.once('SIGINT', async () => {
 const deserializedPublicKey = deserializePublicKey(
   // '0x0269e4e75683762fc5d8af53ee1d1656767b780519a5bd1ecb4bd34d482b100fd7'
   // '0x029f196bbfef4fa6a5eb81dd802133a63498325445ca1af1d154b1bb4542955133'
-  'zQ3shf8EvhRKHSc3skgtupULuLdWh59S5czZkvS69P6rJ7aed'
+  // 'zQ3shf8EvhRKHSc3skgtupULuLdWh59S5czZkvS69P6rJ7aed'
   // 'zQ3shVhrvjfYRfh6unhUpapUNuVHh7BAL3Qx3G7oh6wkYDM7s'
+  'zQ3shgz9XWZerfqsSpR2XKjfyiwtyiRNV6whj6BXREwnuLo5P'
 )
 const detail = await requestClient.fetchCommunityDescription(
   deserializedPublicKey
