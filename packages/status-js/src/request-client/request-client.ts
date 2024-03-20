05155578b10b88098d749a36fe556a62e977aad5
Felicio Mununga <felicio@users.noreply.github.com>
2024-03-20 16:24:25 +0900

    a

--- packages/status-js/src/request-client/request-client.ts
+++ packages/status-js/src/request-client/request-client.ts
@@ -5,7 +5,9 @@ import { createLightNode, waitForRemotePeer } from '@waku/sdk'
 import { bytesToHex } from 'ethereum-cryptography/utils'
 
 import { isEncrypted } from '../client/community/is-encrypted'
+import { contracts } from '../consts/contracts'
 import { peers } from '../consts/peers'
+import { providers } from '../consts/providers'
 import { EthereumClient } from '../ethereum-client/ethereum-client'
 import {
   ApplicationMetadataMessage,
@@ -22,7 +24,6 @@ import { generateKeyFromPassword } from '../utils/generate-key-from-password'
 import { idToContentTopic } from '../utils/id-to-content-topic'
 import { isClockValid } from '../utils/is-clock-valid'
 import { payloadToId } from '../utils/payload-to-id'
-import { publicKeyToETHAddress } from '../utils/public-key-to-eth-address'
 import { recoverPublicKey } from '../utils/recover-public-key'
 import { mapChannel } from './map-channel'
 import { mapCommunity } from './map-community'
@@ -35,7 +36,8 @@ import type { UserInfo } from './map-user'
 import type { LightNode } from '@waku/interfaces'
 
 export interface RequestClientOptions {
-  environment?: 'test' // 'production' | 'test'
+  ethProviderApiKey: string
+  // environment?: 'development' | 'preview' | 'production'
 }
 
 class RequestClient {
@@ -43,16 +45,30 @@ class RequestClient {
   /** Cache. */
   public readonly wakuMessages: Set<string>
 
-  private started: boolean
+  #started: boolean
+  #providerUrls: Record<number, string>
+  #ethProviderApiKey: string
+  #contractsAddress: Record<number, Record<string, string>>
 
-  constructor(waku: LightNode, started = false) {
+  constructor(
+    waku: LightNode,
+    options: {
+      providerUrls: Record<number, string>
+      ethProviderApiKey: string
+      contractsAddress: Record<number, Record<string, string>>
+      started?: boolean
+    }
+  ) {
     this.waku = waku
     this.wakuMessages = new Set()
-    this.started = started
+    this.#started = options.started ?? false
+    this.#providerUrls = options.providerUrls
+    this.#ethProviderApiKey = options.ethProviderApiKey
+    this.#contractsAddress = options.contractsAddress
   }
 
   static async start(options: RequestClientOptions): Promise<RequestClient> {
-    const { environment = 'test' } = options
+    // const { environment = 'development' } = options
 
     let waku: LightNode | undefined
     let client: RequestClient | undefined
@@ -69,7 +85,7 @@ class RequestClient {
         libp2p: {
           peerDiscovery: [
             bootstrap({
-              list: peers[environment],
+              list: peers['production'],
               timeout: 0,
               // note: Infinity prevents connection
               // tagTTL: Infinity,
@@ -84,8 +100,12 @@ class RequestClient {
       await waku.start()
       await waitForRemotePeer(waku, [Protocols.Store], 10 * 1000)
 
-      const started = true
-      client = new RequestClient(waku, started)
+      client = new RequestClient(waku, {
+        started: true,
+        providerUrls: providers['production'].infura,
+        ethProviderApiKey: options.ethProviderApiKey,
+        contractsAddress: contracts['production'],
+      })
     } catch (error) {
       if (waku) {
         await waku.stop()
@@ -98,13 +118,13 @@ class RequestClient {
   }
 
   public async stop() {
-    if (!this.started) {
+    if (!this.#started) {
       throw new Error('Waku instance not created by class initialization')
     }
 
     await this.waku.stop()
 
-    this.started = false
+    this.#started = false
   }
 
   public fetchCommunity = async (
@@ -153,13 +173,14 @@ class RequestClient {
 
   public fetchCommunityDescription = async (
     /** Compressed */
-    publicKey: string
+    communityPublicKey: string
   ): Promise<CommunityDescription | undefined> => {
-    const contentTopic = idToContentTopic(publicKey)
-    const symmetricKey = await generateKeyFromPassword(publicKey)
+    const contentTopic = idToContentTopic(communityPublicKey)
+    const symmetricKey = await generateKeyFromPassword(communityPublicKey)
 
     let communityDescription: CommunityDescription | undefined = undefined
     try {
+      // todo: use queryGenerator() instead
       await this.waku.store.queryWithOrderedCallback(
         [
           createDecoder(contentTopic, symmetricKey, {
@@ -167,7 +188,7 @@ class RequestClient {
             shard: 32,
           }),
         ],
-        wakuMessage => {
+        async wakuMessage => {
           // handle
           const message = this.handleWakuMessage(wakuMessage)
 
@@ -197,12 +218,9 @@ class RequestClient {
             return
           }
 
-          const signerPublicKey = `0x${compressPublicKey(
-            message.signerPublicKey
-          )}`
-
           // isSignatureValid
           if (isEncrypted(decodedCommunityDescription.tokenPermissions)) {
+            // todo?: zod
             const permission = Object.values(
               decodedCommunityDescription.tokenPermissions
             ).find(
@@ -210,28 +228,41 @@ class RequestClient {
                 permission.type ===
                 CommunityTokenPermission_Type.BECOME_TOKEN_OWNER
             )
+
             if (!permission) {
               return
             }
+
             const criteria = permission.tokenCriteria[0]
             const contracts = criteria?.contractAddresses
             const chainId = Object.keys(contracts)[0]
+
             if (!chainId) {
               return
             }
-            // get client config based on chainId
-            // get client
-            const client = new EthereumClient(
-              `https://mainnet.infura.io/v3/${process.env.KEY}`
-            )
-            // call status contract for chainId
-            const address = publicKeyToETHAddress(publicKey)
-            // call contracts from previous call with address
-            const ownerPublicKey = '0x0'
-            if (ownerPublicKey !== signerPublicKey) {
+
+            const providerUrl = this.#providerUrls[Number(chainId)]
+
+            if (!providerUrl) {
               return
             }
-          } else if (publicKey !== signerPublicKey) {
+
+            const client = new EthereumClient(
+              providerUrl + this.#ethProviderApiKey
+            )
+            const ownerPublicKey = await client.resolveOwner(
+              this.#contractsAddress[Number(chainId)]
+                .CommunityOwnerTokenRegistry,
+              communityPublicKey
+            )
+
+            if (ownerPublicKey !== message.signerPublicKey) {
+              return
+            }
+          } else if (
+            communityPublicKey !==
+            `0x${compressPublicKey(message.signerPublicKey)}`
+          ) {
             return
           }
 
