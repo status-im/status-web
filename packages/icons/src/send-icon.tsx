a023653cc60d3a60a415e204a63ca80138b3a3a7
pavel <14926950+prichodko@users.noreply.github.com>
2024-06-17 13:56:58 +0200

    sync icons (#573)

--- packages/icons/src/send-icon.tsx
+++ packages/icons/src/send-icon.tsx
@@ -15,7 +15,7 @@ const SvgSendIcon = createIcon(props => {
       <path
         fill={props.color}
         fillRule="evenodd"
-        d="m10 3.69.404.366 5.5 5-.807.888-5.096-4.633-5.097 4.633-.807-.888 5.5-5 .404-.367Z"
+        d="m10 4.69.404.366 5.5 5-.807.888-5.096-4.633-5.097 4.633-.807-.888 5.5-5 .404-.367Z"
         clipRule="evenodd"
       />
       <mask
@@ -23,7 +23,7 @@ const SvgSendIcon = createIcon(props => {
         width={2}
         height={13}
         x={9}
-        y={4}
+        y={5}
         maskUnits="userSpaceOnUse"
         style={{
           maskType: 'alpha',
@@ -31,20 +31,20 @@ const SvgSendIcon = createIcon(props => {
       >
         <path
           fill="url(#send-icon_svg__a)"
-          d="M11 16.5h2v12h-2z"
-          transform="rotate(-180 11 16.5)"
+          d="M11 17.5h2v12h-2z"
+          transform="rotate(-180 11 17.5)"
         />
       </mask>
       <g mask="url(#send-icon_svg__b)">
-        <path stroke={props.color} strokeWidth={1.2} d="M10 16.5V5" />
+        <path stroke={props.color} strokeWidth={1.2} d="M10 17.5V6" />
       </g>
       <defs>
         <linearGradient
           id="send-icon_svg__a"
           x1={12}
           x2={12}
-          y1={17}
-          y2={22.5}
+          y1={18}
+          y2={23.5}
           gradientUnits="userSpaceOnUse"
         >
           <stop stopColor={props.color} stopOpacity={0} />
