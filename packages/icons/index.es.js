49575819a91ff9797d8e4d6036f11707e2f8e062
Pavel <14926950+prichodko@users.noreply.github.com>
2024-09-10 16:06:55 +0200

    clean up

---
+++ packages/icons/index.es.js
@@ -0,0 +1,16 @@
+// The only reason this file exists is to appease Vite's optimizeDeps feature which requires a root-level import.
+
+export default new Proxy(
+  {},
+  {
+    get: (_, property) => {
+      if (property === '__esModule') {
+        return {}
+      }
+
+      throw new Error(
+        `Importing from \`@status-im/icons\` directly is not supported. Please import from either \`@status-im/icons/12\`, \`@status-im/icons/16\`, \`@status-im/icons/20\`, \`@status-im/icons/reactions\` or \`@status-im/icons/social\` instead.`
+      )
+    },
+  }
+)
---
+++ packages/icons/index.js
@@ -0,0 +1,14 @@
+module.exports = new Proxy(
+  {},
+  {
+    get: (_, property) => {
+      if (property === '__esModule') {
+        return {}
+      }
+
+      throw new Error(
+        `Importing from \`@status-im/icons\` directly is not supported. Please import from either \`@status-im/icons/12\`, \`@status-im/icons/16\`, \`@status-im/icons/20\`, \`@status-im/icons/reactions\`, or \`@status-im/icons/social\` instead.`
+      )
+    },
+  }
+)
