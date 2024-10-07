c2b695ac0c587695d4062eb77a7f94a6c27b8a56
marcelines <marcio@status.im>
2024-10-07 09:45:27 +0100

    f

--- packages/components/src/segmented-control/segmented-control.stories.tsx
+++ packages/components/src/segmented-control/segmented-control.stories.tsx
@@ -1,36 +1,240 @@
-import { useState } from 'react'
+import { createElement, memo, useState } from 'react'
+
+import { BuyIcon, GasIcon } from '@status-im/icons/20'
+import { match } from 'ts-pattern'
 
 import { SegmentedControl } from './segmented-control'
 
 import type { Meta, StoryObj } from '@storybook/react'
 
-const SegmentedControlVariant = (
-  props: React.ComponentPropsWithoutRef<typeof SegmentedControl.Root>,
-) => {
-  const [activeSegment, setActiveSegment] = useState('Option A')
-
-  return (
-    <SegmentedControl.Root
-      {...props}
-      activeSegment={activeSegment}
-      onSegmentChange={setActiveSegment}
-    >
-      <SegmentedControl.Button>Option A</SegmentedControl.Button>
-      <SegmentedControl.Button>Option B</SegmentedControl.Button>
-      <SegmentedControl.Button>Option C</SegmentedControl.Button>
-    </SegmentedControl.Root>
-  )
+type Data = {
+  value: string
+  label: string
+  icon?: React.ComponentType<React.SVGProps<SVGSVGElement>>
+  emoji?: string
 }
+
+const SegmentedControlVariant = memo(
+  (
+    props: React.ComponentPropsWithoutRef<typeof SegmentedControl.Root> & {
+      variant?:
+        | 'default'
+        | 'icon-text'
+        | 'emoji-text'
+        | 'icon-only'
+        | 'emoji-only'
+      data: Data[]
+    },
+  ) => {
+    const [activeSegment, setActiveSegment] = useState(props.data[0].value)
+
+    const { variant = 'default', data, size, type } = props
+
+    return (
+      <SegmentedControl.Root
+        activeSegment={activeSegment}
+        onSegmentChange={setActiveSegment}
+        size={size}
+        type={type}
+      >
+        {match(variant)
+          .with('icon-text', () =>
+            data.map(({ value, icon, label }) => {
+              const iconElement = icon ? createElement(icon) : null
+
+              return (
+                <SegmentedControl.IconButton
+                  icon={iconElement}
+                  key={value}
+                  value={value}
+                >
+                  {label}
+                </SegmentedControl.IconButton>
+              )
+            }),
+          )
+          .with('emoji-text', () =>
+            data.map(({ value, emoji, label }) => (
+              <SegmentedControl.EmojiButton
+                emoji={emoji as string}
+                key={value}
+                value={value}
+              >
+                {label}
+              </SegmentedControl.EmojiButton>
+            )),
+          )
+          .with('icon-only', () =>
+            data.map(({ value, icon }) => {
+              const iconElement = icon ? createElement(icon) : null
+
+              return (
+                <SegmentedControl.IconButton
+                  icon={iconElement}
+                  key={value}
+                  value={value}
+                />
+              )
+            }),
+          )
+          .with('emoji-only', () =>
+            data.map(({ value, emoji }) => (
+              <SegmentedControl.EmojiButton
+                emoji={emoji as string}
+                key={value}
+                value={value}
+              />
+            )),
+          )
+          .otherwise(() =>
+            data.map(({ value, label }) => (
+              <SegmentedControl.Button key={value} value={value}>
+                {label}
+              </SegmentedControl.Button>
+            )),
+          )}
+      </SegmentedControl.Root>
+    )
+  },
+)
+
+SegmentedControlVariant.displayName = 'SegmentedControlVariant'
 
 const meta = {
   title: 'Components/SegmentedControl',
   render: args => {
+    const textData = [
+      { value: 'option-a', label: 'Option A' },
+      { value: 'option-b', label: 'Option B' },
+      { value: 'option-c', label: 'Option C' },
+    ]
+
+    const iconData = [
+      { value: 'buy', label: 'Buy', icon: BuyIcon },
+      { value: 'gas', label: 'Gas', icon: GasIcon },
+    ]
+
+    const emojiData = [
+      { value: 'apple', label: 'Apple', emoji: '🍎' },
+      { value: 'orange', label: 'Orange', emoji: '🍊' },
+      { value: 'grapes', label: 'Grapes', emoji: '🍇' },
+    ]
     return (
       <div className="inline-flex flex-col gap-4">
-        <SegmentedControlVariant {...args} />
-        <SegmentedControlVariant {...args} size="24" />
-        <SegmentedControlVariant {...args} type="dark-grey" />
-        <SegmentedControlVariant {...args} type="dark-grey" size="24" />
+        {/* Only text */}
+        <SegmentedControlVariant {...args} data={textData} />
+        <SegmentedControlVariant {...args} size="24" data={textData} />
+        <SegmentedControlVariant {...args} type="dark-grey" data={textData} />
+        <SegmentedControlVariant
+          {...args}
+          type="dark-grey"
+          size="24"
+          data={textData}
+        />
+
+        {/* Only icon */}
+        <SegmentedControlVariant
+          {...args}
+          variant="icon-only"
+          data={iconData}
+        />
+        <SegmentedControlVariant
+          {...args}
+          variant="icon-only"
+          size="24"
+          data={iconData}
+        />
+        <SegmentedControlVariant
+          {...args}
+          variant="icon-only"
+          type="dark-grey"
+          data={iconData}
+        />
+        <SegmentedControlVariant
+          {...args}
+          variant="icon-only"
+          type="dark-grey"
+          size="24"
+          data={iconData}
+        />
+
+        {/* Only emoji */}
+        <SegmentedControlVariant
+          {...args}
+          variant="emoji-only"
+          data={emojiData}
+        />
+        <SegmentedControlVariant
+          {...args}
+          variant="emoji-only"
+          size="24"
+          data={emojiData}
+        />
+        <SegmentedControlVariant
+          {...args}
+          variant="emoji-only"
+          type="dark-grey"
+          data={emojiData}
+        />
+        <SegmentedControlVariant
+          {...args}
+          variant="emoji-only"
+          type="dark-grey"
+          size="24"
+          data={emojiData}
+        />
+
+        {/* Icon and text */}
+        <SegmentedControlVariant
+          {...args}
+          variant="icon-text"
+          data={iconData}
+        />
+        <SegmentedControlVariant
+          {...args}
+          size="24"
+          variant="icon-text"
+          data={iconData}
+        />
+        <SegmentedControlVariant
+          {...args}
+          type="dark-grey"
+          variant="icon-text"
+          data={iconData}
+        />
+        <SegmentedControlVariant
+          {...args}
+          type="dark-grey"
+          size="24"
+          variant="icon-text"
+          data={iconData}
+        />
+
+        {/* Emoji and text */}
+        <SegmentedControlVariant
+          {...args}
+          variant="emoji-text"
+          data={emojiData}
+        />
+        <SegmentedControlVariant
+          {...args}
+          size="24"
+          variant="emoji-text"
+          data={emojiData}
+        />
+        <SegmentedControlVariant
+          {...args}
+          type="dark-grey"
+          variant="emoji-text"
+          data={emojiData}
+        />
+        <SegmentedControlVariant
+          {...args}
+          type="dark-grey"
+          size="24"
+          variant="emoji-text"
+          data={emojiData}
+        />
       </div>
     )
   },
--- packages/components/src/segmented-control/segmented-control.tsx
+++ packages/components/src/segmented-control/segmented-control.tsx
@@ -13,9 +13,7 @@ import { cva } from 'cva'
 import type { ReactElement, ReactNode } from 'react'
 
 const tabContainerStyles = cva({
-  base: [
-    'relative inline-flex items-center justify-center gap-0.5 rounded-10 p-0.5',
-  ],
+  base: 'relative flex flex-1 items-center justify-center gap-0.5 rounded-10 p-0.5',
   variants: {
     type: {
       grey: 'bg-neutral-10 blur:bg-neutral-80/5 blur:backdrop-blur-[20px] dark:bg-neutral-80 blur:dark:bg-white-5',
@@ -29,37 +27,38 @@ const tabContainerStyles = cva({
 })
 
 const tabStyles = cva({
-  base: ['absolute left-0 rounded-8 transition-all duration-300 ease-out'],
+  base: 'absolute left-0 flex-1 rounded-8 transition-all duration-300 ease-out',
   variants: {
     type: {
       grey: 'bg-neutral-50 blur:bg-neutral-80/60 dark:bg-neutral-60',
       'dark-grey': 'bg-neutral-50 dark:bg-neutral-60',
     },
     size: {
-      '24': 'h-[20px]',
-      '32': 'h-[28px]',
+      '24': 'h-5',
+      '32': 'h-7',
     },
   },
 })
 
 const segmentStyles = cva({
-  base: [
-    'relative z-10 flex select-none items-center font-medium transition-all duration-300 ease-out',
-  ],
+  base: 'relative z-10 flex h-full flex-1 select-none items-center justify-center whitespace-nowrap rounded-8 font-medium transition-all duration-300 ease-out',
+
   variants: {
     active: {
       true: 'text-white-100',
-      false: 'bg-transparent text-neutral-100 dark:text-white-100',
+      false:
+        'bg-transparent text-neutral-80 hover:bg-neutral-20 dark:text-neutral-40 dark:hover:bg-neutral-70 dark:hover:text-white-100',
     },
-
     variant: {
       default: '',
-      emoji: '',
+      emoji: 'gap-1',
+      'emoji-only': '',
       icon: 'gap-1',
+      'icon-only': '',
     },
     size: {
-      '24': 'text-13',
-      '32': 'text-15',
+      '24': 'h-5 text-13',
+      '32': 'h-7 text-15',
     },
   },
   compoundVariants: [
@@ -84,6 +83,16 @@ const segmentStyles = cva({
       className: 'pl-[10px] pr-3',
     },
     {
+      variant: 'emoji-only',
+      size: '24',
+      className: 'px-1',
+    },
+    {
+      variant: 'emoji-only',
+      size: '32',
+      className: 'px-1.5',
+    },
+    {
       variant: 'icon',
       size: '24',
       className: 'pl-[6px] pr-2',
@@ -93,11 +102,22 @@ const segmentStyles = cva({
       size: '32',
       className: 'pl-[10px] pr-3',
     },
+    {
+      variant: 'icon-only',
+      size: '24',
+      className: 'px-1',
+    },
+    {
+      variant: 'icon-only',
+      size: '32',
+      className: 'px-1.5',
+    },
   ],
 })
 
 type SegmentButtonProps = {
-  children: ReactNode
+  children?: ReactNode
+  value?: string | number
   onClick?: () => void
   active?: boolean
   size?: '24' | '32'
@@ -119,17 +139,26 @@ Button.displayName = 'Button'
 
 const IconButton = forwardRef<
   HTMLButtonElement,
-  SegmentButtonProps & { icon: ReactNode }
->(({ children, icon, onClick, active, size = '32' }, ref) => {
+  SegmentButtonProps & {
+    icon: ReactNode
+    value?: string | number
+  }
+>(({ icon, children, value, onClick, active, size = '32' }, ref) => {
   const iconWithCurrentColor = cloneElement(icon as ReactElement, {
     color: 'currentColor',
+    className: 'size-4',
   })
 
   return (
     <button
       ref={ref}
       onClick={onClick}
-      className={segmentStyles({ active, variant: 'icon', size })}
+      className={segmentStyles({
+        active,
+        variant: children ? 'icon' : 'icon-only',
+        size,
+      })}
+      value={value}
     >
       {iconWithCurrentColor}
       {children}
@@ -141,12 +170,20 @@ IconButton.displayName = 'IconButton'
 
 const EmojiButton = forwardRef<
   HTMLButtonElement,
-  SegmentButtonProps & { emoji: string }
->(({ children, emoji, onClick, active, size = '32' }, ref) => (
+  SegmentButtonProps & {
+    emoji: string
+    value?: string | number
+  }
+>(({ children, emoji, onClick, active, size = '32', value }, ref) => (
   <button
     ref={ref}
     onClick={onClick}
-    className={segmentStyles({ active, variant: 'emoji', size })}
+    className={segmentStyles({
+      active,
+      variant: children ? 'emoji' : 'emoji-only',
+      size,
+    })}
+    value={value}
   >
     {emoji} {children}
   </button>
@@ -176,7 +213,11 @@ const SegmentedControl = <T extends string | number>(props: Props<T>) => {
 
   useEffect(() => {
     const activeSegmentIndex = Children.toArray(children).findIndex(
-      child => isValidElement(child) && child.props.children === activeSegment,
+      child =>
+        isValidElement(child) &&
+        (child.props.value
+          ? child.props.value === activeSegment
+          : child.props.children === activeSegment),
     )
 
     const activeSegmentRef = segmentRefs.current[activeSegmentIndex]
@@ -194,19 +235,20 @@ const SegmentedControl = <T extends string | number>(props: Props<T>) => {
       isValidElement(child) &&
       cloneElement(child, {
         ...child.props,
-        onClick: () => onSegmentChange(child.props.children),
-        active: child.props.children === activeSegment,
+        onClick: () =>
+          onSegmentChange(child.props.value || child.props.children),
+        active: child.props.value
+          ? child.props.value === activeSegment
+          : child.props.children === activeSegment,
         ref: (el: HTMLButtonElement) => (segmentRefs.current[index] = el),
         size,
       }),
   )
 
   return (
-    <div>
-      <div className={tabContainerStyles({ size, type })}>
-        <div className={tabStyles({ size, type })} style={indicatorStyle} />
-        {clonedChildren}
-      </div>
+    <div className={tabContainerStyles({ size, type })}>
+      <div className={tabStyles({ size, type })} style={indicatorStyle} />
+      {clonedChildren}
     </div>
   )
 }
