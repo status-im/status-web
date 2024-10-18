e30555c865f5f1acbe1c15d5c3408fbacafdfff7
Jakub Kotula <520927+jkbktl@users.noreply.github.com>
2024-10-18 13:18:57 +0200

    fix `<DropdownMenu.SubTrigger>` icon; unknown network typo (#629)

---
+++ .changeset/polite-cobras-accept.md
@@ -0,0 +1,5 @@
+---
+"@status-im/components": patch
+---
+
+fix <DropdownMenu.SubTrigger /> icon; unknown network typo
--- packages/colors/src/networks.ts
+++ packages/colors/src/networks.ts
@@ -4,7 +4,7 @@ export const networks = {
   hermez: 'rgba(235 132 98 / 100%)',
   optimism: 'rgba(231 110 110 / 100%)',
   polygon: 'rgba(173 113 243 / 100%)',
-  unknow: 'rgba(238 242 245 / 100%)',
+  unknown: 'rgba(238 242 245 / 100%)',
   'x-dai': 'rgba(63 192 189 / 100%)',
   'zk-sync': 'rgba(159 160 254 / 100%)',
 }
