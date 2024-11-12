8a800d3cf9ab5e0db2e7313d8a21edaf35ef55b7
Felicio Mununga <felicio@users.noreply.github.com>
2024-11-12 12:32:39 +0100

    r

--- packages/components/src/button/button.tsx
+++ packages/components/src/button/button.tsx
@@ -88,7 +88,7 @@ function Button(
 
 const styles = cva({
   base: [
-    'inline-flex shrink-0 cursor-pointer select-none items-center justify-center gap-1 border-4 text-center font-medium transition-all',
+    'inline-flex shrink-0 cursor-pointer select-none items-center justify-center gap-1 text-center font-medium transition-all',
     'outline-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-customisation-50 focus-visible:ring-offset-2 dark:focus-visible:ring-offset-neutral-100',
     'disabled:pointer-events-none disabled:cursor-default disabled:opacity-[.3]',
   ],
