dd40535d7f6d78fcd6677e9bf0a00de1ce6e658c
Stasn13 <stasn139@gmail.com>
2025-06-05 14:06:12 +0100

    basic implementation of textarea

---
+++ packages/components/src/textarea/textarea.stories.tsx
@@ -0,0 +1,52 @@
+import { Textarea } from './textarea'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+type Component = typeof Textarea
+
+const meta: Meta<Component> = {
+  title: 'Components/Textarea',
+  component: Textarea,
+  args: {
+    placeholder: 'Type something...',
+  },
+
+  render: props => (
+    <div className="flex w-[300px] flex-col gap-3">
+      <h3 className="mb-2 text-15 font-medium">Basic Inputs</h3>
+      <Textarea {...props} />
+      <Textarea {...props} size="32" />
+
+      <h3 className="mb-2 mt-4 text-15 font-medium">Clearable Inputs</h3>
+      <Textarea {...props} clearable />
+      <Textarea {...props} clearable size="32" />
+
+      <h3 className="mb-2 mt-4 text-15 font-medium">Special States</h3>
+      <Textarea {...props} isDisabled />
+      <Textarea {...props} isInvalid />
+
+      <h3 className="mb-2 mt-4 text-15 font-medium">
+        Inputs with Label and Meta
+      </h3>
+      <Textarea {...props} label="Label" />
+      <Textarea {...props} maxLength={280} />
+      <Textarea {...props} label="Label" maxLength={280} />
+    </div>
+  ),
+}
+
+type Story = StoryObj<Component>
+
+export const Light: Story = {
+  args: {},
+}
+
+export const Dark: Story = {
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+    },
+  },
+}
+
+export default meta
