e76e73a192e9ca1fcce82d71c8d669d673811d44
Stasn13 <stasn139@gmail.com>
2025-06-14 00:30:55 +0100

    add bip39 package and resolve unsued wordlists

--- apps/wallet/vite.config.ts
+++ apps/wallet/vite.config.ts
@@ -7,5 +7,17 @@ export default defineConfig(() => {
     test: {
       // environment: 'happy-dom',
     },
+    // optimise bundle size by removing unused wordlists
+    resolve: {
+      alias: {
+        'bip39/wordlists/chinese_simplified.json': 'node_modules/empty-module',
+        'bip39/wordlists/chinese_traditional.json': 'node_modules/empty-module',
+        'bip39/wordlists/french.json': 'node_modules/empty-module',
+        'bip39/wordlists/italian.json': 'node_modules/empty-module',
+        'bip39/wordlists/japanese.json': 'node_modules/empty-module',
+        'bip39/wordlists/korean.json': 'node_modules/empty-module',
+        'bip39/wordlists/spanish.json': 'node_modules/empty-module',
+      },
+    },
   }
 })
