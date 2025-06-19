39bd946401bd3fb39ae6e971b32c699d4fdd85b8
JulesFILIOT <dev@julesfiliot.com>
2025-06-19 17:40:32 +0900

    feat(lifi-widget): implement ExchangeDialog and integrate with Token component

--- packages/wallet/src/components/lifi-widget/index.tsx
+++ packages/wallet/src/components/lifi-widget/index.tsx
@@ -1,19 +1,21 @@
 import { LiFiWidget, type WidgetConfig } from '@lifi/widget'
+import { useConfig } from 'wagmi'
 
 const LifiWidget = () => {
+  const { chains } = useConfig()
+
   const widgetConfig: WidgetConfig = {
     integrator: 'status-wallet',
     appearance: 'light',
+    chains: {
+      allow: chains.map(chain => chain.id),
+    },
     theme: {
       container: {
         boxShadow: '0px 8px 32px rgba(0, 0, 0, 0.03)',
         borderRadius: '16px',
       },
     },
-    // walletManagement: {
-    // TODO: Add external wallet management
-    //   signer: undefined,
-    // },
   }
 
   return <LiFiWidget {...widgetConfig} />
