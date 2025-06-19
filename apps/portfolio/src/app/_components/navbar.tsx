481b13d2de4f4510c1975b023c7a2a9cd512767a
Felicio <felicio@users.noreply.github.com>
2025-06-19 20:14:54 +0900

    f

--- apps/portfolio/src/app/_components/navbar.tsx
+++ apps/portfolio/src/app/_components/navbar.tsx
@@ -1,12 +1,9 @@
 'use client'
 
 import { Navbar as NavbarBase } from '@status-im/wallet/components'
-import { usePathname } from 'next/navigation'
 
 const Navbar = () => {
-  const pathname = usePathname()
-
-  return <NavbarBase pathname={pathname} />
+  return <NavbarBase />
 }
 
 export { Navbar }
