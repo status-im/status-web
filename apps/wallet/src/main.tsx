d4aba6d7af1902623041628d4a5fd574b334d8cb
Stasn13 <stasn139@gmail.com>
2025-06-20 20:44:04 +0100

    add Buffer to establish correct bip32 lib work

--- apps/wallet/src/main.tsx
+++ apps/wallet/src/main.tsx
@@ -2,12 +2,17 @@ import './styles/global.css'
 
 import { StrictMode } from 'react'
 
+import { Buffer } from 'buffer'
 import ReactDOM from 'react-dom/client'
 
 import { APIClientProvider } from './providers/api-client'
 import { QueryClientProvider } from './providers/query-client'
 import { RouterProvider } from './providers/router'
 import { StatusProvider } from './providers/status'
+
+if (typeof window !== 'undefined') {
+  window.Buffer = Buffer
+}
 
 const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
