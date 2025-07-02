74910f05e0239a7014557e3ba57d7463528d92e1
jinhojang6 <jinho5051@gmail.com>
2025-07-03 00:28:52 +0900

    feat: add mock data to visualize pending state with storage

--- packages/wallet/src/types.ts
+++ packages/wallet/src/types.ts
@@ -9,3 +9,22 @@ export type CustomisationColorType = keyof typeof customisation
 export type Prettify<T> = {
   [K in keyof T]: T[K]
 } & {}
+
+declare global {
+  interface Window {
+    chrome?: {
+      storage?: {
+        local: {
+          get(
+            keys: string[],
+            callback: (result: { pendingTransactions?: [] }) => void,
+          ): void
+          set(
+            items: { pendingTransactions?: unknown[] },
+            callback?: () => void,
+          ): void
+        }
+      }
+    }
+  }
+}
