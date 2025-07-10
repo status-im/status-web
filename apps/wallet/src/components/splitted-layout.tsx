09cf97421d2ee2cafa8e1c2992ad39b6c70f703d
marcelines <marcio@status.im>
2025-07-10 13:08:13 +0100

    f

--- apps/wallet/src/components/splitted-layout.tsx
+++ apps/wallet/src/components/splitted-layout.tsx
@@ -1,13 +1,16 @@
-import { Suspense } from 'react'
-
 import { Avatar, Skeleton } from '@status-im/components'
 import { Balance, StickyHeaderContainer } from '@status-im/wallet/components'
 
-import { useWallet } from '@/providers/wallet-context'
+import { usePortfolio } from '@/hooks/use-portfolio'
 
 import { ActionButtons } from '../components/action-buttons'
 import { RecoveryPhraseBackup } from '../components/recovery-phrase-backup'
-import { DevSuspenseWrapper } from './dev-suspense-wrapper'
+import {
+  AccountSkeleton,
+  ActionButtonsSkeleton,
+  BalanceSkeleton,
+  TabsSkeleton,
+} from './skeletons'
 import { TabLink } from './tab-link'
 
 type Props = {
@@ -17,16 +20,8 @@ type Props = {
   loadingState?: React.ReactNode
 }
 
-const DEFAULT_SUMMARY = {
-  total_balance: 0,
-  total_eur: 0,
-  total_eur_24h_change: 0.0,
-  total_percentage_24h_change: 0.0,
-}
-
-//   Includes mock data for actions buttons and options. todo? Replace with actual data
 const actionsButtonsData = {
-  address: 'portfolio', // This should be the address of the account coming from the URL. Wrote to have active state
+  address: 'portfolio',
   pathname: '/portfolio/assets',
   searchAndSortValues: {
     inputValue: '',
@@ -44,257 +39,22 @@ const actionsButtonsData = {
   },
 }
 
-const account = {
-  name: 'Account 1',
-  emoji: '🍑',
-  color: 'magenta',
-}
-
 const SplittedLayout = (props: Props) => {
   const { list, detail, isLoading, loadingState } = props
 
-  const { currentWallet, isLoading: isWalletLoading } = useWallet()
-
-  const address = currentWallet?.activeAccounts[0].address
-
-  const { data } = useAssets({
-    address,
-    isWalletLoading,
-  })
-
-  const summary = data?.summary ? data.summary : DEFAULT_SUMMARY
-
   return (
     <div className="grid flex-1 divide-x divide-neutral-10 overflow-hidden">
-      {/* Main content */}
       <div className="flex divide-x divide-default-neutral-20">
         <div className="flex grow flex-col 2xl:basis-1/2">
           <div className="relative h-[calc(100vh-56px)] overflow-auto">
             <StickyHeaderContainer
               isLarge
               className="px-6 xl:px-12"
-              leftSlot={
-                <Suspense
-                  fallback={
-                    <div className="flex flex-col gap-2 px-3">
-                      <div className="flex items-center justify-between gap-1.5">
-                        <div className="hidden items-center gap-1.5 xl:flex">
-                          <Skeleton
-                            height={27}
-                            width={27}
-                            className="rounded-10"
-                          />
-                          <Skeleton
-                            height={16}
-                            width={73}
-                            className="rounded-10"
-                          />
-                        </div>
-                      </div>
-                      <div className="-mt-2 flex items-center gap-1">
-                        <Skeleton
-                          height={27}
-                          width={73}
-                          className="rounded-10"
-                        />
-                        <Skeleton
-                          height={16}
-                          width={16}
-                          className="rounded-10"
-                        />
-                      </div>
-                      <div className="flex items-center gap-1">
-                        <Skeleton
-                          height={12}
-                          width={40}
-                          className="rounded-10"
-                        />
-                        <Skeleton
-                          height={12}
-                          width={26}
-                          className="rounded-10"
-                        />
-                        <Skeleton
-                          height={12}
-                          width={34}
-                          className="rounded-10"
-                        />
-                      </div>
-                    </div>
-                  }
-                >
-                  <DevSuspenseWrapper>
-                    <div
-                      className="hidden items-center gap-1.5 xl:flex"
-                      data-customisation={account.color}
-                    >
-                      <Avatar
-                        type="account"
-                        name={account.name}
-                        emoji={account.emoji}
-                        size="24"
-                        bgOpacity="20"
-                      />
-                      <div className="text-15 font-semibold text-neutral-100">
-                        {account.name}
-                      </div>
-                    </div>
-                    <Balance summary={summary} />
-                  </DevSuspenseWrapper>
-                </Suspense>
-              }
-              rightSlot={
-                <Suspense
-                  fallback={
-                    <div className="flex flex-col items-end gap-2 sm:flex-row sm:items-center">
-                      <Skeleton
-                        height={32}
-                        width={70}
-                        className="rounded-10"
-                        variant="secondary"
-                      />
-                      <Skeleton
-                        height={32}
-                        width={105}
-                        className="rounded-10"
-                        variant="secondary"
-                      />
-                    </div>
-                  }
-                >
-                  <DevSuspenseWrapper>
-                    <div className="flex flex-col items-end gap-2 sm:flex-row sm:items-center">
-                      <TabLink
-                        href="/portfolio/assets"
-                        className="w-full justify-center text-center sm:w-fit"
-                      >
-                        Assets
-                      </TabLink>
-                      <TabLink href="/portfolio/collectibles">
-                        Collectibles
-                      </TabLink>
-                    </div>
-                  </DevSuspenseWrapper>
-                </Suspense>
-              }
+              leftSlot={<HeaderLeftSlot />}
+              rightSlot={<HeaderRightSlot />}
             >
               <div className="relative -mt-8 flex flex-1 flex-col px-3 xl:mt-0 xl:px-12">
-                {/* Todo?: decide if it makes sense to abstract this component with the account call inside keeping the current suspense model or change it to a regular loading state here instead */}
-                <Suspense
-                  fallback={
-                    <div className="flex flex-col gap-2 px-3">
-                      <div className="flex items-center justify-between gap-1.5">
-                        <div className="hidden items-center gap-1.5 xl:flex">
-                          <Skeleton
-                            height={27}
-                            width={27}
-                            className="rounded-10"
-                            variant="secondary"
-                          />
-                          <Skeleton
-                            height={16}
-                            width={73}
-                            className="rounded-10"
-                            variant="secondary"
-                          />
-                        </div>
-                        <Skeleton
-                          height={27}
-                          width={186}
-                          className="rounded-10"
-                          variant="secondary"
-                        />
-                      </div>
-                      <div className="mt-1 flex items-center gap-1">
-                        <Skeleton
-                          height={27}
-                          width={73}
-                          className="rounded-10"
-                          variant="secondary"
-                        />
-                        <Skeleton
-                          height={16}
-                          width={16}
-                          className="rounded-10"
-                          variant="secondary"
-                        />
-                      </div>
-                      <div className="flex items-center gap-1">
-                        <Skeleton
-                          height={12}
-                          width={40}
-                          className="rounded-10"
-                          variant="secondary"
-                        />
-                        <Skeleton
-                          height={12}
-                          width={26}
-                          className="rounded-10"
-                          variant="secondary"
-                        />
-                        <Skeleton
-                          height={12}
-                          width={34}
-                          className="rounded-10"
-                          variant="secondary"
-                        />
-                      </div>
-                      <div className="my-5 flex items-center justify-between">
-                        <div className="flex items-center gap-1.5">
-                          <Skeleton
-                            height={32}
-                            width={70}
-                            className="rounded-10"
-                            variant="secondary"
-                          />
-                          <Skeleton
-                            height={32}
-                            width={105}
-                            className="rounded-10"
-                            variant="secondary"
-                          />
-                        </div>
-                        <Skeleton
-                          height={32}
-                          width={32}
-                          className="rounded-10"
-                          variant="secondary"
-                        />
-                      </div>
-                    </div>
-                  }
-                >
-                  <DevSuspenseWrapper>
-                    <div className="mb-5 flex flex-col gap-2 px-3">
-                      <div
-                        className="flex items-center justify-between gap-1.5"
-                        data-customisation={account.color}
-                      >
-                        <div className="hidden items-center gap-1.5 xl:flex">
-                          <Avatar
-                            type="account"
-                            name={account.name}
-                            emoji={account.emoji}
-                            size="24"
-                            bgOpacity="20"
-                          />
-                          <div className="text-15 font-semibold text-neutral-100">
-                            {account.name}
-                          </div>
-                        </div>
-                        <div data-customisation="blue">
-                          <RecoveryPhraseBackup />
-                        </div>
-                      </div>
-
-                      <div className="mb-4">
-                        <Balance summary={summary} />
-                      </div>
-
-                      <ActionButtons {...actionsButtonsData} />
-                    </div>
-                  </DevSuspenseWrapper>
-                </Suspense>
+                <MainContentBody />
 
                 {isLoading ? loadingState : list}
               </div>
@@ -319,3 +79,139 @@ const SplittedLayout = (props: Props) => {
 }
 
 export default SplittedLayout
+
+// Components
+const AccountInfo = () => {
+  const { account, isWalletLoading } = usePortfolio()
+
+  if (isWalletLoading) {
+    return <AccountSkeleton variant="secondary" />
+  }
+
+  return (
+    <div
+      className="hidden items-center gap-1.5 xl:flex"
+      data-customisation={account.color}
+    >
+      <Avatar
+        type="account"
+        name={account.name}
+        emoji={account.emoji}
+        size="24"
+        bgOpacity="20"
+      />
+      <div className="text-15 font-semibold text-neutral-100">
+        {account.name}
+      </div>
+    </div>
+  )
+}
+
+const PortfolioBalance = ({
+  variant = 'primary',
+}: {
+  variant?: 'primary' | 'secondary'
+}) => {
+  const { summary, isLoading } = usePortfolio()
+
+  if (isLoading) {
+    return <BalanceSkeleton variant={variant} />
+  }
+
+  return <Balance summary={summary} />
+}
+
+const HeaderLeftSlot = () => {
+  const { isLoading } = usePortfolio()
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col gap-2 px-3">
+        <div className="flex items-center justify-between gap-1.5">
+          <AccountSkeleton />
+        </div>
+        <BalanceSkeleton />
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <AccountInfo />
+      <PortfolioBalance />
+    </>
+  )
+}
+
+const HeaderRightSlot = () => {
+  const { isLoading } = usePortfolio()
+
+  if (isLoading) {
+    return <TabsSkeleton />
+  }
+
+  return (
+    <div className="flex flex-col items-end gap-2 sm:flex-row sm:items-center">
+      <TabLink
+        href="/portfolio/assets"
+        className="w-full justify-center text-center sm:w-fit"
+      >
+        Assets
+      </TabLink>
+      <TabLink href="/portfolio/collectibles">Collectibles</TabLink>
+      <TabLink href="/portfolio/activity">Activity</TabLink>
+    </div>
+  )
+}
+
+const MainContentBody = () => {
+  const { account, isLoading } = usePortfolio()
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col gap-2 px-3">
+        <div className="flex items-center justify-between gap-1.5">
+          <AccountSkeleton variant="secondary" />
+          <Skeleton
+            height={27}
+            width={186}
+            className="rounded-10"
+            variant="secondary"
+          />
+        </div>
+        <div className="mt-1">
+          <BalanceSkeleton variant="secondary" />
+        </div>
+        <div className="my-5 flex items-center justify-between">
+          <ActionButtonsSkeleton />
+          <Skeleton
+            height={32}
+            width={32}
+            className="rounded-10"
+            variant="secondary"
+          />
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className="mb-5 flex flex-col gap-2 px-3">
+      <div
+        className="flex items-center justify-between gap-1.5"
+        data-customisation={account.color}
+      >
+        <AccountInfo />
+        <div data-customisation="blue">
+          <RecoveryPhraseBackup />
+        </div>
+      </div>
+
+      <div className="mb-4">
+        <PortfolioBalance variant="secondary" />
+      </div>
+
+      <ActionButtons {...actionsButtonsData} />
+    </div>
+  )
+}
