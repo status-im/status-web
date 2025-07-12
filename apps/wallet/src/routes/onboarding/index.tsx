41cd65195e0c5d25f902c26331da944065300276
Felicio <felicio@users.noreply.github.com>
2025-07-12 09:51:07 +0900

    Revert "update legal refs"
    This reverts commit 739b7c5a37853624358a9c9f957397c5e7f86573.

--- apps/wallet/src/routes/onboarding/index.tsx
+++ apps/wallet/src/routes/onboarding/index.tsx
@@ -33,7 +33,7 @@ function RouteComponent() {
         By continuing you agree with Status
         <br />
         <a
-          href="https://github.com/status-im/status-software-legal-documents/blob/master/browser-extension-wallet-terms-of-use"
+          href="https://github.com/status-im/status-software-legal-documents/blob/master/terms-of-use.md"
           className="text-neutral-100"
           target="_blank"
           rel="noopener noreferrer"
@@ -42,7 +42,7 @@ function RouteComponent() {
         </a>{' '}
         and{' '}
         <a
-          href="https://github.com/status-im/status-software-legal-documents/blob/master/browser-extension-wallet-privacy-policy"
+          href="https://github.com/status-im/status-software-legal-documents/blob/master/privacy-policy.md"
           className="text-neutral-100"
           target="_blank"
           rel="noopener noreferrer"
