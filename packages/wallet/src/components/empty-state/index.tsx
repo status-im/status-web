b40f241919ca466d62d8bfabe449182fee01f9f4
marcelines <marcio@status.im>
2025-07-30 15:13:38 +0100

    f: empty states chart

--- packages/wallet/src/components/empty-state/index.tsx
+++ packages/wallet/src/components/empty-state/index.tsx
@@ -1,11 +1,23 @@
+import { cva } from 'cva'
 import { match } from 'ts-pattern'
 
 import { Image } from '../image'
 
 import type { ImageId } from '../image'
 
+const styles = cva({
+  variants: {
+    variant: {
+      activity: 'size-20',
+      collectible: 'size-20',
+      balance: 'h-[112px]',
+      price: 'h-[112px]',
+    },
+  },
+})
+
 type Props = {
-  variant: 'activity' | 'collectible'
+  variant: 'activity' | 'collectible' | 'balance' | 'price'
 }
 
 const EmptyState = (props: Props) => {
@@ -24,11 +36,27 @@ const EmptyState = (props: Props) => {
       title: 'No Collectibles',
       description: 'Your collectibles will be visible on this page',
     }))
+    .with('balance', () => ({
+      imageId: 'Portfolio/Empty States/No_Balance:750:232' as ImageId,
+      imageAlt: 'No balance',
+      title: 'No balance',
+      description: 'Hodl this asset to view your balance history here',
+    }))
+    .with('price', () => ({
+      imageId: 'Portfolio/Empty States/No_Balance:750:232' as ImageId,
+      imageAlt: 'No prices',
+      title: 'No prices',
+      description: 'Prices will be visible on this page',
+    }))
     .exhaustive()
 
   return (
     <div className="m-auto flex flex-col items-center justify-center text-center">
-      <Image id={content.imageId} alt={content.imageAlt} className="size-20" />
+      <Image
+        id={content.imageId}
+        alt={content.imageAlt}
+        className={styles({ variant })}
+      />
       <h2 className="mb-0.5 text-15 font-600">{content.title}</h2>
       <p className="text-13">{content.description}</p>
     </div>
