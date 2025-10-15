b56d206c7fade6de4852a21f3d4843bd8c615b18
Jakub Kotula <520927+jkbktl@users.noreply.github.com>
2025-10-15 22:04:07 +0200

    make env variable required

--- apps/hub/src/config/env.client.mjs
+++ apps/hub/src/config/env.client.mjs
@@ -2,7 +2,9 @@ import { z } from 'zod'
 import { handleError } from './env.base.mjs'
 
 export const envSchema = z.object({
-  NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: z.string().optional(),
+  NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: z
+    .string()
+    .min(1, 'WalletConnect Project ID is required.'),
 })
 
 export const result = envSchema.strip().safeParse({
