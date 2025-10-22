65cef9eec9e4d1c6a2e302d2b3e1f292a2505854
Jakub Kotula <520927+jkbktl@users.noreply.github.com>
2025-10-22 14:26:07 +0200

    Revert "update dropdown icon"
    This reverts commit 3046eee7561dbd6f8234ad7a127b5ce98cf47ee5.

--- packages/wallet/src/components/feedback/index.tsx
+++ packages/wallet/src/components/feedback/index.tsx
@@ -35,9 +35,7 @@ const FeedbackPopover = () => {
       <button className="flex cursor-pointer select-none items-center rounded-10 border border-neutral-70 hover:border-neutral-60">
         <div className="flex items-center gap-1 px-2 py-[5px] text-15 font-500 text-white-100 transition">
           Share feedback
-          <span className="text-neutral-40">
-            <DropdownIcon />
-          </span>
+          <DropdownIcon />
         </div>
       </button>
 
