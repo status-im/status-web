f2047467d88680d680fa74ad1a4b66659b177b05
Jakub Kotula <520927+jkbktl@users.noreply.github.com>
2025-10-22 14:26:38 +0200

    update correct dropdown icon

--- packages/status-network/src/components/feedback-popover/dropdown-icon.tsx
+++ packages/status-network/src/components/feedback-popover/dropdown-icon.tsx
@@ -1,12 +1,15 @@
-const DropdownIcon = () => {
+import type { SVGProps } from 'react'
+
+const DropdownIcon = (props: SVGProps<SVGSVGElement>) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="12"
       height="12"
       fill="none"
-      aria-hidden="true"
-      focusable="false"
+      aria-hidden={true}
+      focusable={false}
+      {...props}
     >
       <g stroke="currentColor" clipPath="url(#a)">
         <circle cx="6" cy="6" r="5" strokeWidth="1.2" />
