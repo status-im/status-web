7c211ad67cb2123bed7d921993623291d7cebb03
Felicio <felicio@users.noreply.github.com>
2025-11-05 14:42:01 +0000

    disable SIWE

--- apps/hub/src/app/_providers/index.tsx
+++ apps/hub/src/app/_providers/index.tsx
@@ -2,11 +2,14 @@
 
 import { ToastContainer } from '@status-im/components'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ConnectKitProvider, SIWEProvider } from 'connectkit'
+import {
+  ConnectKitProvider,
+  // SIWEProvider
+} from 'connectkit'
 import { WagmiProvider } from 'wagmi'
 
 import { testnet, wagmiConfig } from '~constants/chain'
-import { siweConfig } from '~constants/siwe'
+// import { siweConfig } from '~constants/siwe'
 import { VaultStateProvider } from '~hooks/useVaultStateContext'
 
 import { VaultProvider } from './vault-provider'
@@ -42,20 +45,20 @@ export function Providers({ children }: ProvidersProps) {
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <SIWEProvider {...siweConfig}>
-          <ConnectKitProvider
-            options={{
-              initialChainId: testnet.id,
-            }}
-          >
-            <VaultStateProvider>
-              <VaultProvider>
-                {children}
-                <ToastContainer />
-              </VaultProvider>
-            </VaultStateProvider>
-          </ConnectKitProvider>
-        </SIWEProvider>
+        {/* <SIWEProvider {...siweConfig}> */}
+        <ConnectKitProvider
+          options={{
+            initialChainId: testnet.id,
+          }}
+        >
+          <VaultStateProvider>
+            <VaultProvider>
+              {children}
+              <ToastContainer />
+            </VaultProvider>
+          </VaultStateProvider>
+        </ConnectKitProvider>
+        {/* </SIWEProvider> */}
       </QueryClientProvider>
     </WagmiProvider>
   )
