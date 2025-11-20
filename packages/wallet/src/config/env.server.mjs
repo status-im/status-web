58ae1de6d0db4fcd764ef96acb8a42eb236e85fa
jinhojang6 <jinho5051@gmail.com>
2025-11-20 20:51:05 +0900

    Update ETH_RPC_PROXY authentication to make username and password optional

--- packages/wallet/src/config/env.server.mjs
+++ packages/wallet/src/config/env.server.mjs
@@ -13,8 +13,8 @@ export const envSchema = z.object({
   COINGECKO_API_KEY: z.string(),
   PORT: z.coerce.number().optional(),
   MERCURYO_SECRET_KEY: z.string(),
-  ETH_RPC_PROXY_AUTH_USERNAME: z.string(),
-  ETH_RPC_PROXY_AUTH_PASSWORD: z.string(),
+  ETH_RPC_PROXY_AUTH_USERNAME: z.string().optional(),
+  ETH_RPC_PROXY_AUTH_PASSWORD: z.string().optional(),
   MARKET_PROXY_AUTH_USERNAME: z.string().optional(),
   MARKET_PROXY_AUTH_PASSWORD: z.string().optional(),
 })
