e36eff1b37b5a4a8753523f436397057ea9c77d6
Jakub Kotula <520927+jkbktl@users.noreply.github.com>
2025-12-18 10:47:20 +0100

    fix spacings

--- apps/hub/src/app/_components/accordion.tsx
+++ apps/hub/src/app/_components/accordion.tsx
@@ -22,17 +22,15 @@ const Accordion = ({ items }: Props) => {
           className="rounded-20 border border-neutral-10 bg-white-100 shadow-1 lg:rounded-32"
         >
           <AccordionPrimitive.Header>
-            <AccordionPrimitive.Trigger className="group flex w-full items-center justify-start gap-3 p-3 text-left text-15 font-500 text-neutral-100 lg:px-8 lg:py-6 lg:text-19">
-              <div className="flex size-8 items-center justify-center rounded-full bg-customisation-purple-50/5 text-19 text-purple transition-colors duration-200 group-active:bg-customisation-purple-50/30 group-hover:bg-customisation-purple-50/20 lg:size-12">
-                <ChevronDownIcon className="size-5 shrink-0 text-purple transition-transform duration-200 group-data-[state=open]:rotate-180" />
+            <AccordionPrimitive.Trigger className="group flex w-full items-center justify-start gap-3 p-3 pb-2 text-left text-15 font-500 text-neutral-100 transition-all lg:px-8 lg:py-6 lg:text-19 data-[state=open]:lg:pb-0">
+              <div className="flex size-8 shrink-0 items-center justify-center rounded-full bg-customisation-purple-50/5 text-19 text-purple transition-colors duration-200 group-active:bg-customisation-purple-50/30 group-hover:bg-customisation-purple-50/20 lg:size-12 lg:min-w-12">
+                <ChevronDownIcon className="size-5 min-w-5 shrink-0 text-purple transition-transform duration-200 group-data-[state=open]:rotate-180" />
               </div>
-              <span className="transition-all group-data-[state=open]:-mt-2 lg:group-data-[state=open]:-mt-3">
-                {item.title}
-              </span>
+              {item.title}
             </AccordionPrimitive.Trigger>
           </AccordionPrimitive.Header>
           <AccordionPrimitive.Content className="overflow-hidden data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down">
-            <div className="pb-4 pl-[46px] text-13 text-neutral-50 lg:pl-[62px] lg:text-15">
+            <div className="pb-3 pl-[58px] pr-3 pt-0 text-13 text-neutral-50 lg:pb-6 lg:pl-[94px] lg:pr-8 lg:text-15">
               {item.content}
             </div>
           </AccordionPrimitive.Content>
