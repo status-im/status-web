9262d0636809c9d4a507503a4e0f955adeae59c8
jinhojang6 <jinho5051@gmail.com>
2026-01-16 00:18:06 +0900

    refactor: move getPathname utility to status-network package and remove redundant implementations

--- apps/status.network/src/app/[locale]/layout.tsx
+++ apps/status.network/src/app/[locale]/layout.tsx
@@ -1,7 +1,7 @@
+import { getPathname } from '@status-im/status-network/utils'
 import { NextIntlClientProvider } from 'next-intl'
 import { getMessages } from 'next-intl/server'
 import { Metadata } from '../_metadata'
-import { getPathname } from '../_utils/get-pathname'
 
 export async function generateMetadata() {
   const pathname = await getPathname()
