7f83497185d470a2484f697f3f5885de6e66ae71
Felicio <felicio@users.noreply.github.com>
2026-01-27 00:05:48 +0000

    hotfix: enable all vaults by default

--- apps/hub/src/app/_components/vault-card.tsx
+++ apps/hub/src/app/_components/vault-card.tsx
@@ -145,8 +145,9 @@ const VaultCardContent: FC<VaultCardContentProps> = ({
   const t = useTranslations()
 
   const vaultAddressLower = vault.address.toLowerCase()
-  const isVaultInApi = apyMap !== undefined && vaultAddressLower in apyMap
-  const isDisabled = !isVaultInApi
+  // const isVaultInApi = apyMap !== undefined && vaultAddressLower in apyMap
+  // const isDisabled = !isVaultInApi
+  const isDisabled = false
   const dynamicApy = apyMap?.[vaultAddressLower]
   const apyValue = dynamicApy !== undefined ? String(dynamicApy) : null
   const rewardsLine = rewards
