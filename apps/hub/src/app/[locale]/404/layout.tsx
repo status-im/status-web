816f4eae4521dcd2692e89bb42932a0421144273
Felicio <felicio@users.noreply.github.com>
2026-02-05 09:06:51 +0000

    u

--- apps/hub/src/app/[locale]/404/layout.tsx
+++ apps/hub/src/app/[locale]/404/layout.tsx
@@ -3,9 +3,9 @@ import { Metadata as MetadataFn } from '~/app/_metadata'
 export const dynamic = 'force-static'
 
 export const metadata = MetadataFn({
-  // title: '404 — Page Not Found',
-  // description:
-  //   'The page you were looking for could not be found. Return to the Status Hub homepage.',
+  title: '404 — Page Not Found',
+  description:
+    'The page you were looking for could not be found. Return to the Status Hub homepage.',
   robots: {
     index: false,
   },
