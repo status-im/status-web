c4688d8b99baf37e4df07e0f3888d59b5812ae03
JulesFILIOT <dev@julesfiliot.com>
2026-04-24 18:12:50 +0900

    feat(wallet): extract buildCanonicalTimestamps utility for uniform chart grid

--- packages/wallet/src/utils/index.ts
+++ packages/wallet/src/utils/index.ts
@@ -1,2 +1,7 @@
+export type { ChartDays } from './chart-timestamps'
+export {
+  buildCanonicalTimestamps,
+  chartIntervalSeconds,
+} from './chart-timestamps'
 export { toChecksumAddress } from './to-checksum-address'
 export { getTransactionHash } from './transaction-hash'
